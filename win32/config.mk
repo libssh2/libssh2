22c105332d7cd63b40fd782301342eec5efa52b1
Wez Furlong <kingwez@gmail.com>
2005-01-11 02:23:28 +0000

    Add release build for win32

--- win32/config.mk
+++ win32/config.mk
@@ -6,18 +6,23 @@ OPENSSLLIB=\local\php\php_build\lib
 ZLIBINC=-DLIBSSH2_HAVE_ZLIB=1 /I\local\php\php_build\include
 ZLIBLIB=\local\php\php_build\lib
 
-CPPFLAGS=/nologo /GL /Zi /EHsc /MD /Iwin32 /Iinclude /I$(OPENSSLINC) $(ZLIBINC) -DLIBSSH2_WIN32
-CFLAGS=$(CPPFLAGS)
-DLLFLAGS=$(CFLAGS) /LDd
-LIBS=$(OPENSSLLIB)\libeay32.lib $(OPENSSLLIB)\ssleay32.lib ws2_32.lib $(ZLIBLIB)\zlib.lib
-
 !if "$(TARGET)" == ""
-TARGET=Debug
+TARGET=Release
 !endif
 
 !if "$(TARGET)" == "Debug"
 SUFFIX=_debug
+CPPFLAGS=/Od /MDd
+DLLFLAGS=/DEBUG /LDd
+!else
+CPPFLAGS=/Og /Oi /O2 /Oy /GF /Y- /MD /DNDEBUG
+DLLFLAGS=/DEBUG /LD
 !endif
+
+CPPFLAGS=/nologo /GL /Zi /EHsc $(CPPFLAGS) /Iwin32 /Iinclude /I$(OPENSSLINC) $(ZLIBINC) -DLIBSSH2_WIN32
+CFLAGS=$(CPPFLAGS)
+DLLFLAGS=$(CFLAGS) $(DLLFLAGS)
+LIBS=$(OPENSSLLIB)\libeay32.lib $(OPENSSLLIB)\ssleay32.lib ws2_32.lib $(ZLIBLIB)\zlib.lib
 
 INTDIR=$(TARGET)\$(SUBDIR)
 
