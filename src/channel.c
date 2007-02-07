fee0239211298a4c17fb461e86ea52c186f77a5b
Daniel Stenberg <daniel@haxx.se>
2007-02-07 21:39:17 +0000

    setting sockets non-blocking using more portable code and configure macros from the libcurl project (written by me)

--- src/channel.c
+++ src/channel.c
@@ -43,6 +43,76 @@
 
 #include <inttypes.h>
 
+
+/*
+ * _libssh2_nonblock() sets the given socket to either blocking or
+ * non-blocking mode based on the 'nonblock' boolean argument. This function
+ * is copied from the libcurl sources with permission.
+ */
+static int _libssh2_nonblock(int sockfd,    /* operate on this */
+			     int nonblock   /* TRUE or FALSE */)
+{
+#undef SETBLOCK
+#define SETBLOCK 0
+#ifdef HAVE_O_NONBLOCK
+  /* most recent unix versions */
+  int flags;
+
+  flags = fcntl(sockfd, F_GETFL, 0);
+  if (nonblock)
+    return fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
+  else
+    return fcntl(sockfd, F_SETFL, flags & (~O_NONBLOCK));
+#undef SETBLOCK
+#define SETBLOCK 1
+#endif
+
+#if defined(HAVE_FIONBIO) && (SETBLOCK == 0)
+  /* older unix versions */
+  int flags;
+
+  flags = nonblock;
+  return ioctl(sockfd, FIONBIO, &flags);
+#undef SETBLOCK
+#define SETBLOCK 2
+#endif
+
+#if defined(HAVE_IOCTLSOCKET) && (SETBLOCK == 0)
+  /* Windows? */
+  unsigned long flags;
+  flags = nonblock;
+
+  return ioctlsocket(sockfd, FIONBIO, &flags);
+#undef SETBLOCK
+#define SETBLOCK 3
+#endif
+
+#if defined(HAVE_IOCTLSOCKET_CASE) && (SETBLOCK == 0)
+  /* presumably for Amiga */
+  return IoctlSocket(sockfd, FIONBIO, (long)nonblock);
+#undef SETBLOCK
+#define SETBLOCK 4
+#endif
+
+#if defined(HAVE_SO_NONBLOCK) && (SETBLOCK == 0)
+  /* BeOS */
+  long b = nonblock ? 1 : 0;
+  return setsockopt(sockfd, SOL_SOCKET, SO_NONBLOCK, &b, sizeof(b));
+#undef SETBLOCK
+#define SETBLOCK 5
+#endif
+
+#ifdef HAVE_DISABLED_NONBLOCKING
+  return 0; /* returns success */
+#undef SETBLOCK
+#define SETBLOCK 6
+#endif
+
+#if (SETBLOCK == 0)
+#error "no non-blocking method was found/used/set"
+#endif
+}
+
 /* {{{ libssh2_channel_nextid
  * Determine the next channel ID we can use at our end
  */
@@ -775,20 +845,8 @@ int _libssh2_channel_set_blocking(LIBSSH2_CHANNEL *channel,
 	}
 	channel->blocking = blocking;
 
-#ifdef F_SETFL
-	/* FIXME: this can/should be done in a more portable manner */
-	rc = fcntl(channel->session->socket_fd, F_GETFL, 0);
-	if(!blocking) {
-		rc |= O_NONBLOCK;
-	}
-	else {
-		rc &= ~O_NONBLOCK;
-	}
+	_libssh2_nonblock(channel->session->socket_fd, !blocking);
 
-	fcntl(channel->session->socket_fd, F_SETFL, rc);
-#else
-#error "add support for setting the socket non-blocking here"
-#endif
 	return bl;
 }
 /* }}} */
