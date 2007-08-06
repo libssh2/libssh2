9f27d176f84e0d4dd10b1d9415a75ea36f30159f
Daniel Stenberg <daniel@haxx.se>
2007-08-06 20:59:08 +0000

    we work on 0.18 now, although it might be 1.0 - who knows?

--- include/libssh2.h
+++ include/libssh2.h
@@ -79,12 +79,12 @@ typedef unsigned long long libssh2_uint64_t;
 typedef long long libssh2_int64_t;
 #endif
 
-#define LIBSSH2_VERSION                             "0.16.0-CVS"
+#define LIBSSH2_VERSION                             "0.18.0-CVS"
 
 /* The numeric version number is also available "in parts" by using these
    defines: */
 #define LIBSSH2_VERSION_MAJOR                       0
-#define LIBSSH2_VERSION_MINOR                       16
+#define LIBSSH2_VERSION_MINOR                       18
 #define LIBSSH2_VERSION_PATCH                       0
 
 /* This is the numeric version of the libssh2 version number, meant for easier
@@ -102,7 +102,7 @@ typedef long long libssh2_int64_t;
    and it is always a greater number in a more recent release. It makes
    comparisons with greater than and less than work.
 */
-#define LIBSSH2_VERSION_NUM                         0x001000
+#define LIBSSH2_VERSION_NUM                         0x001200
 
 /*
  * This is the date and time when the full source package was created. The
