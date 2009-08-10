ce4ad0d08679b26c94e29bfcbf814126bbe4512b
Daniel Stenberg <daniel@haxx.se>
2009-08-10 22:58:15 +0200

    start working on 1.2.1

--- include/libssh2.h
+++ include/libssh2.h
@@ -88,12 +88,12 @@ typedef long long libssh2_int64_t;
    to make the BANNER define (used by src/session.c) be a valid SSH
    banner. Release versions have no appended strings and may of course not
    have dashes either. */
-#define LIBSSH2_VERSION                             "1.1.1_CVS"
+#define LIBSSH2_VERSION                             "1.2.1_CVS"
 
 /* The numeric version number is also available "in parts" by using these
    defines: */
 #define LIBSSH2_VERSION_MAJOR                       1
-#define LIBSSH2_VERSION_MINOR                       1
+#define LIBSSH2_VERSION_MINOR                       2
 #define LIBSSH2_VERSION_PATCH                       1
 
 /* This is the numeric version of the libssh2 version number, meant for easier
@@ -111,7 +111,7 @@ typedef long long libssh2_int64_t;
    and it is always a greater number in a more recent release. It makes
    comparisons with greater than and less than work.
 */
-#define LIBSSH2_VERSION_NUM                         0x010101
+#define LIBSSH2_VERSION_NUM                         0x010201
 
 /*
  * This is the date and time when the full source package was created. The
