6c3f2ec81591a15d70fc1c71438d18fc463e7799
Alexander Lamaison <awl03@doc.ic.ac.uk>
2010-04-28 17:41:17 +0100

    Removed unecessary brackets.

--- src/sftp.c
+++ src/sftp.c
@@ -95,10 +95,10 @@ static int sftp_close_handle(LIBSSH2_SFTP_HANDLE *handle);
 static int sftp_attrsize(unsigned long flags)
 {
     return (4 +                                 /* flags(4) */
-            (((flags) & LIBSSH2_SFTP_ATTR_SIZE) ? 8 : 0) +
-            (((flags) & LIBSSH2_SFTP_ATTR_UIDGID) ? 8 : 0) +
-            (((flags) & LIBSSH2_SFTP_ATTR_PERMISSIONS) ? 4 : 0) +
-            (((flags) & LIBSSH2_SFTP_ATTR_ACMODTIME) ? 8 : 0));
+            ((flags & LIBSSH2_SFTP_ATTR_SIZE) ? 8 : 0) +
+            ((flags & LIBSSH2_SFTP_ATTR_UIDGID) ? 8 : 0) +
+            ((flags & LIBSSH2_SFTP_ATTR_PERMISSIONS) ? 4 : 0) +
+            ((flags & LIBSSH2_SFTP_ATTR_ACMODTIME) ? 8 : 0));
                                                 /* atime + mtime as u32 */
 }
 
