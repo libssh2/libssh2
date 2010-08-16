43001570d668fd55babd9254112b6a7c8e5b510d
Guenter Knauf <lists@gknw.net>
2010-08-17 01:35:55 +0200

    Updated Win32 MSVC dependencies versions.

--- win32/config.mk
+++ win32/config.mk
@@ -1,10 +1,10 @@
 
 # Tweak these for your system
-OPENSSLINC=..\openssl-0.9.8e\inc32
-OPENSSLLIB=..\openssl-0.9.8e\out32dll
+OPENSSLINC=..\openssl-0.9.8o\inc32
+OPENSSLLIB=..\openssl-0.9.8o\out32dll
 
-ZLIBINC=-DLIBSSH2_HAVE_ZLIB=1 /I..\zlib-1.2.3
-ZLIBLIB=..\zlib-1.2.3
+ZLIBINC=-DLIBSSH2_HAVE_ZLIB=1 /I..\zlib-1.2.5
+ZLIBLIB=..\zlib-1.2.5
 
 !if "$(TARGET)" == ""
 TARGET=Release
