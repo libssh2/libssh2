075ff1957407218da5dbbb2451b4c6f0166314a3
Daniel Stenberg <daniel@haxx.se>
2010-11-09 14:57:36 +0100

    _libssh2_packet_add: SSH_MSG_IGNORE skip memmove
    There's no promise of a zero termination of the data in the callback so
    no longer perform ugly operation in order to provide it.

--- src/packet.c
+++ src/packet.c
@@ -528,12 +528,9 @@ _libssh2_packet_add(LIBSSH2_SESSION * session, unsigned char *data,
         break;
 
         case SSH_MSG_IGNORE:
-            if (datalen >= 5) {
-                /* Back it up one and add a trailing NULL */
-                memmove(data, data + 1, datalen - 1);
-                data[datalen] = '\0';
+            if (datalen >= 2) {
                 if (session->ssh_msg_ignore) {
-                    LIBSSH2_IGNORE(session, (char *) data + 4, datalen - 1);
+                    LIBSSH2_IGNORE(session, (char *) data + 1, datalen - 1);
                 }
             } else if (session->ssh_msg_ignore) {
                 LIBSSH2_IGNORE(session, "", 0);
