9a7311ba57cfc0096055d61c536a9434cf4d6ced
Guenter Knauf <lists@gknw.net>
2012-05-22 04:10:52 +0200

    Updated dependency libary versions.

--- win32/config.mk
+++ win32/config.mk
@@ -1,10 +1,10 @@
 
 # Tweak these for your system
-OPENSSLINC=..\openssl-0.9.8v\inc32
-OPENSSLLIB=..\openssl-0.9.8v\out32dll
+OPENSSLINC=..\openssl-0.9.8x\inc32
+OPENSSLLIB=..\openssl-0.9.8x\out32dll
 
-ZLIBINC=-DLIBSSH2_HAVE_ZLIB=1 /I..\zlib-1.2.6
-ZLIBLIB=..\zlib-1.2.6
+ZLIBINC=-DLIBSSH2_HAVE_ZLIB=1 /I..\zlib-1.2.7
+ZLIBLIB=..\zlib-1.2.7
 
 !if "$(TARGET)" == ""
 TARGET=Release
