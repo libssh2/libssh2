7d4b5a8e077165cb82ace3e32fc5b576ed791a45
Bob Kast <bobkast@icloud.com>
2014-03-20 14:45:46 -0400

    libssh2_priv.h: a 1 bit bit-field should be unsigned
    some compilers may not like this

--- src/libssh2_priv.h
+++ src/libssh2_priv.h
@@ -574,7 +574,7 @@ struct _LIBSSH2_SESSION
 
     /* Agreed Key Exchange Method */
     const LIBSSH2_KEX_METHOD *kex;
-    int burn_optimistic_kexinit:1;
+    unsigned int burn_optimistic_kexinit:1;
 
     unsigned char *session_id;
     uint32_t session_id_len;
