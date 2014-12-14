bc9d73566494b0b0e5f45bfa4a9d315955876a16
Marc Hoersken <info@marc-hoersken.de>
2014-12-15 00:27:29 +0100

    wincng.c: fix possible NULL pointer de-reference of bignum
    Fixes VS2012 code analysis warning C6011:
    dereferencing NULL pointer 'bignum'. libssh2 wincng.c 1567

--- src/wincng.c
+++ src/wincng.c
@@ -1563,9 +1563,11 @@ _libssh2_wincng_bignum_init(void)
 {
     _libssh2_bn *bignum;
 
-    bignum = malloc(sizeof(_libssh2_bn));
-    bignum->bignum = NULL;
-    bignum->length = 0;
+    bignum = (_libssh2_bn *)malloc(sizeof(_libssh2_bn));
+    if (bignum) {
+        bignum->bignum = NULL;
+        bignum->length = 0;
+    }
 
     return bignum;
 }
