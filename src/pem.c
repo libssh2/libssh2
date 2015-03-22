1429ad749d6560458c7941b3246730aed7328b87
Marc Hoersken <info@marc-hoersken.de>
2015-03-22 13:58:09 +0100

    pem.c: fix copy and paste mistake from 55d030089b8

--- src/pem.c
+++ src/pem.c
@@ -43,21 +43,25 @@ readline(char *line, int line_size, FILE * fp)
 {
     size_t len;
 
-    if (!*line) {
+    if (!line) {
         return -1;
     }
     if (!fgets(line, line_size, fp)) {
         return -1;
     }
 
-    len = strlen(line);
-    if (len > 0 && line[len - 1] == '\n') {
-        line[len - 1] = '\0';
+    if (*line) {
+        len = strlen(line);
+        if (len > 0 && line[len - 1] == '\n') {
+            line[len - 1] = '\0';
+        }
     }
 
-    len = strlen(line);
-    if (len > 0 && line[len - 1] == '\r') {
-        line[len - 1] = '\0';
+    if (*line) {
+        len = strlen(line);
+        if (len > 0 && line[len - 1] == '\r') {
+            line[len - 1] = '\0';
+        }
     }
 
     return 0;
