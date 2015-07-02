13f8addd1bc17e6c55d52491cc6304319ac38c6d
Kamil Dudka <kdudka@redhat.com>
2015-07-02 12:08:03 +0200

    tests/mansyntax.sh: fix 'make distcheck' with recent autotools
    Do not create symbolic links off the build directory.  Recent autotools
    verify that out-of-source build works even if the source directory tree
    is not writable.

--- tests/mansyntax.sh
+++ tests/mansyntax.sh
@@ -6,7 +6,8 @@ set -e
 # Run syntax checks for all manpages in the documentation tree.
 #
 
-srcdir="`cd $(dirname $0);pwd`"
+srcdir=${srcdir:-$PWD}
+dstdir=${builddir:-$PWD}
 mandir=${srcdir}/../docs
 
 #
@@ -19,13 +20,13 @@ fi
 
 ec=0
 
-trap "rm -f $srcdir/man3" EXIT
+trap "rm -f $dstdir/man3" EXIT
 
-ln -sf "$mandir" "$srcdir/man3"
+ln -sf "$mandir" "$dstdir/man3"
 
 for manpage in $mandir/libssh2_*.*; do
   echo "$manpage"
-  warnings=$(LANG=en_US.UTF-8 MANWIDTH=80 man -M "$srcdir" --warnings \
+  warnings=$(LANG=en_US.UTF-8 MANWIDTH=80 man -M "$dstdir" --warnings \
     -E UTF-8 -l "$manpage" 2>&1 >/dev/null)
   if [ -n "$warnings" ]; then
     echo "$warnings"
