d06564e759fa74e725f5793b082a9ff3c4c0119e
Patrick Monnerat <patrick.monnerat@dh.com>
2015-12-09 14:20:16 +0100

    os400qc3: Slave descriptor for key encryption key
    The Qc3 library requires the key encryption key to exist as long as
    the encrypted key is used. Its descriptor token is then kept as an
    "encrypted key slave" for recursive release.

--- src/os400qc3.c
+++ src/os400qc3.c
@@ -956,6 +956,8 @@ null_token(const char *token)
 void
 _libssh2_os400qc3_crypto_dtor(_libssh2_os400qc3_crypto_ctx *x)
 {
+    if (!x)
+        return;
     if (!null_token(x->hash.Alg_Context_Token)) {
         Qc3DestroyAlgorithmContext(x->hash.Alg_Context_Token, (char *) &ecnull);
         memset(x->hash.Alg_Context_Token, 0, sizeof x->hash.Alg_Context_Token);
@@ -963,6 +965,11 @@ _libssh2_os400qc3_crypto_dtor(_libssh2_os400qc3_crypto_ctx *x)
     if (!null_token(x->key.Key_Context_Token)) {
         Qc3DestroyKeyContext(x->key.Key_Context_Token, (char *) &ecnull);
         memset(x->key.Key_Context_Token, 0, sizeof x->key.Key_Context_Token);
+    }
+    if (x->kek) {
+        _libssh2_os400qc3_crypto_dtor(x->kek);
+        free((char *) x->kek);
+        x->kek = NULL;
     }
 }
 
