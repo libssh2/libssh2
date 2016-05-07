339c3fc8c7b9251687e5e0fabbe9e4e0b8607f8c
Salvador Fandiño <sfandino@yahoo.com>
2016-05-07 16:56:22 +0200

    libssh2_channel_window_read_ex: use channel->read_avail instead of walking the packet queue

--- src/channel.c
+++ src/channel.c
@@ -2461,29 +2461,11 @@ libssh2_channel_window_read_ex(LIBSSH2_CHANNEL *channel,
     if(!channel)
         return 0; /* no channel, no window! */
 
-    if (window_size_initial) {
+    if (window_size_initial)
         *window_size_initial = channel->remote.window_size_initial;
-    }
 
-    if (read_avail) {
-        size_t bytes_queued = 0;
-        LIBSSH2_PACKET *packet =
-            _libssh2_list_first(&channel->session->packets);
-
-        while (packet) {
-            unsigned char packet_type = packet->data[0];
-
-            if (((packet_type == SSH_MSG_CHANNEL_DATA)
-                 || (packet_type == SSH_MSG_CHANNEL_EXTENDED_DATA))
-                && (_libssh2_ntohu32(packet->data + 1) == channel->local.id)) {
-                bytes_queued += packet->data_len - packet->data_head;
-            }
-
-            packet = _libssh2_list_next(&packet->node);
-        }
-
-        *read_avail = bytes_queued;
-    }
+    if (read_avail)
+        *read_avail = channel->read_avail;
 
     return channel->remote.window_size;
 }
