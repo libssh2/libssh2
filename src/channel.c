369f3b4908ddae3261b36e4bd7d72cc5dcda45eb
Salvador Fandiño <sfandino@yahoo.com>
2016-05-07 13:03:30 +0200

    libssh2_channel_read_ex: Dont adjust the receive window.
    _libssh2_channel_read is already doing it.

--- src/channel.c
+++ src/channel.c
@@ -1856,18 +1856,9 @@ libssh2_channel_read_ex(LIBSSH2_CHANNEL *channel, int stream_id, char *buf,
                         size_t buflen)
 {
     int rc;
-    unsigned long recv_window;
 
     if(!channel)
         return LIBSSH2_ERROR_BAD_USE;
-
-    recv_window = libssh2_channel_window_read_ex(channel, NULL, NULL);
-
-    if(buflen > recv_window) {
-        BLOCK_ADJUST(rc, channel->session,
-                     _libssh2_channel_receive_window_adjust(channel, buflen,
-                                                            1, NULL));
-    }
 
     BLOCK_ADJUST(rc, channel->session,
                  _libssh2_channel_read(channel, stream_id, buf, buflen));
