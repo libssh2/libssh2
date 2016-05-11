84172e5188fb73d719242cf52a29da8d83a27355
Salvador Fandino <sfandino@yahoo.com>
2016-05-11 11:56:59 +0200

    LIBSSH2_ERROR_CHANNEL_WINDOW_FULL: add new error code
    In order to signal that the requested operation can not succeed
    because the receiving window had been exhausted, the error code
    LIBSSH2_ERROR_BUFFER_TOO_SMALL has been reused but I have found
    that in certain context it may be ambigous.
    
    This patch introduces a new error code,
    LIBSSH2_ERROR_CHANNEL_WINDOW_FULL, exclusive to signal that condition.

--- include/libssh2.h
+++ include/libssh2.h
@@ -482,6 +482,7 @@ typedef struct _LIBSSH2_POLLFD {
 #define LIBSSH2_ERROR_ENCRYPT                   -44
 #define LIBSSH2_ERROR_BAD_SOCKET                -45
 #define LIBSSH2_ERROR_KNOWN_HOSTS               -46
+#define LIBSSH2_ERROR_CHANNEL_WINDOW_FULL       -47
 
 /* this is a define to provide the old (<= 1.2.7) name */
 #define LIBSSH2_ERROR_BANNER_NONE LIBSSH2_ERROR_BANNER_RECV
