61214bc26107d5970a7cda45331bd97815f58142
Michael Buckley <michael@buckleyisms.com>
2018-12-04 12:44:26 -0800

    Sanitize padding_length - _libssh2_transport_read(). https://libssh2.org/CVE-2019-3861.html
    This prevents an underflow resulting in a potential out-of-bounds read if a server sends a too-large padding_length, possibly with malicious intent.

--- src/transport.c
+++ src/transport.c
@@ -444,6 +444,10 @@ int _libssh2_transport_read(LIBSSH2_SESSION * session)
             else if(p->packet_length > LIBSSH2_PACKET_MAXPAYLOAD) {
                 return LIBSSH2_ERROR_OUT_OF_BOUNDARY;
             }
+            else if ( p->padding_length > p->packet_length - 1 ) {
+                return LIBSSH2_ERROR_DECRYPT;
+            }
+
 
             /* total_num is the number of bytes following the initial
                (5 bytes) packet length and padding length fields */
