fb43746aaad4f45835a9ca7b7bb5074bbad1c383
Daniel Stenberg <daniel@haxx.se>
2019-03-15 01:08:58 +0100

    pem: //-comments are not allowed

--- src/pem.c
+++ src/pem.c
@@ -439,7 +439,7 @@ _libssh2_openssh_pem_parse_data(LIBSSH2_SESSION * session,
 
     if((passphrase == NULL || strlen((const char*)passphrase) == 0) &&
         strcmp((const char*)ciphername, "none") != 0) {
-        // passphrase required
+        /* passphrase required */
         ret = LIBSSH2_ERROR_KEYFILE_AUTH_FAILED;
         goto out;
     }
