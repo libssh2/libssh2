libssh2/libssh2

commit 4d66f6762ca3fc45d9c74f175edd991c5bb41499

session_disconnect: clear state
If authentication is started but not completed before the application
gives up and instead wants to shut down the session, the '->state' field
might still be set and thus effectively dead-lock session_disconnect.

This happens because both _libssh2_transport_send() and
_libssh2_transport_read() refuse to do anything as long as state is set
without the LIBSSH2_STATE_KEX_ACTIVE bit.

Reported in curl bug https://github.com/curl/curl/issues/3650

Closes #310

src/session.c
@@ -1161,7 +1161,7 @@ libssh2_session_disconnect_ex(LIBSSH2_SESSION *session, int reason,
                               const char *desc, const char *lang)
 {
     int rc;
-
+    session->state = 0;
     BLOCK_ADJUST(rc, session,
                  session_disconnect(session, reason, desc, lang));
 

commit 05ee5ea50b9f9ed2d7778266557e2648e6a050db

session_disconnect: don't zero state, just clear the right bit
If we clear the entire field, the freeing of data in session_free() is
skipped. Instead just clear the bit that risk making the code get stuck
in the transport functions.

Regression from 4d66f6762ca3fc45d9.

Reported-by: dimmaq on github
Fixes #338

src/session.c
@@ -1168,7 +1168,7 @@ libssh2_session_disconnect_ex(LIBSSH2_SESSION *session, int reason,
                               const char *desc, const char *lang)
 {
     int rc;
-    session->state = 0;
+    session->state &= ~LIBSSH2_STATE_EXCHANGING_KEYS;
     BLOCK_ADJUST(rc, session,
                  session_disconnect(session, reason, desc, lang));
 