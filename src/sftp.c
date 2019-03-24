38818082c1e28cb26d6c41a8a80d7280ad3bb546
Daniel Stenberg <daniel@haxx.se>
2019-03-24 23:10:03 +0100

    sftp: repair mtime from e1ead35e475
    A regression from e1ead35e4759 broke the SFTP mtime logic in
    sftp_bin2attr
    
    Also simplified the _libssh2_get_u32/u64 functions slightly.
    
    Closes #342

--- src/sftp.c
+++ src/sftp.c
@@ -719,7 +719,7 @@ sftp_bin2attr(LIBSSH2_SFTP_ATTRIBUTES *attrs, const unsigned char *p,
             return LIBSSH2_ERROR_BUFFER_TOO_SMALL;
         }
         attrs->atime = atime;
-        attrs->atime = mtime;
+        attrs->mtime = mtime;
     }
 
     return (buf.dataptr - buf.data);
