728a1ff81c95be8b67ce0be9523e9078ec514cd1
Viktor Szakats <commit@vsz.me>
2023-03-02 20:08:26 +0000

    _libssh2_*_safe_free(): simplify silencing warnings

--- src/wincng.c
+++ src/wincng.c
@@ -432,16 +432,14 @@ _libssh2_wincng_random(void *buf, int len)
 static void
 _libssh2_wincng_safe_free(void *buf, int len)
 {
-#ifndef LIBSSH2_CLEAR_MEMORY
-    (void)len;
-#endif
-
     if(!buf)
         return;
 
 #ifdef LIBSSH2_CLEAR_MEMORY
     if(len > 0)
         SecureZeroMemory(buf, len);
+#else
+    (void)len;
 #endif
 
     free(buf);
