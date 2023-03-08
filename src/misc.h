libssh2/libssh2

commit a0e424a51c27cc27af611ba20d134f9a9ae35273

src: enable clear memory on all platforms
- convert `_libssh2_explicit_zero()` to macro. This allows inlining
  where supported (e.g. `SecureZeroMemory()`).

- replace `SecureZeroMemory()` (in `wincng.c`) and
  `LIBSSH2_CLEAR_MEMORY`-guarded `memset()` (in `os400qc3.c`) with
  `_libssh2_explicit_zero()` macro.

- delete `LIBSSH2_CLEAR_MEMORY` guards, which enables secure-zeroing
  universally.

- add `LIBSSH2_NO_CLEAR_MEMORY` option to disable secure-zeroing.

- while here, delete double/triple inclusion of `misc.h`.
  `libssh2_priv.h` included it already.

Closes #810

src/misc.h
@@ -38,6 +38,23 @@
  * OF SUCH DAMAGE.
  */
 
+#ifdef LIBSSH2_NO_CLEAR_MEMORY
+#define _libssh2_explicit_zero(buf, size) do { \
+                                              (void)buf; \
+                                              (void)size; \
+                                          } while(0)
+#else
+#ifdef WIN32
+#define _libssh2_explicit_zero(buf, size) SecureZeroMemory(buf, size)
+#elif defined(HAVE_MEMSET_S)
+#define _libssh2_explicit_zero(buf, size) (void)memset_s(buf, size, 0, size)
+#else
+#define LIBSSH2_MEMZERO
+void _libssh2_memzero(void *buf, size_t size);
+#define _libssh2_explicit_zero(buf, size) _libssh2_memzero(buf, size)
+#endif
+#endif
+
 struct list_head {
     struct list_node *last;
     struct list_node *first;
@@ -89,7 +106,6 @@ void _libssh2_store_bignum2_bytes(unsigned char **buf,
                                   const unsigned char *bytes,
                                   size_t len);
 void *_libssh2_calloc(LIBSSH2_SESSION *session, size_t size);
-void _libssh2_explicit_zero(void *buf, size_t size);
 
 struct string_buf* _libssh2_string_buf_new(LIBSSH2_SESSION *session);
 void _libssh2_string_buf_free(LIBSSH2_SESSION *session,

acinclude.m4
@@ -456,7 +456,6 @@ m4_case([$1],
     AC_DEFINE(LIBSSH2_MBEDTLS, 1, [Use $1])
     LIBS="$LIBS -lmbedcrypto"
     found_crypto="$1"
-    support_clear_memory=yes
   ])
 ],
 

configure.ac
@@ -83,7 +83,6 @@ AC_SYS_LARGEFILE
 
 found_crypto=none
 found_crypto_str=""
-support_clear_memory=no
 crypto_errors=""
 
 m4_set_add([crypto_backends], [openssl])
@@ -176,25 +175,11 @@ fi
 AC_ARG_ENABLE(clear-memory,
   AC_HELP_STRING([--disable-clear-memory],[Disable clearing of memory before being freed]),
   [CLEAR_MEMORY=$enableval])
-if test "$CLEAR_MEMORY" != "no"; then
-  if test "$support_clear_memory" = "yes"; then
-    AC_DEFINE(LIBSSH2_CLEAR_MEMORY, 1, [Enable clearing of memory before being freed])
-    enable_clear_memory=yes
-  else
-    if test "$CLEAR_MEMORY" = "yes"; then
-      AC_MSG_ERROR([secure clearing/zeroing of memory is not supported by the selected crypto backend])
-    else
-      AC_MSG_WARN([secure clearing/zeroing of memory is not supported by the selected crypto backend])
-    fi
-    enable_clear_memory=unsupported
-  fi
+if test "$CLEAR_MEMORY" = "no"; then
+  AC_DEFINE(LIBSSH2_NO_CLEAR_MEMORY, 1, [Disable clearing of memory before being freed])
+  enable_clear_memory=no
 else
-  if test "$support_clear_memory" = "yes"; then
-    enable_clear_memory=no
-  else
-    AC_MSG_WARN([secure clearing/zeroing of memory is not supported by the selected crypto backend])
-    enable_clear_memory=unsupported
-  fi
+  enable_clear_memory=yes
 fi
 
 dnl ************************************************************

os400/libssh2_config.h
@@ -185,9 +185,6 @@
 /* to make a symbol visible */
 #undef LIBSSH2_API
 
-/* Enable clearing of memory before being freed */
-#define LIBSSH2_CLEAR_MEMORY 1
-
 /* Enable "none" cipher -- NOT RECOMMENDED */
 #undef LIBSSH2_CRYPT_NONE
 

src/CMakeLists.txt
@@ -236,9 +236,9 @@ target_include_directories(libssh2
 ## Options
 
 option(CLEAR_MEMORY "Enable clearing of memory before being freed" ON)
-if(CLEAR_MEMORY)
-    add_definitions(-DLIBSSH2_CLEAR_MEMORY)
-endif(CLEAR_MEMORY)
+if(NOT CLEAR_MEMORY)
+    add_definitions(-DLIBSSH2_NO_CLEAR_MEMORY)
+endif()
 
 add_feature_info("Shared library" BUILD_SHARED_LIBS
   "creating libssh2 as a shared library (.so/.dll)")

src/agent.c
@@ -39,7 +39,6 @@
 
 #include "libssh2_priv.h"
 #include "agent.h"
-#include "misc.h"
 #include <errno.h>
 #ifdef HAVE_SYS_UN_H
 #include <sys/un.h>

src/agent.h
@@ -40,7 +40,6 @@
  */
 
 #include "libssh2_priv.h"
-#include "misc.h"
 #include "session.h"
 #ifdef WIN32
 #include <stdlib.h>

src/agent_win.c
@@ -39,7 +39,6 @@
 
 #include "libssh2_priv.h"
 #include "agent.h"
-#include "misc.h"
 #include <errno.h>
 #ifdef HAVE_SYS_UN_H
 #include <sys/un.h>

commit 3b3707d7b3d7f73d207eddd4103ea5c5a51d9195

fix compiling with LIBSSH2_NO_CLEAR_MEMORY and OpenSSL
Regression from a0e424a51c27cc27af611ba20d134f9a9ae35273

Fixes #824

src/misc.h
@@ -40,8 +40,8 @@
 
 #ifdef LIBSSH2_NO_CLEAR_MEMORY
 #define _libssh2_explicit_zero(buf, size) do { \
-                                              (void)buf; \
-                                              (void)size; \
+                                              (void)(buf); \
+                                              (void)(size); \
                                           } while(0)
 #else
 #ifdef WIN32