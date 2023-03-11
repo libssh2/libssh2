libssh2/libssh2

commit 2e2812dde8c1fc9b48eca592823770ab2e601f7a

openssl.h: enable ed25519 for LibreSSL 3.7.0 (#778)
This brings LibreSSL libssh2 builds on par with OpenSSL.

src/openssl.h
@@ -122,8 +122,9 @@
 # define LIBSSH2_ECDSA 1
 #endif
 
-#if OPENSSL_VERSION_NUMBER >= 0x10101000L && \
-!defined(LIBRESSL_VERSION_NUMBER)
+#if (OPENSSL_VERSION_NUMBER >= 0x10101000L && \
+    !defined(LIBRESSL_VERSION_NUMBER)) || \
+    LIBRESSL_VERSION_NUMBER >= 0x3070000fL
 # define LIBSSH2_ED25519 1
 #else
 # define LIBSSH2_ED25519 0

commit 5e9fe1dfb667c4552e4dadf1278ff26c0749c617

openssl: fix possible compiler warning in macro condition
Building with wolfSSL or pre-OpenSSL v1.1.1 could trigger it.

```
../src/openssl.h:130:5: warning: 'LIBRESSL_VERSION_NUMBER' is not defined, evaluates to 0 [-Wundef]
    LIBRESSL_VERSION_NUMBER >= 0x3070000fL
    ^
```

Regression from 2e2812dde8c1fc9b48eca592823770ab2e601f7a

src/openssl.h
@@ -127,7 +127,8 @@
 
 #if (OPENSSL_VERSION_NUMBER >= 0x10101000L && \
     !defined(LIBRESSL_VERSION_NUMBER)) || \
-    LIBRESSL_VERSION_NUMBER >= 0x3070000fL
+    (defined(LIBRESSL_VERSION_NUMBER) && \
+    LIBRESSL_VERSION_NUMBER >= 0x3070000fL)
 # define LIBSSH2_ED25519 1
 #else
 # define LIBSSH2_ED25519 0