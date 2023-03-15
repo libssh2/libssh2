12e194b8707294a6b456f51d0cfd3a7137854efe
Viktor Szakats <commit@vsz.me>
2023-03-15 16:28:35 +0000

    sftp.c: fix compiler warning
    ```
    src/sftp.c:889:..: warning C4244: '+=': conversion from 'ssize_t' to 'int', possible loss of data
    ```

--- src/libssh2_priv.h
+++ src/libssh2_priv.h
@@ -850,8 +850,8 @@ struct _LIBSSH2_SESSION
     LIBSSH2_CHANNEL *sftpInit_channel;
     unsigned char sftpInit_buffer[9];   /* sftp_header(5){excludes request_id}
                                            + version_id(4) */
-    int sftpInit_sent; /* number of bytes from the buffer that have been
-                          sent */
+    size_t sftpInit_sent; /* number of bytes from the buffer that have been
+                             sent */
 
     /* State variables used in libssh2_scp_recv() / libssh_scp_recv2() */
     libssh2_nonblocking_states scpRecv_state;
