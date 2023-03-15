840cd928e7d78e80c3fbf1657b61ee30972a4b19
Viktor Szakats <commit@vsz.me>
2023-03-15 03:12:49 +0000

    mbedtls.c: fix warnings (CHECKME)

--- src/mbedtls.h
+++ src/mbedtls.h
@@ -124,7 +124,7 @@
 #define libssh2_hmac_cleanup(pctx) \
   mbedtls_md_free(pctx)
 #define libssh2_hmac_update(ctx, data, datalen) \
-  mbedtls_md_hmac_update(&ctx, (unsigned char *) data, datalen)
+  mbedtls_md_hmac_update(&ctx, (const unsigned char *) data, datalen)
 #define libssh2_hmac_final(ctx, hash) \
   mbedtls_md_hmac_finish(&ctx, hash)
 
@@ -152,7 +152,7 @@
 #define libssh2_sha1_init(pctx) \
   _libssh2_mbedtls_hash_init(pctx, MBEDTLS_MD_SHA1, NULL, 0)
 #define libssh2_sha1_update(ctx, data, datalen) \
-  mbedtls_md_update(&ctx, (unsigned char *) data, datalen)
+  mbedtls_md_update(&ctx, (const unsigned char *) data, datalen)
 #define libssh2_sha1_final(ctx, hash) \
   _libssh2_mbedtls_hash_final(&ctx, hash)
 #define libssh2_sha1(data, datalen, hash) \
@@ -168,7 +168,7 @@
 #define libssh2_sha256_init(pctx) \
   _libssh2_mbedtls_hash_init(pctx, MBEDTLS_MD_SHA256, NULL, 0)
 #define libssh2_sha256_update(ctx, data, datalen) \
-  mbedtls_md_update(&ctx, (unsigned char *) data, datalen)
+  mbedtls_md_update(&ctx, (const unsigned char *) data, datalen)
 #define libssh2_sha256_final(ctx, hash) \
   _libssh2_mbedtls_hash_final(&ctx, hash)
 #define libssh2_sha256(data, datalen, hash) \
@@ -185,7 +185,7 @@
 #define libssh2_sha384_init(pctx) \
   _libssh2_mbedtls_hash_init(pctx, MBEDTLS_MD_SHA384, NULL, 0)
 #define libssh2_sha384_update(ctx, data, datalen) \
-  mbedtls_md_update(&ctx, (unsigned char *) data, datalen)
+  mbedtls_md_update(&ctx, (const unsigned char *) data, datalen)
 #define libssh2_sha384_final(ctx, hash) \
   _libssh2_mbedtls_hash_final(&ctx, hash)
 #define libssh2_sha384(data, datalen, hash) \
@@ -202,7 +202,7 @@
 #define libssh2_sha512_init(pctx) \
   _libssh2_mbedtls_hash_init(pctx, MBEDTLS_MD_SHA512, NULL, 0)
 #define libssh2_sha512_update(ctx, data, datalen) \
-  mbedtls_md_update(&ctx, (unsigned char *) data, datalen)
+  mbedtls_md_update(&ctx, (const unsigned char *) data, datalen)
 #define libssh2_sha512_final(ctx, hash) \
   _libssh2_mbedtls_hash_final(&ctx, hash)
 #define libssh2_sha512(data, datalen, hash) \
@@ -219,7 +219,7 @@
 #define libssh2_md5_init(pctx) \
   _libssh2_mbedtls_hash_init(pctx, MBEDTLS_MD_MD5, NULL, 0)
 #define libssh2_md5_update(ctx, data, datalen) \
-  mbedtls_md_update(&ctx, (unsigned char *) data, datalen)
+  mbedtls_md_update(&ctx, (const unsigned char *) data, datalen)
 #define libssh2_md5_final(ctx, hash) \
   _libssh2_mbedtls_hash_final(&ctx, hash)
 #define libssh2_md5(data, datalen, hash) \
