9d62430dd4b5278fbd146617f02373601b92e019
Viktor Szakats <commit@vsz.me>
2023-03-16 04:19:54 +0000

    make ed25519 key_len size_t

--- src/crypto.h
+++ src/crypto.h
@@ -246,7 +246,7 @@ int
 _libssh2_ed25519_new_public(libssh2_ed25519_ctx **ed_ctx,
                             LIBSSH2_SESSION *session,
                             const unsigned char *raw_pub_key,
-                            const uint8_t key_len);
+                            const size_t key_len);
 
 int
 _libssh2_ed25519_sign(libssh2_ed25519_ctx *ctx, LIBSSH2_SESSION *session,
