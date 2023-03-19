f3b157e67d254f59ac4a5dac1fb6703d8b5a9b25
Viktor Szakats <commit@vsz.me>
2023-03-19 01:04:22 +0000

    make bcrypt_pbkdf() static
    While here, make the low-level `bcrypt_pbkdf()` function static, and
    avoid namespace pollution.

--- src/bcrypt_pbkdf.c
+++ src/bcrypt_pbkdf.c
@@ -100,7 +100,7 @@ bcrypt_hash(uint8_t *sha2pass, uint8_t *sha2salt, uint8_t *out)
     _libssh2_explicit_zero(&state, sizeof(state));
 }
 
-int
+static int
 bcrypt_pbkdf(const char *pass, size_t passlen, const uint8_t *salt,
              size_t saltlen,
              uint8_t *key, size_t keylen, unsigned int rounds)
