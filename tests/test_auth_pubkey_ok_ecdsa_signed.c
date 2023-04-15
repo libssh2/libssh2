09d258c0147d81b0aaf88dd9f11ec3365c5c35c4
Viktor Szakats <commit@vsz.me>
2023-04-15 19:55:05 +0000

    limit "signed" key tests to openssl
    Also assume wolfSSL, but this hasn't been tested.

--- tests/test_auth_pubkey_ok_ecdsa_signed.c
+++ tests/test_auth_pubkey_ok_ecdsa_signed.c
@@ -2,7 +2,8 @@
 
 int test(LIBSSH2_SESSION *session)
 {
-#if LIBSSH2_ECDSA
+#if LIBSSH2_ECDSA && \
+    (defined(LIBSSH2_OPENSSL) || defined(LIBSSH2_WOLFSSL))
     /* configured in Dockerfile */
     return test_auth_pubkey(session, 0,
                             "libssh2",
--- tests/test_auth_pubkey_ok_rsa_signed.c
+++ tests/test_auth_pubkey_ok_rsa_signed.c
@@ -2,10 +2,15 @@
 
 int test(LIBSSH2_SESSION *session)
 {
+#if defined(LIBSSH2_OPENSSL) || defined(LIBSSH2_WOLFSSL)
     /* configured in Dockerfile */
     return test_auth_pubkey(session, 0,
                             "libssh2",
                             NULL,
                             "key_rsa_signed-cert.pub",
                             "key_rsa_signed");
+#else
+    (void)session;
+    return 0;
+#endif
 }
