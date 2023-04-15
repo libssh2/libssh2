bf3b44ca1856a7f7d4d55638b2f7a14685af7876
Viktor Szakats <commit@vsz.me>
2023-04-15 02:50:41 +0000

    cmake: use a single build rule for all tests
    - use the complete filename of test sources in the input list.
    
    - build all tests with the ability to access libssh2 internals.
    
      This is necessary for `test_keyboard_interactive_auth_info_request`
      now and might be necessary for others in the future, e.g. to avoid
      the depreacted public base64 decoding API.
    
    - move `test_keyboard_interactive_auth_info_request` into the main
      test build loop.
    
    - move `simple` into the main test build loop too.
    
    - build `ssh2` also in static mode.
    
    - cleanup the way we detect and enable gcov.
    
    - fix indentation.
    
    Closes #967

--- cmake/max_warnings.cmake
+++ cmake/max_warnings.cmake
@@ -167,11 +167,11 @@ elseif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_C_COMPILER_I
       endif()
       if(NOT CMAKE_C_COMPILER_VERSION VERSION_LESS 7.0)
         list(APPEND WPICKY_ENABLE
-        -Walloc-zero                       #             gcc  7.0
-        -Wduplicated-branches              #             gcc  7.0
-        -Wformat-overflow=2                #             gcc  7.0
-        -Wformat-truncation=1              #             gcc  7.0
-        -Wrestrict                         #             gcc  7.0
+          -Walloc-zero                     #             gcc  7.0
+          -Wduplicated-branches            #             gcc  7.0
+          -Wformat-overflow=2              #             gcc  7.0
+          -Wformat-truncation=1            #             gcc  7.0
+          -Wrestrict                       #             gcc  7.0
         )
       endif()
       if(NOT CMAKE_C_COMPILER_VERSION VERSION_LESS 10.0)
