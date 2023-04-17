6cd7a296c29d5d57972033bf95dd332d99b83523
Viktor Szakats <commit@vsz.me>
2023-04-17 22:42:39 +0000

    delete macOS _DARWIN_USE_64_BIT_INODE from OS/400 config header

--- os400/libssh2_config.h
+++ os400/libssh2_config.h
@@ -196,11 +196,6 @@
    significant byte first (like Motorola and SPARC, unlike Intel). */
 #define WORDS_BIGENDIAN 1
 
-/* Enable large inode numbers on Mac OS X 10.5.  */
-#ifndef _DARWIN_USE_64_BIT_INODE
-# define _DARWIN_USE_64_BIT_INODE 1
-#endif
-
 /* Number of bits in a file offset, on hosts where this is settable. */
 #undef _FILE_OFFSET_BITS
 
