222e0818218ea58f8218e22b6a3a79378a93389b
Viktor Szakats <commit@vsz.me>
2023-04-18 10:45:09 +0000

    make RSA-SHA1 tests conditional

--- tests/test_auth_pubkey_ok_rsa_signed.c
+++ tests/test_auth_pubkey_ok_rsa_signed.c
@@ -2,7 +2,8 @@
 
 int test(LIBSSH2_SESSION *session)
 {
-#if defined(LIBSSH2_OPENSSL) || defined(LIBSSH2_WOLFSSL)
+#if LIBSSH2_RSA_SHA1 && \
+    (defined(LIBSSH2_OPENSSL) || defined(LIBSSH2_WOLFSSL))
     /* set in Dockerfile */
     return test_auth_pubkey(session, 0,
                             "libssh2",
