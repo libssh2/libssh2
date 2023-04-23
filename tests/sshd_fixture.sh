e613d727336b6edbc30021027651dc9f440c99fa
Viktor Szakats <commit@vsz.me>
2023-04-23 14:15:44 +0000

    sync sshd_fixture.sh with Dockerfile sshd_config
    This fixed broken test_ssh2 tests run with libgcrypt.
    
    It seems this crypto backend relies on some obsolete algos.

--- tests/sshd_fixture.sh
+++ tests/sshd_fixture.sh
@@ -33,6 +33,12 @@ chmod go-rwx "$srcdir"/openssh_server/ssh_host*
   -o 'Port 4711' \
   -o 'Protocol 2' \
   -o "AuthorizedKeysFile ${PUBKEY}" \
+  -o 'UsePrivilegeSeparation no' \
+  -o 'TrustedUserCAKeys /etc/ssh/ca_main.pub' \
+  -o 'HostKeyAlgorithms +ssh-rsa' \
+  -o 'PubkeyAcceptedKeyTypes +ssh-rsa,ssh-dss' \
+  -o 'MACs +hmac-sha1,hmac-sha1-96,hmac-sha2-256,hmac-sha2-512,hmac-md5,hmac-md5-96,umac-64@openssh.com,umac-128@openssh.com,hmac-sha1-etm@openssh.com,hmac-sha1-96-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-md5-etm@openssh.com,hmac-md5-96-etm@openssh.com,umac-64-etm@openssh.com,umac-128-etm@openssh.com' \
+  -o 'Ciphers +3des-cbc,aes128-cbc,aes192-cbc,aes256-cbc,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com,chacha20-poly1305@openssh.com' \
   -o 'StrictModes no' \
   -D \
   $libssh2_sshd_params &
