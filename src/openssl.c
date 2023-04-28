libssh2/libssh2

commit 097c8f0dae558643d43051947a1c35b65e1c5761

openssl: fix misleading error message if wrong passphrase (#789)
Fixes #608

src/openssl.c
@@ -3849,6 +3849,7 @@ _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
     BIO*      bp;
     EVP_PKEY* pk;
     int       pktype;
+    unsigned long sslError;
 
     _libssh2_debug((session,
                    LIBSSH2_TRACE_AUTH,
@@ -3865,6 +3866,7 @@ _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
                               "computing public key");
     BIO_reset(bp);
     pk = PEM_read_bio_PrivateKey(bp, NULL, NULL, (void *)passphrase);
+    sslError = ERR_get_error();
     BIO_free(bp);
 
     if(!pk) {
@@ -3877,9 +3879,22 @@ _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
                                                      privatekeydata,
                                                      privatekeydata_len,
                                             (unsigned const char *)passphrase);
-        if(st)
-            return st;
-        return 0;
+        if(st == 0)
+            return 0;
+
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L /* OpenSSL 3.0.0 */
+        if((ERR_GET_LIB(sslError) == ERR_LIB_PEM &&
+            ERR_GET_REASON(sslError) == PEM_R_BAD_DECRYPT) ||
+           (ERR_GET_LIB(sslError) == ERR_LIB_PROV &&
+            ERR_GET_REASON(sslError) == EVP_R_BAD_DECRYPT))
+            return _libssh2_error(session, LIBSSH2_ERROR_KEYFILE_AUTH_FAILED,
+                                  "Wrong passphrase for private key");
+#endif
+        return _libssh2_error(session,
+                              LIBSSH2_ERROR_FILE,
+                              "Unable to extract public key "
+                              "from private key file: "
+                              "Unsupported private key file format");
     }
 
 #ifdef HAVE_OPAQUE_STRUCTS

src/pem.c
@@ -764,7 +764,8 @@ _libssh2_openssh_pem_parse_memory(LIBSSH2_SESSION * session,
 
         if(off >= filedata_len)
             return _libssh2_error(session, LIBSSH2_ERROR_PROTO,
-                                  "Error parsing PEM: offset out of bounds");
+                                  "Error parsing PEM: "
+                                  "OpenSSH header not found");
 
         if(readline_memory(line, LINE_SIZE, filedata, filedata_len, &off)) {
             return -1;

commit d93ccf4901ef26443707d341553994715414e207

ci: add macOS CI jobs + fix issues revealed
Add macOS CI jobs, both cmake and autotools for all supported crypto
backends (except BoringSSL), with debug, zlib enabled. Without running
tests. It also introduces OpenSSL 1.1 into the CI with a non-MSVC
compiler.

Credits to curl's `macos.yml`, that I used as a base.

Fix these issues uncovered by the new tests:

- openssl: fix warning when built with wolfSSL, or OpenSSL 1.1 and
  earlier. CI missed it because apparently the only OpenSSL 1.1 test
  we had used MSVC, which did not complain.

  ```
  ../src/openssl.c:3852:19: error: variable 'sslError' set but not used [-Werror,-Wunused-but-set-variable]
      unsigned long sslError;
                    ^
  ```

  Regression from 097c8f0dae558643d43051947a1c35b65e1c5761

- pem: add hack to build without MD5 crypto-backend support.

  The Homebrew wolfSSL build comes with MD5 support disabled. We can
  expect this becoming the norm. FIPS also requires MD5 disabled.

  We deleted the same hack from `hostkey.c` a month ago:
  ad6aae302aaec84afbfacf0c1dfdc446d46eaf21

  A better fix would be to guard the MD5 logic with our `LIBSSH2_MD5`
  macro.

  ```
  pem.c:214:32: error: use of undeclared identifier 'MD5_DIGEST_LENGTH'; did you mean 'SHA_DIGEST_LENGTH'?
          unsigned char secret[2*MD5_DIGEST_LENGTH];
                                 ^~~~~~~~~~~~~~~~~
                                 SHA_DIGEST_LENGTH
  ```

  Regression from 386e012292a96fcf0dc6861588397845df0aba2c

- `configure.ac`: add crypto libs late.

  Fix it by adding crypto libs to `LIBS` at the end of the configuration
  process.

  Otherwise `configure` links crypto libs while doing feature tests,
  which can cause unwanted detections. For example LibreSSL publishes
  the function `explicit_bzero()`, which masks the system alternative,
  e.g. `memset_s()` on macOS. Then when trying to compile libssh2, its
  declaration is missing:

  ```

.github/workflows/ci.yml
@@ -2,9 +2,9 @@ name: CI
 
 on:
   push:
-    branches: [ master ]
+    branches: [master]
   pull_request:
-    branches: [ master ]
+    branches: [master]
 
 jobs:
   check_style:
@@ -68,7 +68,7 @@ jobs:
         if: ${{ matrix.b == 'configure' }}
         run: |
           autoreconf -fi
-          ./configure --enable-debug --enable-werror
+          ./configure --enable-werror --enable-debug
           make -j3
           make check VERBOSE=1
       - name: Build with CMake
@@ -103,16 +103,16 @@ jobs:
       - uses: actions/checkout@v3
       - uses: msys2/setup-msys2@v2
         with:
-          msystem: ${{matrix.sys}}
+          msystem: ${{ matrix.sys }}
           install: >-
-            mingw-w64-${{matrix.env}}-cc
-            mingw-w64-${{matrix.env}}-autotools
-            mingw-w64-${{matrix.env}}-openssl
+            mingw-w64-${{ matrix.env }}-cc
+            mingw-w64-${{ matrix.env }}-autotools
+            mingw-w64-${{ matrix.env }}-openssl
       - name: Building libssh2
         shell: msys2 {0}
         run: |
           autoreconf -fi
-          ./configure --enable-debug --enable-static --disable-shared --with-crypto=openssl
+          ./configure --enable-werror --enable-debug --enable-static --disable-shared --with-crypto=openssl
           make -j3
           make check VERBOSE=1
 
@@ -133,11 +133,11 @@ jobs:
       fail-fast: false
     steps:
       - uses: actions/checkout@v3
-      - name: Configure with CMake
+      - name: 'cmake generate'
         shell: bash
         run: |
-          archgen=${{matrix.arch}}; [ "${archgen}" = 'x86' ] && archgen='Win32'
-          if [ "${{matrix.plat}}" = 'uwp' ]; then
+          archgen=${{ matrix.arch }}; [ "${archgen}" = 'x86' ] && archgen='Win32'
+          if [ "${{ matrix.plat }}" = 'uwp' ]; then
             system='WindowsStore'
             options='-DCMAKE_SYSTEM_VERSION=10.0'
           else
@@ -147,12 +147,77 @@ jobs:
             -DCMAKE_SYSTEM_NAME=${system} \
             -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake \
             -DCMAKE_GENERATOR_PLATFORM=${archgen} \
-            -DVCPKG_TARGET_TRIPLET=${{matrix.arch}}-${{matrix.plat}} \
+            -DVCPKG_TARGET_TRIPLET=${{ matrix.arch }}-${{ matrix.plat }} \
             -DENABLE_WERROR=ON \
-            -DENABLE_DEBUG_LOGGING=${{matrix.log}} \
-            -DBUILD_SHARED_LIBS=${{matrix.shared}} \
-            -DCRYPTO_BACKEND=${{matrix.crypto}} \
-            -DENABLE_ZLIB_COMPRESSION=${{matrix.zlib}}
-      - name: Build with CMake
+            -DENABLE_DEBUG_LOGGING=${{ matrix.log }} \
+            -DBUILD_SHARED_LIBS=${{ matrix.shared }} \
+            -DCRYPTO_BACKEND=${{ matrix.crypto }} \
+            -DENABLE_ZLIB_COMPRESSION=${{ matrix.zlib }}
+      - name: 'cmake build'
         run: |
           cmake --build bld --config Release --target package
+
+  build_macos:
+    name: macOS (${{ matrix.build }}, ${{ matrix.crypto.name }})
+    runs-on: macos-latest
+    timeout-minutes: 30
+    strategy:
+      fail-fast: false
+      matrix:
+        build: [autotools, cmake]
+        crypto:
+          - name: OpenSSL 3
+            install: openssl
+            configure: --with-crypto=openssl --with-libssl-prefix=/usr/local/opt/openssl
+            cmake: -DCRYPTO_BACKEND=OpenSSL -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl
+          - name: OpenSSL 1.1
+            install: openssl@1.1
+            configure: --with-crypto=openssl --with-libssl-prefix=/usr/local/opt/openssl@1.1
+            cmake: -DCRYPTO_BACKEND=OpenSSL -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1
+          - name: LibreSSL
+            install: libressl
+            configure: --with-crypto=openssl --with-libssl-prefix=/usr/local/opt/libressl
+            cmake: -DCRYPTO_BACKEND=OpenSSL -DOPENSSL_ROOT_DIR=/usr/local/opt/libressl
+          - name: wolfSSL
+            install: wolfssl
+            configure: --with-crypto=wolfssl --with-libwolfssl-prefix=/usr/local/opt/wolfssl
+            cmake: -DCRYPTO_BACKEND=wolfSSL
+          - name: libgcrypt
+            install: libgcrypt
+            configure: --with-crypto=libgcrypt --with-libgcrypt-prefix=/usr/local/opt/libgcrypt
+            cmake: -DCRYPTO_BACKEND=Libgcrypt
+          - name: mbedTLS
+            install: mbedtls
+            configure: --with-crypto=mbedtls --with-libmbedcrypto-prefix=/usr/local/opt/mbedtls
+            cmake: -DCRYPTO_BACKEND=mbedTLS -DMBEDCRYPTO_LIBRARY=/usr/local/opt/mbedtls/lib/libmbedcrypto.a -DMBEDTLS_LIBRARY=/usr/local/opt/mbedtls/lib/libmbedtls.a -DMBEDX509_LIBRARY=/usr/local/opt/mbedtls/lib/libmbedx509.a -DMBEDTLS_INCLUDE_DIR=/usr/local/opt/mbedtls/include
+    steps:
+      - name: 'brew install'
+        run: |
+          brew install automake ${{ matrix.crypto.install }}
+
+      - uses: actions/checkout@v3
+
+      - name: 'autotools autoreconf'
+        if: ${{ matrix.build == 'autotools' }}
+        run: autoreconf -fi
+      - name: 'autotools configure'
+        if: ${{ matrix.build == 'autotools' }}
+        run: |
+          ./configure ${{ matrix.crypto.configure }} \
+          --enable-werror \
+          --enable-debug \
+          --with-libz
+      - name: 'autotools make'
+        if: ${{ matrix.build == 'autotools' }}
+        run: make
+
+      - name: 'cmake generate'
+        if: ${{ matrix.build == 'cmake' }}
+        run: |
+          cmake . -B bld ${{ matrix.crypto.cmake }} \
+            -DENABLE_WERROR=ON \
+            -DENABLE_DEBUG_LOGGING=ON \
+            -DENABLE_ZLIB_COMPRESSION=ON
+      - name: 'cmake build'
+        if: ${{ matrix.build == 'cmake' }}
+        run: cmake --build bld

appveyor.yml
@@ -24,25 +24,27 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+# https://www.appveyor.com/docs/windows-images-software/
+
 environment:
   CONFIGURATION: "Release"
   FIXTURE_XFER_COUNT: 35020
   matrix:
-    - job_name: "VS2022, OpenSSL3, x64, Server 2019"
+    - job_name: "VS2022, OpenSSL 3, x64, Server 2019"
       APPVEYOR_BUILD_WORKER_IMAGE: "Visual Studio 2022"
       GENERATOR: "Visual Studio 17 2022"
       PLATFORM: "x64"
       BUILD_SHARED_LIBS: "ON"
       CRYPTO_BACKEND: "OpenSSL"
 
-    - job_name: "VS2015, OpenSSL, x86, Server 2016"
+    - job_name: "VS2015, OpenSSL 1.1, x86, Server 2016"
       APPVEYOR_BUILD_WORKER_IMAGE: "Visual Studio 2017"
       GENERATOR: "Visual Studio 14 2015"
       PLATFORM: "x86"
       BUILD_SHARED_LIBS: "ON"
       CRYPTO_BACKEND: "OpenSSL"
 
-    - job_name: "VS2015, OpenSSL, x64, Server 2012 R2, Logging"
+    - job_name: "VS2015, OpenSSL 1.1, x64, Server 2012 R2, Logging"
       APPVEYOR_BUILD_WORKER_IMAGE: "Visual Studio 2015"
       GENERATOR: "Visual Studio 14 2015"
       PLATFORM: "x64"
@@ -50,21 +52,21 @@ environment:
       CRYPTO_BACKEND: "OpenSSL"
       ENABLE_DEBUG_LOGGING: "ON"
 
-    - job_name: "VS2013, OpenSSL, x64, Server 2012 R2"
+    - job_name: "VS2013, OpenSSL 1.1, x64, Server 2012 R2"
       APPVEYOR_BUILD_WORKER_IMAGE: "Visual Studio 2015"
       GENERATOR: "Visual Studio 12 2013"
       PLATFORM: "x64"
       BUILD_SHARED_LIBS: "ON"
       CRYPTO_BACKEND: "OpenSSL"
 
-    - job_name: "VS2013, OpenSSL, x86, Server 2012 R2"
+    - job_name: "VS2013, OpenSSL 1.1, x86, Server 2012 R2"
       APPVEYOR_BUILD_WORKER_IMAGE: "Visual Studio 2015"
       GENERATOR: "Visual Studio 12 2013"
       PLATFORM: "x86"
       BUILD_SHARED_LIBS: "ON"
       CRYPTO_BACKEND: "OpenSSL"
 
-    - job_name: "VS2013, OpenSSL, x64, Build-only, Static-only"
+    - job_name: "VS2013, OpenSSL 1.0.2, x64, Build-only, Static-only"
       APPVEYOR_BUILD_WORKER_IMAGE: "Visual Studio 2015"
       GENERATOR: "Visual Studio 12 2013"
       PLATFORM: "x64"

configure.ac
@@ -117,18 +117,6 @@ else
   test "$found_crypto_str" = "" && found_crypto_str="$found_crypto"
 fi
 
-if   test "$found_crypto" = "openssl"; then
-  LIBS="${LIBS} ${LTLIBSSL}"
-elif test "$found_crypto" = "wolfssl"; then
-  LIBS="${LIBS} ${LTLIBWOLFSSL}"
-elif test "$found_crypto" = "libgcrypt"; then
-  LIBS="${LIBS} ${LTLIBGCRYPT}"
-elif test "$found_crypto" = "wincng"; then
-  LIBS="${LIBS} ${LTLIBBCRYPT}"
-elif test "$found_crypto" = "mbedtls"; then
-  LIBS="${LIBS} ${LTLIBMBEDCRYPTO}"
-fi
-
 # libz
 
 AC_ARG_WITH([libz],
@@ -376,6 +364,19 @@ AM_CONDITIONAL([HOST_WINDOWS], [test "x$host_windows" = "xyes"])
 
 # Configure parameters
 LIBSSH2_CHECK_OPTION_WERROR
+
+# Append crypto lib
+if   test "$found_crypto" = "openssl"; then
+  LIBS="${LIBS} ${LTLIBSSL}"
+elif test "$found_crypto" = "wolfssl"; then
+  LIBS="${LIBS} ${LTLIBWOLFSSL}"
+elif test "$found_crypto" = "libgcrypt"; then
+  LIBS="${LIBS} ${LTLIBGCRYPT}"
+elif test "$found_crypto" = "wincng"; then
+  LIBS="${LIBS} ${LTLIBBCRYPT}"
+elif test "$found_crypto" = "mbedtls"; then
+  LIBS="${LIBS} ${LTLIBMBEDCRYPTO}"
+fi
 
 AC_CONFIG_FILES([Makefile
                  src/Makefile

src/openssl.c
@@ -3835,6 +3835,10 @@ read_openssh_private_key_from_memory(void **key_ctx, LIBSSH2_SESSION *session,
                                                    passphrase);
 }
 
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+#define HAVE_SSLERROR_BAD_DECRYPT
+#endif
+
 int
 _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
                                 unsigned char **method,
@@ -3849,7 +3853,9 @@ _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
     BIO*      bp;
     EVP_PKEY* pk;
     int       pktype;
+#ifdef HAVE_SSLERROR_BAD_DECRYPT
     unsigned long sslError;
+#endif
 
     _libssh2_debug((session,
                    LIBSSH2_TRACE_AUTH,
@@ -3866,7 +3872,9 @@ _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
                               "computing public key");
     BIO_reset(bp);
     pk = PEM_read_bio_PrivateKey(bp, NULL, NULL, (void *)passphrase);
+#ifdef HAVE_SSLERROR_BAD_DECRYPT
     sslError = ERR_get_error();
+#endif
     BIO_free(bp);
 
     if(!pk) {
@@ -3882,7 +3890,7 @@ _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
         if(st == 0)
             return 0;
 
-#if OPENSSL_VERSION_NUMBER >= 0x30000000L /* OpenSSL 3.0.0 */
+#ifdef HAVE_SSLERROR_BAD_DECRYPT
         if((ERR_GET_LIB(sslError) == ERR_LIB_PEM &&
             ERR_GET_REASON(sslError) == PEM_R_BAD_DECRYPT) ||
            (ERR_GET_LIB(sslError) == ERR_LIB_PROV &&

src/pem.c
@@ -104,6 +104,12 @@ static unsigned char hex_decode(char digit)
         ((digit >= 'A') ? (0xA + (digit - 'A')) : (digit - '0'));
 }
 
+/* Hack to fix builds with crypto backends with MD5 support disabled.
+   FIXME: Honor our LIBSSH2_MD5 macro for MD5-dependent logic. */
+#ifdef OPENSSL_NO_MD5
+#define MD5_DIGEST_LENGTH 16
+#endif
+
 int
 _libssh2_pem_parse(LIBSSH2_SESSION * session,
                    const char *headerbegin,

tests/CMakeLists.txt
@@ -79,7 +79,7 @@ foreach(test ${DOCKER_TESTS} ${STANDALONE_TESTS} ${SSHD_TESTS})
   if(NOT TARGET ${test})
     add_executable(${test} ${test}.c)
     target_compile_definitions(${test} PRIVATE "${CRYPTO_BACKEND_DEFINE}")
-    target_include_directories(${test} PRIVATE "${CMAKE_CURRENT_BINARY_DIR}/../src" ../src "${CRYPTO_BACKEND_INCLUDE_DIR}")
+    target_include_directories(${test} PRIVATE "${CMAKE_CURRENT_BINARY_DIR}/../src" ../src ../include "${CRYPTO_BACKEND_INCLUDE_DIR}")
 
     # build a single test with gcov
     if(GCOV_PATH AND test STREQUAL test_auth_keyboard_info_request)

tests/session_fixture.h
@@ -41,7 +41,6 @@
 #define LIBSSH2_TESTS
 
 #include "libssh2_priv.h"
-#include <libssh2.h>
 
 LIBSSH2_SESSION *start_session_fixture(int *skipped);
 void stop_session_fixture(void);