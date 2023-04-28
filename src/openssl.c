libssh2/libssh2

commit 097c8f0dae558643d43051947a1c35b65e1c5761

openssl: fix misleading error message if wrong passphrase (#789)
Fixes #608

src/openssl.c
@@ -3849,6 +3849,7 @@ _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
     BIO*      bp;
     EVP_PKEY* pk;
     int       pktype;
+    unsigned long sslError;
 
     _libssh2_debug((session,
                    LIBSSH2_TRACE_AUTH,
@@ -3865,6 +3866,7 @@ _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
                               "computing public key");
     BIO_reset(bp);
     pk = PEM_read_bio_PrivateKey(bp, NULL, NULL, (void *)passphrase);
+    sslError = ERR_get_error();
     BIO_free(bp);
 
     if(!pk) {
@@ -3877,9 +3879,22 @@ _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
                                                      privatekeydata,
                                                      privatekeydata_len,
                                             (unsigned const char *)passphrase);
-        if(st)
-            return st;
-        return 0;
+        if(st == 0)
+            return 0;
+
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L /* OpenSSL 3.0.0 */
+        if((ERR_GET_LIB(sslError) == ERR_LIB_PEM &&
+            ERR_GET_REASON(sslError) == PEM_R_BAD_DECRYPT) ||
+           (ERR_GET_LIB(sslError) == ERR_LIB_PROV &&
+            ERR_GET_REASON(sslError) == EVP_R_BAD_DECRYPT))
+            return _libssh2_error(session, LIBSSH2_ERROR_KEYFILE_AUTH_FAILED,
+                                  "Wrong passphrase for private key");
+#endif
+        return _libssh2_error(session,
+                              LIBSSH2_ERROR_FILE,
+                              "Unable to extract public key "
+                              "from private key file: "
+                              "Unsupported private key file format");
     }
 
 #ifdef HAVE_OPAQUE_STRUCTS

src/pem.c
@@ -764,7 +764,8 @@ _libssh2_openssh_pem_parse_memory(LIBSSH2_SESSION * session,
 
         if(off >= filedata_len)
             return _libssh2_error(session, LIBSSH2_ERROR_PROTO,
-                                  "Error parsing PEM: offset out of bounds");
+                                  "Error parsing PEM: "
+                                  "OpenSSH header not found");
 
         if(readline_memory(line, LINE_SIZE, filedata, filedata_len, &off)) {
             return -1;

commit 0c04acc3dca8320a222c9f176784c10dae53b439

fix openssl regression
when built with wolfSSL. Not sure why it didn't come up
with OpenSSL 1.x.

```
../src/openssl.c:3852:19: error: variable 'sslError' set but not used [-Werror,-Wunused-but-set-variable]
    unsigned long sslError;
                  ^
```

Regression from 097c8f0dae558643d43051947a1c35b65e1c5761

src/openssl.c
@@ -3835,6 +3835,10 @@ read_openssh_private_key_from_memory(void **key_ctx, LIBSSH2_SESSION *session,
                                                    passphrase);
 }
 
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+#define HAVE_SSLERROR_BAD_DECRYPT
+#endif
+
 int
 _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
                                 unsigned char **method,
@@ -3849,7 +3853,9 @@ _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
     BIO*      bp;
     EVP_PKEY* pk;
     int       pktype;
+#ifdef HAVE_SSLERROR_BAD_DECRYPT
     unsigned long sslError;
+#endif
 
     _libssh2_debug((session,
                    LIBSSH2_TRACE_AUTH,
@@ -3866,7 +3872,9 @@ _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
                               "computing public key");
     BIO_reset(bp);
     pk = PEM_read_bio_PrivateKey(bp, NULL, NULL, (void *)passphrase);
+#ifdef HAVE_SSLERROR_BAD_DECRYPT
     sslError = ERR_get_error();
+#endif
     BIO_free(bp);
 
     if(!pk) {
@@ -3882,7 +3890,7 @@ _libssh2_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
         if(st == 0)
             return 0;
 
-#if OPENSSL_VERSION_NUMBER >= 0x30000000L /* OpenSSL 3.0.0 */
+#ifdef HAVE_SSLERROR_BAD_DECRYPT
         if((ERR_GET_LIB(sslError) == ERR_LIB_PEM &&
             ERR_GET_REASON(sslError) == PEM_R_BAD_DECRYPT) ||
            (ERR_GET_LIB(sslError) == ERR_LIB_PROV &&