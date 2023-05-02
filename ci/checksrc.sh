c2aa8f1c111fbd50adefa9ba6f550bb835324d54
Viktor Szakats <commit@vsz.me>
2023-05-02 11:47:01 +0000

    checksrc.sh: make it work regardless of current directory
    Also silence a shellcheck warning.

--- ci/checksrc.sh
+++ ci/checksrc.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+cd "$(dirname "$0")/.."
+
 FILES="src/*.[ch] include/*.h example/*.c tests/*.[ch]"
 WHITELIST="-Wsrc/libssh2_config.h"
 
 # shellcheck disable=SC2086
+# shellcheck disable=SC2248
 perl ./ci/checksrc.pl -i4 -m79 \
   -ASNPRINTF \
   -ACOPYRIGHT \
