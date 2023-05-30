0d6b748dc8439ec801a7fad137964c04984e169e
Viktor Szakats <commit@vsz.me>
2023-05-30 12:19:30 +0000

    checksrc: switch to dot file
    Closes #1051

--- ci/checksrc.sh
+++ ci/checksrc.sh
@@ -4,14 +4,6 @@ set -e
 
 cd "$(dirname "$0")/.."
 
-FILES="src/*.[ch] include/*.h example/*.c tests/*.[ch]"
-WHITELIST="-Wsrc/libssh2_config.h"
-
-# shellcheck disable=SC2086
-# shellcheck disable=SC2248
 perl ./ci/checksrc.pl -i4 -m79 \
-  -ASNPRINTF \
-  -ACOPYRIGHT \
-  -AFOPENMODE \
-  -ATYPEDEFSTRUCT \
-  $WHITELIST $FILES
+  -Wsrc/libssh2_config.h \
+  src/*.[ch] include/*.h example/*.c tests/*.[ch]
