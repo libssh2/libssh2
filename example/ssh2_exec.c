libssh2/libssh2

commit b13936bd6a89993cd3bf4a18317ca5bd84bb08d7

example, tests: address compiler warnings
Fix or silence all C compiler warnings discovered with (or without)
`PICKY_COMPILER=ON` (in CMake). This means all warnings showing up in
CI (gcc, clang, MSVS 2013/2015), in local tests on macOS (clang 14) and
Windows cross-builds using gcc (12) and llvm/clang (14/15).

Also fix the expression `nread -= nread` in `sftp_RW_nonblock.c`.

Cherry-picked from: #846
Closes #861

example/ssh2_exec.c
@@ -74,7 +74,7 @@ static int waitsocket(libssh2_socket_t socket_fd, LIBSSH2_SESSION *session)
     if(dir & LIBSSH2_SESSION_BLOCK_OUTBOUND)
         writefd = &fd;
 
-    rc = select(socket_fd + 1, readfd, writefd, NULL, &timeout);
+    rc = select((int)(socket_fd + 1), readfd, writefd, NULL, &timeout);
 
     return rc;
 }
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
     const char *commandline = "uptime";
     const char *username    = "user";
     const char *password    = "password";
-    unsigned long hostaddr;
+    uint32_t hostaddr;
     libssh2_socket_t sock;
     struct sockaddr_in sin;
     const char *fingerprint;
@@ -94,7 +94,7 @@ int main(int argc, char *argv[])
     int rc;
     int exitcode;
     char *exitsignal = (char *)"none";
-    int bytecount = 0;
+    ssize_t bytecount = 0;
     size_t len;
     LIBSSH2_KNOWNHOSTS *nh;
     int type;
@@ -259,25 +259,27 @@ int main(int argc, char *argv[])
         exit(1);
     }
     for(;;) {
+        ssize_t nread;
         /* loop until we block */
         do {
             char buffer[0x4000];
-            rc = libssh2_channel_read(channel, buffer, sizeof(buffer) );
-            if(rc > 0) {
-                int i;
-                bytecount += rc;
+            nread = libssh2_channel_read(channel, buffer, sizeof(buffer));
+            if(nread > 0) {
+                ssize_t i;
+                bytecount += nread;
                 fprintf(stderr, "We read:\n");
-                for(i = 0; i < rc; ++i)
+                for(i = 0; i < nread; ++i)
                     fputc(buffer[i], stderr);
                 fprintf(stderr, "\n");
             }
             else {
-                if(rc != LIBSSH2_ERROR_EAGAIN)
+                if(nread != LIBSSH2_ERROR_EAGAIN)
                     /* no need to output this for the EAGAIN case */
-                    fprintf(stderr, "libssh2_channel_read returned %d\n", rc);
+                    fprintf(stderr, "libssh2_channel_read returned %d\n",
+                            (int)nread);
             }
         }
-        while(rc > 0);
+        while(nread > 0);
 
         /* this is due to blocking that would occur otherwise so we loop on
            this condition */
@@ -300,7 +302,8 @@ int main(int argc, char *argv[])
     if(exitsignal)
         fprintf(stderr, "\nGot signal: %s\n", exitsignal);
     else
-        fprintf(stderr, "\nEXIT: %d bytecount: %d\n", exitcode, bytecount);
+        fprintf(stderr, "\nEXIT: %d bytecount: %d\n",
+                exitcode, (int)bytecount);
 
     libssh2_channel_free(channel);
     channel = NULL;

example/direct_tcpip.c
@@ -11,6 +11,8 @@
 #include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#define recv(s, b, l, f)  recv((s), (b), (int)(l), (f))
+#define send(s, b, l, f)  send((s), (b), (int)(l), (f))
 #else
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -161,7 +163,8 @@ int main(int argc, char *argv[])
     fprintf(stderr, "\n");
 
     /* check what authentication methods are available */
-    userauthlist = libssh2_userauth_list(session, username, strlen(username));
+    userauthlist = libssh2_userauth_list(session, username,
+                                         (unsigned int)strlen(username));
     fprintf(stderr, "Authentication methods: %s\n", userauthlist);
     if(strstr(userauthlist, "password"))
         auth |= AUTH_PASSWORD;
@@ -261,7 +264,7 @@ int main(int argc, char *argv[])
         FD_SET(forwardsock, &fds);
         tv.tv_sec = 0;
         tv.tv_usec = 100000;
-        rc = select(forwardsock + 1, &fds, NULL, NULL, &tv);
+        rc = select((int)(forwardsock + 1), &fds, NULL, NULL, &tv);
         if(-1 == rc) {
             perror("select");
             goto shutdown;
@@ -279,15 +282,17 @@ int main(int argc, char *argv[])
             }
             wr = 0;
             while(wr < len) {
-                i = libssh2_channel_write(channel, buf + wr, len - wr);
-                if(LIBSSH2_ERROR_EAGAIN == i) {
+                ssize_t nwritten = libssh2_channel_write(channel,
+                                                         buf + wr, len - wr);
+                if(nwritten == LIBSSH2_ERROR_EAGAIN) {
                     continue;
                 }
-                if(i < 0) {
-                    fprintf(stderr, "libssh2_channel_write: %d\n", i);
+                if(nwritten < 0) {
+                    fprintf(stderr, "libssh2_channel_write: %d\n",
+                            (int)nwritten);
                     goto shutdown;
                 }
-                wr += i;
+                wr += nwritten;
             }
         }
         for(;;) {
@@ -300,12 +305,12 @@ int main(int argc, char *argv[])
             }
             wr = 0;
             while(wr < len) {
-                i = send(forwardsock, buf + wr, len - wr, 0);
-                if(i <= 0) {
+                ssize_t nsent = send(forwardsock, buf + wr, len - wr, 0);
+                if(nsent <= 0) {
                     perror("write");
                     goto shutdown;
                 }
-                wr += i;
+                wr += nsent;
             }
             if(libssh2_channel_eof(channel)) {
                 fprintf(stderr, "The server at %s:%d disconnected!\n",

example/scp.c
@@ -36,9 +36,13 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#ifdef WIN32
+#define write(f, b, c)  write((f), (b), (unsigned int)(c))
+#endif
+
 int main(int argc, char *argv[])
 {
-    unsigned long hostaddr;
+    uint32_t hostaddr;
     libssh2_socket_t sock;
     int i, auth_pw = 1;
     struct sockaddr_in sin;
@@ -159,20 +163,22 @@ int main(int argc, char *argv[])
     while(got < fileinfo.st_size) {
         char mem[1024];
         int amount = sizeof(mem);
+        ssize_t nread;
 
-        if((fileinfo.st_size -got) < amount) {
-            amount = (int)(fileinfo.st_size -got);
+        if((fileinfo.st_size - got) < amount) {
+            amount = (int)(fileinfo.st_size - got);
         }
 
-        rc = libssh2_channel_read(channel, mem, amount);
-        if(rc > 0) {
-            write(1, mem, rc);
+        nread = libssh2_channel_read(channel, mem, amount);
+        if(nread > 0) {
+            write(1, mem, nread);
         }
-        else if(rc < 0) {
-            fprintf(stderr, "libssh2_channel_read() failed: %d\n", rc);
+        else if(nread < 0) {
+            fprintf(stderr, "libssh2_channel_read() failed: %d\n",
+                    (int)nread);
             break;
         }
-        got += rc;
+        got += nread;
     }
 
     libssh2_channel_free(channel);

example/scp_nonblock.c
@@ -44,6 +44,10 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#ifdef WIN32
+#define write(f, b, c)  write((f), (b), (unsigned int)(c))
+#endif
+
 #ifdef HAVE_GETTIMEOFDAY
 /* diff in ms */
 static long tvdiff(struct timeval newer, struct timeval older)
@@ -78,14 +82,14 @@ static int waitsocket(libssh2_socket_t socket_fd, LIBSSH2_SESSION *session)
     if(dir & LIBSSH2_SESSION_BLOCK_OUTBOUND)
         writefd = &fd;
 
-    rc = select(socket_fd + 1, readfd, writefd, NULL, &timeout);
+    rc = select((int)(socket_fd + 1), readfd, writefd, NULL, &timeout);
 
     return rc;
 }
 
 int main(int argc, char *argv[])
 {
-    unsigned long hostaddr;
+    uint32_t hostaddr;
     libssh2_socket_t sock;
     int i, auth_pw = 1;
     struct sockaddr_in sin;
@@ -238,6 +242,7 @@ int main(int argc, char *argv[])
 
     while(got < fileinfo.st_size) {
         char mem[1024*24];
+        ssize_t nread;
 
         do {
             int amount = sizeof(mem);
@@ -247,15 +252,15 @@ int main(int argc, char *argv[])
             }
 
             /* loop until we block */
-            rc = libssh2_channel_read(channel, mem, amount);
-            if(rc > 0) {
-                write(1, mem, rc);
-                got += rc;
-                total += rc;
+            nread = libssh2_channel_read(channel, mem, amount);
+            if(nread > 0) {
+                write(1, mem, nread);
+                got += nread;
+                total += nread;
             }
-        } while(rc > 0);
+        } while(nread > 0);
 
-        if((rc == LIBSSH2_ERROR_EAGAIN) && (got < fileinfo.st_size)) {
+        if((nread == LIBSSH2_ERROR_EAGAIN) && (got < fileinfo.st_size)) {
             /* this is due to blocking that would occur otherwise
             so we loop on this condition */
 
@@ -271,8 +276,7 @@ int main(int argc, char *argv[])
 
     time_ms = tvdiff(end, start);
     fprintf(stderr, "Got %ld bytes in %ld ms = %.1f bytes/sec spin: %d\n",
-            (long)total,
-            time_ms, total/(time_ms/1000.0), spin);
+            (long)total, time_ms, (double)total/(time_ms/1000.0), spin);
 #else
     fprintf(stderr, "Got %ld bytes spin: %d\n", (long)total, spin);
 #endif

example/scp_write.c
@@ -42,7 +42,7 @@
 
 int main(int argc, char *argv[])
 {
-    unsigned long hostaddr;
+    uint32_t hostaddr;
     libssh2_socket_t sock;
     int i, auth_pw = 1;
     struct sockaddr_in sin;
@@ -171,7 +171,7 @@ int main(int argc, char *argv[])
 
     /* Send a file via scp. The mode parameter must only have permissions! */
     channel = libssh2_scp_send(session, scppath, fileinfo.st_mode & 0777,
-                               (unsigned long)fileinfo.st_size);
+                               (size_t)fileinfo.st_size);
 
     if(!channel) {
         char *errmsg;
@@ -191,16 +191,17 @@ int main(int argc, char *argv[])
         ptr = mem;
 
         do {
+            ssize_t nwritten;
             /* write the same data over and over, until error or completion */
-            rc = libssh2_channel_write(channel, ptr, nread);
-            if(rc < 0) {
-                fprintf(stderr, "ERROR %d\n", rc);
+            nwritten = libssh2_channel_write(channel, ptr, nread);
+            if(nwritten < 0) {
+                fprintf(stderr, "ERROR %d\n", (int)nwritten);
                 break;
             }
             else {
-                /* rc indicates how many bytes were written this time */
-                ptr += rc;
-                nread -= rc;
+                /* nwritten indicates how many bytes were written this time */
+                ptr += nwritten;
+                nread -= nwritten;
             }
         } while(nread);
 

example/scp_write_nonblock.c
@@ -65,14 +65,14 @@ static int waitsocket(libssh2_socket_t socket_fd, LIBSSH2_SESSION *session)
     if(dir & LIBSSH2_SESSION_BLOCK_OUTBOUND)
         writefd = &fd;
 
-    rc = select(socket_fd + 1, readfd, writefd, NULL, &timeout);
+    rc = select((int)(socket_fd + 1), readfd, writefd, NULL, &timeout);
 
     return rc;
 }
 
 int main(int argc, char *argv[])
 {
-    unsigned long hostaddr;
+    uint32_t hostaddr;
     libssh2_socket_t sock;
     int i, auth_pw = 1;
     struct sockaddr_in sin;
@@ -90,7 +90,7 @@ int main(int argc, char *argv[])
     char *ptr;
     struct stat fileinfo;
     time_t start;
-    long total = 0;
+    libssh2_struct_stat_size total = 0;
     int duration;
     size_t prev;
 
@@ -210,7 +210,7 @@ int main(int argc, char *argv[])
     /* Send a file via scp. The mode parameter must only have permissions! */
     do {
         channel = libssh2_scp_send(session, scppath, fileinfo.st_mode & 0777,
-                                   (unsigned long)fileinfo.st_size);
+                                   (size_t)fileinfo.st_size);
 
         if((!channel) && (libssh2_session_last_errno(session) !=
                           LIBSSH2_ERROR_EAGAIN)) {
@@ -236,22 +236,23 @@ int main(int argc, char *argv[])
 
         prev = 0;
         do {
-            while((rc = libssh2_channel_write(channel, ptr, nread)) ==
+            ssize_t nwritten;
+            while((nwritten = libssh2_channel_write(channel, ptr, nread)) ==
                   LIBSSH2_ERROR_EAGAIN) {
                 waitsocket(sock, session);
                 prev = 0;
             }
-            if(rc < 0) {
-                fprintf(stderr, "ERROR %d total %ld / %d prev %d\n", rc,
-                        total, (int)nread, (int)prev);
+            if(nwritten < 0) {
+                fprintf(stderr, "ERROR %d total %ld / %d prev %d\n",
+                        (int)nwritten, (long)total, (int)nread, (int)prev);
                 break;
             }
             else {
                 prev = nread;
 
-                /* rc indicates how many bytes were written this time */
-                nread -= rc;
-                ptr += rc;
+                /* nwritten indicates how many bytes were written this time */
+                nread -= nwritten;
+                ptr += nwritten;
             }
         } while(nread);
     } while(!nread); /* only continue if nread was drained */
@@ -259,7 +260,7 @@ int main(int argc, char *argv[])
     duration = (int)(time(NULL)-start);
 
     fprintf(stderr, "%ld bytes in %d seconds makes %.1f bytes/sec\n",
-           total, duration, total/(double)duration);
+           (long)total, duration, (double)total / duration);
 
     fprintf(stderr, "Sending EOF\n");
     while(libssh2_channel_send_eof(channel) == LIBSSH2_ERROR_EAGAIN);

example/sftp.c
@@ -46,6 +46,10 @@
 #pragma warning(disable:4127)
 #endif
 
+#ifdef WIN32
+#define write(f, b, c)  write((f), (b), (unsigned int)(c))
+#endif
+
 const char *keyfile1 = "~/.ssh/id_rsa.pub";
 const char *keyfile2 = "~/.ssh/id_rsa";
 const char *username = "username";
@@ -90,7 +94,7 @@ static void kbd_callback(const char *name, int name_len,
         buf[n] = 0;
 
         responses[i].text = strdup(buf);
-        responses[i].length = n;
+        responses[i].length = (unsigned int)n;
 
         fprintf(stderr, "Response %d from user is '", i);
         fwrite(responses[i].text, 1, responses[i].length, stderr);
@@ -104,7 +108,7 @@ static void kbd_callback(const char *name, int name_len,
 
 int main(int argc, char *argv[])
 {
-    unsigned long hostaddr;
+    uint32_t hostaddr;
     libssh2_socket_t sock;
     int i, auth_pw = 0;
     struct sockaddr_in sin;
@@ -195,7 +199,8 @@ int main(int argc, char *argv[])
     fprintf(stderr, "\n");
 
     /* check what authentication methods are available */
-    userauthlist = libssh2_userauth_list(session, username, strlen(username));
+    userauthlist = libssh2_userauth_list(session, username,
+                                         (unsigned int)strlen(username));
     fprintf(stderr, "Authentication methods: %s\n", userauthlist);
     if(strstr(userauthlist, "password") != NULL) {
         auth_pw |= 1;
@@ -277,12 +282,13 @@ int main(int argc, char *argv[])
     fprintf(stderr, "libssh2_sftp_open() is done, now receive data!\n");
     do {
         char mem[1024];
+        ssize_t nread;
 
         /* loop until we fail */
         fprintf(stderr, "libssh2_sftp_read()!\n");
-        rc = libssh2_sftp_read(sftp_handle, mem, sizeof(mem));
-        if(rc > 0) {
-            write(1, mem, rc);
+        nread = libssh2_sftp_read(sftp_handle, mem, sizeof(mem));
+        if(nread > 0) {
+            write(1, mem, nread);
         }
         else {
             break;

example/sftp_RW_nonblock.c
@@ -43,6 +43,10 @@
 #pragma warning(disable:4127)
 #endif
 
+#ifdef WIN32
+#define write(f, b, c)  write((f), (b), (unsigned int)(c))
+#endif
+
 #define STORAGE "/tmp/sftp-storage" /* this is the local file name this
                                        example uses to store the downloaded
                                        file in */
@@ -72,7 +76,7 @@ static int waitsocket(libssh2_socket_t socket_fd, LIBSSH2_SESSION *session)
     if(dir & LIBSSH2_SESSION_BLOCK_OUTBOUND)
         writefd = &fd;
 
-    rc = select(socket_fd + 1, readfd, writefd, NULL, &timeout);
+    rc = select((int)(socket_fd + 1), readfd, writefd, NULL, &timeout);
 
     return rc;
 }
@@ -237,21 +241,22 @@ int main(int argc, char *argv[])
 
     fprintf(stderr, "libssh2_sftp_open() is done, now receive data!\n");
     do {
+        ssize_t nread;
         do {
             /* read in a loop until we block */
-            rc = libssh2_sftp_read(sftp_handle, mem, sizeof(mem));
+            nread = libssh2_sftp_read(sftp_handle, mem, sizeof(mem));
             fprintf(stderr, "libssh2_sftp_read returned %d\n",
-                    rc);
+                    (int)nread);
 
-            if(rc > 0) {
+            if(nread > 0) {
                 /* write to stderr */
-                write(2, mem, rc);
+                write(2, mem, nread);
                 /* write to temporary storage area */
-                fwrite(mem, rc, 1, tempstorage);
+                fwrite(mem, nread, 1, tempstorage);
             }
-        } while(rc > 0);
+        } while(nread > 0);
 
-        if(rc != LIBSSH2_ERROR_EAGAIN) {
+        if(nread != LIBSSH2_ERROR_EAGAIN) {
             /* error or end of file */
             break;
         }
@@ -265,7 +270,7 @@ int main(int argc, char *argv[])
         FD_SET(sock, &fd2);
 
         /* wait for readable or writeable */
-        rc = select(sock + 1, &fd, &fd2, NULL, &timeout);
+        rc = select((int)(sock + 1), &fd, &fd2, NULL, &timeout);
         if(rc <= 0) {
             /* negative is error
                0 is timeout */
@@ -296,6 +301,7 @@ int main(int argc, char *argv[])
         size_t nread;
         char *ptr;
         do {
+            ssize_t nwritten;
             nread = fread(mem, 1, sizeof(mem), tempstorage);
             if(nread <= 0) {
                 /* end of file */
@@ -305,13 +311,13 @@ int main(int argc, char *argv[])
 
             do {
                 /* write data in a loop until we block */
-                rc = libssh2_sftp_write(sftp_handle, ptr,
-                                        nread);
-                ptr += rc;
-                nread -= nread;
-            } while(rc >= 0);
+                nwritten = libssh2_sftp_write(sftp_handle, ptr,
+                                              nread);
+                ptr += nwritten;
+                nread -= nwritten;
+            } while(nwritten >= 0);
 
-            if(rc != LIBSSH2_ERROR_EAGAIN) {
+            if(nwritten != LIBSSH2_ERROR_EAGAIN) {
                 /* error or end of file */
                 break;
             }
@@ -325,7 +331,7 @@ int main(int argc, char *argv[])
             FD_SET(sock, &fd2);
 
             /* wait for readable or writeable */
-            rc = select(sock + 1, &fd, &fd2, NULL, &timeout);
+            rc = select((int)(sock + 1), &fd, &fd2, NULL, &timeout);
             if(rc <= 0) {
                 /* negative is error
                    0 is timeout */

commit ba487d7a49bd67e07b6ae74487509c75712d103a

example: fix regression in ssh2_exec.c
Regression from b13936bd6a89993cd3bf4a18317ca5bd84bb08d7 #861 #846.
Update a variable name missed in above.

Reported-by: PewPewPew
Fixes #1105
Closes #1106

example/ssh2_exec.c
@@ -260,7 +260,7 @@ int main(int argc, char *argv[])
 
         /* this is due to blocking that would occur otherwise so we loop on
            this condition */
-        if(rc == LIBSSH2_ERROR_EAGAIN) {
+        if(nread == LIBSSH2_ERROR_EAGAIN) {
             waitsocket(sock, session);
         }
         else