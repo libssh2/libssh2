aeaefaf6cc5f832dd24d7c6301ea6b5ae6e11496
Viktor Szakats <commit@vsz.me>
2023-08-17 23:02:35 +0000

    cmake: add `ExternalProject` integration test
    - via `ExternalProject_Add()`:
      https://cmake.org/cmake/help/latest/module/ExternalProject.html
      (as documented in `docs/INSTALL_CMAKE.md`)
    
    - also make `FetchContent` fetch from local repo instead of live master.
    
    Closes #1171

--- tests/cmake/test.sh
+++ tests/cmake/test.sh
@@ -8,7 +8,9 @@ set -u
 
 cd "$(dirname "$0")"
 
-rm -rf bld-fetchcontent; cmake -B bld-fetchcontent -DTEST_INTEGRATION_MODE=FetchContent
+rm -rf bld-fetchcontent; cmake -B bld-fetchcontent -DTEST_INTEGRATION_MODE=FetchContent \
+  -DFROM_GIT_REPO="${PWD}/../.." \
+  -DFROM_GIT_TAG="$(git rev-parse HEAD)"
 make -C bld-fetchcontent -j3
 
 rm -rf libssh2; ln -s ../.. libssh2
@@ -17,5 +19,12 @@ make -C bld-add_subdirectory -j3
 
 rm -rf bld-libssh2; cmake ../.. -B bld-libssh2
 make -C bld-libssh2 -j3 DESTDIR=pkg install
-rm -rf bld-find_package; cmake -B bld-find_package -DTEST_INTEGRATION_MODE=find_package -DCMAKE_PREFIX_PATH="${PWD}/bld-libssh2/pkg/usr/local/lib/cmake/libssh2"
+rm -rf bld-find_package; cmake -B bld-find_package -DTEST_INTEGRATION_MODE=find_package \
+  -DCMAKE_PREFIX_PATH="${PWD}/bld-libssh2/pkg/usr/local/lib/cmake/libssh2"
 make -C bld-find_package -j3
+
+(cd ../..; git archive --format=tar HEAD) | gzip > source.tar.gz
+rm -rf bld-externalproject; cmake -B bld-externalproject -DTEST_INTEGRATION_MODE=ExternalProject \
+  -DFROM_ARCHIVE="${PWD}/source.tar.gz" \
+  -DFROM_HASH="$(openssl dgst -sha256 source.tar.gz | grep -a -i -o -E '[0-9a-f]{64}$')"
+make -C bld-externalproject -j3
