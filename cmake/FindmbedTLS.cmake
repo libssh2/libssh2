8a8c17df8e3a64023b0ed5c6c127fd635f5b7b03
Viktor Szakats <commit@vsz.me>
2023-09-28 13:09:36 +0000

    cmake: improve mbedTLS detection
    - libssh2 needs the crypto lib only, stop dealing with the rest.
    
    - simplify logic.
    
    - drop hard-wired toolchain specific options that broke with e.g. MSVC.
    
      Reported by: AR Visions
      Fixes #1191
    
    - add mbedTLS version detection for recent releases.
    
    - merge custom detection results into a single line.
    
    Used the curl mbedTLS detection logic for ideas:
    https://github.com/curl/curl/blob/a8c773845f4fdbfb09b08a6ec4b656c812568995/CMake/FindMbedTLS.cmake
    
    Closes #xxxx

--- cmake/FindmbedTLS.cmake
+++ cmake/FindmbedTLS.cmake
@@ -9,47 +9,31 @@
 #  MBEDTLS_INCLUDE_DIR - the mbedTLS include directory
 #  MBEDTLS_LIBRARY_DIR - the mbedTLS library directory
 #  MBEDTLS_LIBRARIES - Link these to use mbedTLS
-#  MBEDTLS_LIBRARY - path to mbedTLS library
-#  MBEDX509_LIBRARY - path to mbedTLS X.509 library
 #  MBEDCRYPTO_LIBRARY - path to mbedTLS Crypto library
 
 find_path(MBEDTLS_INCLUDE_DIR mbedtls/version.h)
+find_library(MBEDCRYPTO_LIBRARY mbedcrypto libmbedcrypto)
 
 if(MBEDTLS_INCLUDE_DIR AND MBEDTLS_LIBRARIES)
   # Already in cache, be silent
   set(MBEDTLS_FIND_QUIETLY TRUE)
 endif()
 
-find_library(MBEDTLS_LIBRARY NAMES mbedtls libmbedtls libmbedx509)
-find_library(MBEDX509_LIBRARY NAMES mbedx509 libmbedx509)
-find_library(MBEDCRYPTO_LIBRARY NAMES mbedcrypto libmbedcrypto)
+set(MBEDTLS_LIBRARIES "${MBEDCRYPTO_LIBRARY}")
 
-if(MBEDTLS_INCLUDE_DIR AND MBEDTLS_LIBRARY AND MBEDX509_LIBRARY AND MBEDCRYPTO_LIBRARY)
-  set(MBEDTLS_FOUND TRUE)
-endif()
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(mbedTLS DEFAULT_MSG
+  MBEDTLS_INCLUDE_DIR MBEDCRYPTO_LIBRARY)
 
 if(MBEDTLS_FOUND)
-  # split mbedTLS into -L and -l linker options, so we can set them for pkg-config
-  get_filename_component(MBEDTLS_LIBRARY_DIR ${MBEDTLS_LIBRARY} PATH)
-  get_filename_component(MBEDTLS_LIBRARY_FILE ${MBEDTLS_LIBRARY} NAME_WE)
-  get_filename_component(MBEDX509_LIBRARY_FILE ${MBEDX509_LIBRARY} NAME_WE)
-  get_filename_component(MBEDCRYPTO_LIBRARY_FILE ${MBEDCRYPTO_LIBRARY} NAME_WE)
-  string(REGEX REPLACE "^lib" "" MBEDTLS_LIBRARY_FILE ${MBEDTLS_LIBRARY_FILE})
-  string(REGEX REPLACE "^lib" "" MBEDX509_LIBRARY_FILE ${MBEDX509_LIBRARY_FILE})
-  string(REGEX REPLACE "^lib" "" MBEDCRYPTO_LIBRARY_FILE ${MBEDCRYPTO_LIBRARY_FILE})
-  set(MBEDTLS_LIBRARIES "-L${MBEDTLS_LIBRARY_DIR} -l${MBEDTLS_LIBRARY_FILE} -l${MBEDX509_LIBRARY_FILE} -l${MBEDCRYPTO_LIBRARY_FILE}")
-
   if(NOT MBEDTLS_FIND_QUIETLY)
-    message(STATUS "Found mbedTLS:")
-    file(READ ${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h MBEDTLSCONTENT)
-    string(REGEX MATCH "MBEDTLS_VERSION_STRING +\"[0-9|.]+\"" MBEDTLSMATCH ${MBEDTLSCONTENT})
+    file(READ "${MBEDTLS_INCLUDE_DIR}/mbedtls/build_info.h" mbedtls_header_1)
+    file(READ "${MBEDTLS_INCLUDE_DIR}/mbedtls/version.h" mbedtls_header_2)
+    string(REGEX MATCH "MBEDTLS_VERSION_STRING +\"[0-9|.]+\"" MBEDTLSMATCH "${mbedtls_header_1} ${mbedtls_header_2}")
     if(MBEDTLSMATCH)
       string(REGEX REPLACE "MBEDTLS_VERSION_STRING +\"([0-9|.]+)\"" "\\1" MBEDTLS_VERSION ${MBEDTLSMATCH})
-      message(STATUS "  version ${MBEDTLS_VERSION}")
     endif()
-    message(STATUS "  TLS: ${MBEDTLS_LIBRARY}")
-    message(STATUS "  X509: ${MBEDX509_LIBRARY}")
-    message(STATUS "  Crypto: ${MBEDCRYPTO_LIBRARY}")
+    message(STATUS "Found mbedTLS crypto: ${MBEDCRYPTO_LIBRARY} (version \"${MBEDTLS_VERSION}\")")
   endif()
 elseif(MBEDTLS_FIND_REQUIRED)
   message(FATAL_ERROR "Could not find mbedTLS")
@@ -59,7 +43,5 @@ mark_as_advanced(
   MBEDTLS_INCLUDE_DIR
   MBEDTLS_LIBRARY_DIR
   MBEDTLS_LIBRARIES
-  MBEDTLS_LIBRARY
-  MBEDX509_LIBRARY
   MBEDCRYPTO_LIBRARY
 )
