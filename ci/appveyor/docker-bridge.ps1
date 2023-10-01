15bc98dbfbc74e7b013836ed0092d8d7cb6c3c62
Viktor Szakats <commit@vsz.me>
2023-10-01 12:08:16 +0000

    sync else-style and indentation
    indentation is 2 for scripts.
    opted for the else-style we use in C.

--- ci/appveyor/docker-bridge.ps1
+++ ci/appveyor/docker-bridge.ps1
@@ -8,12 +8,14 @@
 $ip = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -like 'ethernet*'}).IPAddress
 $port = 3389
 if($ip.StartsWith('172.24.')) {
-    $port = 33800 + ($ip.split('.')[2] - 16) * 256 + $ip.split('.')[3]
-} elseif ($ip.StartsWith('192.168.') -or $ip.StartsWith('10.240.')) {
-    # new environment - behind NAT
-    $port = 33800 + ($ip.split('.')[2] - 0) * 256 + $ip.split('.')[3]
-} elseif ($ip.StartsWith('10.0.')) {
-    $port = 33800 + ($ip.split('.')[2] - 0) * 256 + $ip.split('.')[3]
+  $port = 33800 + ($ip.split('.')[2] - 16) * 256 + $ip.split('.')[3]
+}
+elseif($ip.StartsWith('192.168.') -or $ip.StartsWith('10.240.')) {
+  # new environment - behind NAT
+  $port = 33800 + ($ip.split('.')[2] - 0) * 256 + $ip.split('.')[3]
+}
+elseif($ip.StartsWith('10.0.')) {
+  $port = 33800 + ($ip.split('.')[2] - 0) * 256 + $ip.split('.')[3]
 }
 
 # get external IP
