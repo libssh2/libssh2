5675061234a641596a0c8774c134e76894e31f88
Viktor Szakats <commit@vsz.me>
2023-12-22 18:28:33 +0000

    libgcrypt verify `gcry_md_read()` success

--- src/libgcrypt.c
+++ src/libgcrypt.c
@@ -124,8 +124,13 @@ int _libssh2_hmac_update(libssh2_hmac_ctx *ctx,
 
 int _libssh2_hmac_final(libssh2_hmac_ctx *ctx, void *data)
 {
-    memcpy(data, gcry_md_read(*ctx, 0),
-           gcry_md_get_algo_dlen(gcry_md_get_algo(*ctx)));
+    unsigned char *res = gcry_md_read(*ctx, 0);
+
+    if(!res)
+        return 0;
+
+    memcpy(data, res, gcry_md_get_algo_dlen(gcry_md_get_algo(*ctx)));
+
     return 1;
 }
 
