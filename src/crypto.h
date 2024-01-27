72d959f328f4ed9c7bf5f13dbefd5ff07eb638fa
Johannes Passing <jpassing@hotmail.com>
2024-01-27 11:16:17 +1100

    Add missing declaration for _libssh2_ecdsa_free

--- src/crypto.h
+++ src/crypto.h
@@ -230,6 +230,9 @@ int
 _libssh2_ecdsa_curve_type_from_name(const char *name,
                                     libssh2_curve_type *out_type);
 
+void
+_libssh2_ecdsa_free(libssh2_ecdsa_ctx* ctx);
+
 #endif /* LIBSSH2_ECDSA */
 
 #if LIBSSH2_ED25519
