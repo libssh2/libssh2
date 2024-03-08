5e0b0b58eae3d13b3e3fec93fe3ef47aecfe569d
Tejaswikandula <teju256@gmail.com>
2024-03-07 16:15:27 -0800

    Update src/openssl.c
    Co-authored-by: Viktor Szakats <vszakats@users.noreply.github.com>

--- src/openssl.c
+++ src/openssl.c
@@ -5135,8 +5135,8 @@ _libssh2_supported_key_sign_algorithms(LIBSSH2_SESSION *session,
 
 #if LIBSSH2_RSA_SHA2
     if((key_method_len == 7 &&
-       memcmp(key_method, "ssh-rsa", key_method_len) == 0)
-       || (key_method_len == 28 &&
+        memcmp(key_method, "ssh-rsa", key_method_len) == 0) ||
+       (key_method_len == 28 &&
        memcmp(key_method, "ssh-rsa-cert-v01@openssh.com", key_method_len) == 0)
        ) {
         return "rsa-sha2-512,rsa-sha2-256"
