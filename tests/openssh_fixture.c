cab13712a88ec8e8444b223dbe9f297c80fad818
Viktor Szakats <commit@vsz.me>
2024-03-29 11:05:57 +0000

    build: drop `-Wformat-nonliteral` warning suppressions
    Also markup a vararg function as such.
    
    In functions marked up as vararg functions, there is no need to suppress
    `-Wformat-nonliteral` warnings. It's done automatically by the compiler.
    
    Closes #1342

--- tests/openssh_fixture.c
+++ tests/openssh_fixture.c
@@ -80,6 +80,9 @@ int openssh_fixture_have_docker(void)
 }
 
 static int run_command_varg(char **output, const char *command, va_list args)
+    LIBSSH2_PRINTF(2, 0);
+
+static int run_command_varg(char **output, const char *command, va_list args)
 {
     static const char redirect_stderr[] = "%s 2>&1";
 
@@ -94,14 +97,7 @@ static int run_command_varg(char **output, const char *command, va_list args)
     }
 
     /* Format the command string */
-#ifdef __clang__
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wformat-nonliteral"
-#endif
     ret = vsnprintf(command_buf, sizeof(command_buf), command, args);
-#ifdef __clang__
-#pragma clang diagnostic pop
-#endif
     if(ret < 0 || ret >= BUFSIZ) {
         fprintf(stderr, "Unable to format command (%s)\n", command);
         return -1;
