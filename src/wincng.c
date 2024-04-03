a6f46aa7183e13af621645345774a54465368b84
Johannes Passing <jpassing@hotmail.com>
2024-04-04 09:42:15 +1100

    Fix code style violations

--- src/wincng.c
+++ src/wincng.c
@@ -320,12 +320,12 @@ typedef struct __libssh2_ecdsa_point {
 /* Lookup libssh2_curve_type by name */
 static int
 _libssh2_wincng_ecdsa_curve_type_from_name(IN const char* name,
-    OUT libssh2_curve_type* out_curve);
+    OUT libssh2_curve_type *out_curve);
 
 /* Parse an OpenSSL-formatted ECDSA private key */
 static int
 _libssh2_wincng_parse_ecdsa_privatekey(OUT _libssh2_wincng_ecdsa_key** key,
-    IN unsigned char* privatekey,
+    IN unsigned char *privatekey,
     IN size_t privatekey_len);
 
 #endif
