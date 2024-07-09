d268926ae70e84b1ea60683481bbe86368e8cf0a
Viktor Szakats <commit@vsz.me>
2024-07-10 00:55:19 +0200

    try reshuffle 1

--- cmake/FindLibgcrypt.cmake
+++ cmake/FindLibgcrypt.cmake
@@ -41,25 +41,23 @@
 #  LIBGCRYPT_INCLUDE_DIRS - The Libgcrypt include directories
 #  LIBGCRYPT_LIBRARIES    - The libraries needed to use Libgcrypt
 
-find_package(PkgConfig QUIET)
-pkg_check_modules(PC_LIBGCRYPT QUIET "libgcrypt")
-
-find_path(LIBGCRYPT_INCLUDE_DIR
-  NAMES "gcrypt.h"
-  HINTS ${PC_LIBGCRYPT_INCLUDE_DIRS})
-
-find_library(LIBGCRYPT_LIBRARY
-  NAMES "gcrypt" "libgcrypt"
-  HINTS ${PC_LIBGCRYPT_LIBRARY_DIRS})
-
 include(FindPackageHandleStandardArgs)
+
+find_package(PkgConfig QUIET)
+pkg_check_modules(LIBGCRYPT QUIET "libgcrypt")
+
+if(NOT LIBGCRYPT_FOUND)
+  find_path(LIBGCRYPT_INCLUDE_DIR NAMES "gcrypt.h" )
+  find_library(LIBGCRYPT_LIBRARY NAMES "gcrypt" "libgcrypt")
+
+  mark_as_advanced(LIBGCRYPT_INCLUDE_DIR LIBGCRYPT_LIBRARY)
+
+  if(LIBGCRYPT_FOUND)
+    set(LIBGCRYPT_INCLUDE_DIRS ${LIBGCRYPT_INCLUDE_DIR})
+    set(LIBGCRYPT_LIBRARIES    ${LIBGCRYPT_LIBRARY})
+  endif()
+endif()
+
 find_package_handle_standard_args("Libgcrypt"
   DEFAULT_MSG
-  LIBGCRYPT_INCLUDE_DIR LIBGCRYPT_LIBRARY)
-
-mark_as_advanced(LIBGCRYPT_INCLUDE_DIR LIBGCRYPT_LIBRARY)
-
-if(LIBGCRYPT_FOUND)
-  set(LIBGCRYPT_INCLUDE_DIRS ${LIBGCRYPT_INCLUDE_DIR})
-  set(LIBGCRYPT_LIBRARIES    ${LIBGCRYPT_LIBRARY})
-endif()
+  LIBGCRYPT_INCLUDE_DIRS LIBGCRYPT_LIBRARIES)
