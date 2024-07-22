1701d5c0d3a97f861064deb1ff6a3678ff5f67bc
Will Cosgrove <will@panic.com>
2024-07-22 09:57:24 -0700

    Correctly initialize values (#1428)
    Fix regression with commit from #1421

--- src/mbedtls.c
+++ src/mbedtls.c
@@ -1294,8 +1294,11 @@ _libssh2_mbedtls_ecdsa_new_private(libssh2_ecdsa_ctx **ctx,
                                    const unsigned char *pwd)
 {
     mbedtls_pk_context pkey;
-    unsigned char *data;
-    size_t data_len;
+    unsigned char *data = NULL;
+    size_t data_len = 0;
+    (void)session;
+    (void)filename;
+    (void)pwd;
 
     /* FIXME: Reimplement this functionality via a public API. */
     if(mbedtls_pk_load_file(filename, &data, &data_len))
