4699f2fe1204aa9cc483e5e81d60b34d7eeab928
Viktor Szakats <commit@vsz.me>
2024-07-25 19:30:48 +0200

    Revert "test 10"
    This reverts commit ca3e10742eed7c0088d3da834b30c0057c4e1044.

--- cmake/FindLibgcrypt.cmake
+++ cmake/FindLibgcrypt.cmake
@@ -41,10 +41,8 @@
 #  LIBGCRYPT_INCLUDE_DIRS - The Libgcrypt include directories
 #  LIBGCRYPT_LIBRARIES    - The libraries needed to use Libgcrypt
 
-if(NOT(CMAKE_CROSSCOMPILING AND "$ENV{PKG_CONFIG_LIBDIR}" STREQUAL ""))
-  find_package(PkgConfig QUIET)
-  pkg_check_modules(LIBGCRYPT "libgcrypt")
-endif()
+find_package(PkgConfig QUIET)
+pkg_check_modules(LIBGCRYPT "libgcrypt")
 
 if(LIBGCRYPT_FOUND)
   # Use LIBGCRYPT_INCLUDE_DIRS as-is instead of forwarding it as a hint
