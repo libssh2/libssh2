d3f3f65cba2d35465368473db7afd5a8cb63299c
Viktor Szakats <commit@vsz.me>
2024-07-30 14:39:50 +0200

    Findwolfssl: improve pkg-config support

--- cmake/Findwolfssl.cmake
+++ cmake/Findwolfssl.cmake
@@ -8,38 +8,37 @@
 #  WOLFSSL_LIBRARIES       - The libraries needed to use wolfSSL
 
 find_package(PkgConfig QUIET)
-pkg_check_modules(PC_WOLFSSL "wolfssl")
-
-find_path(WOLFSSL_INCLUDE_DIR
-  NAMES "wolfssl/options.h"
-  HINTS ${PC_WOLFSSL_INCLUDE_DIRS})
-
-find_library(WOLFSSL_LIBRARY
-  NAMES "wolfssl"
-  HINTS ${PC_WOLFSSL_LIBRARY_DIRS})
-
-if(WOLFSSL_INCLUDE_DIR)
-  set(_version_regex "^#define[ \t]+LIBWOLFSSL_VERSION_STRING[ \t]+\"([^\"]+)\".*")
-  file(STRINGS "${WOLFSSL_INCLUDE_DIR}/wolfssl/version.h"
-    WOLFSSL_VERSION REGEX "${_version_regex}")
-  string(REGEX REPLACE "${_version_regex}" "\\1"
-    WOLFSSL_VERSION "${WOLFSSL_VERSION}")
-  unset(_version_regex)
-endif()
-
-include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set WOLFSSL_FOUND
-# to TRUE if all listed variables are TRUE and the requested version
-# matches.
-find_package_handle_standard_args("wolfssl"
-  REQUIRED_VARS
-    WOLFSSL_INCLUDE_DIR
-    WOLFSSL_LIBRARY
-  VERSION_VAR WOLFSSL_VERSION)
-
-mark_as_advanced(WOLFSSL_INCLUDE_DIR WOLFSSL_LIBRARY)
+pkg_check_modules(WOLFSSL "wolfssl")
 
 if(WOLFSSL_FOUND)
-  set(WOLFSSL_INCLUDE_DIRS ${WOLFSSL_INCLUDE_DIR})
-  set(WOLFSSL_LIBRARIES    ${WOLFSSL_LIBRARY})
+  set(WOLFSSL_LIBRARIES ${WOLFSSL_LINK_LIBRARIES})
+else()
+  find_path(WOLFSSL_INCLUDE_DIR NAMES "wolfssl/options.h")
+  find_library(WOLFSSL_LIBRARY NAMES "wolfssl")
+
+  if(WOLFSSL_INCLUDE_DIR)
+    set(_version_regex "^#define[ \t]+LIBWOLFSSL_VERSION_STRING[ \t]+\"([^\"]+)\".*")
+    file(STRINGS "${WOLFSSL_INCLUDE_DIR}/wolfssl/version.h"
+      WOLFSSL_VERSION REGEX "${_version_regex}")
+    string(REGEX REPLACE "${_version_regex}" "\\1"
+      WOLFSSL_VERSION "${WOLFSSL_VERSION}")
+    unset(_version_regex)
+  endif()
+
+  include(FindPackageHandleStandardArgs)
+  # handle the QUIETLY and REQUIRED arguments and set WOLFSSL_FOUND
+  # to TRUE if all listed variables are TRUE and the requested version
+  # matches.
+  find_package_handle_standard_args("wolfssl"
+    REQUIRED_VARS
+      WOLFSSL_INCLUDE_DIR
+      WOLFSSL_LIBRARY
+    VERSION_VAR WOLFSSL_VERSION)
+
+  mark_as_advanced(WOLFSSL_INCLUDE_DIR WOLFSSL_LIBRARY)
+
+  if(WOLFSSL_FOUND)
+    set(WOLFSSL_INCLUDE_DIRS ${WOLFSSL_INCLUDE_DIR})
+    set(WOLFSSL_LIBRARIES    ${WOLFSSL_LIBRARY})
+  endif()
 endif()
