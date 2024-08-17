eaafb8d72fcc8334c743f0c3983efa293a1521b7
Viktor Szakats <commit@vsz.me>
2024-08-18 00:43:32 +0200

    wolfssl: sync up

--- cmake/FindWolfSSL.cmake
+++ cmake/FindWolfSSL.cmake
@@ -1,40 +1,53 @@
 # Copyright (C) The libssh2 project and its contributors.
 # SPDX-License-Identifier: BSD-3-Clause
 #
-# - Try to find wolfssl
-# Once done this will define
-#  WOLFSSL_FOUND           - System has wolfssl
-#  WOLFSSL_INCLUDE_DIR     - The wolfssl include directories
-#  WOLFSSL_LIBRARIES       - The libraries needed to use wolfssl
+###########################################################################
+# Find the wolfssl library
+#
+# Input variables:
+#
+# WOLFSSL_INCLUDE_DIR   The wolfssl include directory
+# WOLFSSL_LIBRARY       Path to wolfssl library
+#
+# Result variables:
+#
+# WOLFSSL_FOUND         System has wolfssl
+# WOLFSSL_INCLUDE_DIRS  The wolfssl include directories
+# WOLFSSL_LIBRARIES     The wolfssl library names
+# WOLFSSL_VERSION       Version of wolfssl
 
 find_package(PkgConfig QUIET)
 pkg_check_modules(PC_WOLFSSL QUIET "wolfssl")
 
-find_path(WOLFSSL_INCLUDE_DIR
-  NAMES "wolfssl/options.h"
-  HINTS ${PC_WOLFSSL_INCLUDE_DIRS}
-)
-find_library(WOLFSSL_LIBRARY
-  NAMES "wolfssl"
-  HINTS ${PC_WOLFSSL_LIBRARY_DIRS}
+find_path(WOLFSSL_INCLUDE_DIR NAMES "wolfssl/options.h"
+  HINTS
+    ${PC_WOLFSSL_INCLUDEDIR}
+    ${PC_WOLFSSL_INCLUDE_DIRS}
 )
 
-if(WOLFSSL_INCLUDE_DIR)
-  set(_version_regex "^#define[ \t]+LIBWOLFSSL_VERSION_STRING[ \t]+\"([^\"]+)\".*")
-  file(STRINGS "${WOLFSSL_INCLUDE_DIR}/wolfssl/version.h"
-    WOLFSSL_VERSION REGEX "${_version_regex}")
-  string(REGEX REPLACE "${_version_regex}" "\\1"
-    WOLFSSL_VERSION "${WOLFSSL_VERSION}")
+find_library(WOLFSSL_LIBRARY NAMES "wolfssl"
+  HINTS
+    ${PC_WOLFSSL_LIBDIR}
+    ${PC_WOLFSSL_LIBRARY_DIRS}
+)
+
+if(WOLFSSL_INCLUDE_DIR AND EXISTS "${WOLFSSL_INCLUDE_DIR}/wolfssl/version.h")
+  set(_version_regex "#[\t ]*define[\t ]+LIBWOLFSSL_VERSION_STRING[\t ]+\"([^\"]*)\"")
+  file(STRINGS "${WOLFSSL_INCLUDE_DIR}/wolfssl/version.h" _version_str REGEX "${_version_regex}")
+  string(REGEX REPLACE "${_version_regex}" "\\1" _version_str "${_version_str}")
+  set(WOLFSSL_VERSION "${_version_str}")
   unset(_version_regex)
+  unset(_version_str)
 endif()
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set WOLFSSL_FOUND
-# to TRUE if all listed variables are TRUE and the requested version
-# matches.
 find_package_handle_standard_args(WolfSSL
-  REQUIRED_VARS WOLFSSL_INCLUDE_DIR WOLFSSL_LIBRARY
-  VERSION_VAR WOLFSSL_VERSION)
+  REQUIRED_VARS
+    WOLFSSL_INCLUDE_DIR
+    WOLFSSL_LIBRARY
+  VERSION_VAR
+    WOLFSSL_VERSION
+)
 
 if(WOLFSSL_FOUND)
   set(WOLFSSL_INCLUDE_DIRS ${WOLFSSL_INCLUDE_DIR})
