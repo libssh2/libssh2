318b35f9f5469fc786c2005993c568ffb405b5a2
Viktor Szakats <commit@vsz.me>
2024-08-18 01:13:37 +0200

    libgcrypt: add version detection

--- cmake/FindLibgcrypt.cmake
+++ cmake/FindLibgcrypt.cmake
@@ -53,11 +53,22 @@
 find_path(LIBGCRYPT_INCLUDE_DIR NAMES "gcrypt.h")
 find_library(LIBGCRYPT_LIBRARY NAMES "gcrypt" "libgcrypt")
 
+if(LIBGCRYPT_INCLUDE_DIR AND EXISTS "${LIBGCRYPT_INCLUDE_DIR}/gcrypt.h")
+  set(_version_regex "#[\t ]*define[\t ]+GCRYPT_VERSION[\t ]+\"([^\"]*)\"")
+  file(STRINGS "${LIBGCRYPT_INCLUDE_DIR}/gcrypt.h" _version_str REGEX "${_version_regex}")
+  string(REGEX REPLACE "${_version_regex}" "\\1" _version_str "${_version_str}")
+  set(LIBGCRYPT_VERSION "${_version_str}")
+  unset(_version_regex)
+  unset(_version_str)
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Libgcrypt
   REQUIRED_VARS
     LIBGCRYPT_INCLUDE_DIR
     LIBGCRYPT_LIBRARY
+  VERSION_VAR
+    LIBGCRYPT_VERSION
 )
 
 if(LIBGCRYPT_FOUND)
