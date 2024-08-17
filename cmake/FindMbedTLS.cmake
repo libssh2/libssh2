a5e448060a62a18df1769e007613be3dbdcf716d
Viktor Szakats <commit@vsz.me>
2024-08-17 23:27:40 +0200

    mbedtls: add pkg-config support

--- cmake/FindMbedTLS.cmake
+++ cmake/FindMbedTLS.cmake
@@ -16,8 +16,19 @@
 # MBEDTLS_LIBRARIES     The mbedtls library names
 # MBEDTLS_VERSION       Version of mbedtls
 
-find_path(MBEDTLS_INCLUDE_DIR NAMES "mbedtls/version.h")
-find_library(MBEDCRYPTO_LIBRARY NAMES "mbedcrypto" "libmbedcrypto")
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_MBEDTLS QUIET "mbedtls")
+
+find_path(MBEDTLS_INCLUDE_DIR NAMES "mbedtls/version.h"
+  HINTS
+    ${PC_MBEDTLS_INCLUDEDIR}
+    ${PC_MBEDTLS_INCLUDE_DIRS}
+)
+find_library(MBEDCRYPTO_LIBRARY NAMES "mbedcrypto" "libmbedcrypto"
+  HINTS
+    ${PC_MBEDTLS_LIBDIR}
+    ${PC_MBEDTLS_LIBRARY_DIRS}
+)
 
 if(MBEDTLS_INCLUDE_DIR)
   if(EXISTS "${MBEDTLS_INCLUDE_DIR}/mbedtls/build_info.h")  # 3.x
