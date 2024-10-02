62afec1ff34a945a55d329a753863b6069aadae9
Patrick Monnerat <patrick@monnerat.net>
2024-10-02 08:39:32 +0200

    os400: update my e-mail address

--- os400/include/sys/socket.h
+++ os400/include/sys/socket.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Patrick Monnerat, D+H <patrick.monnerat@dh.com>
+ * Copyright (C) Patrick Monnerat <patrick@monnerat.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms,
