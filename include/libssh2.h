00e5975ad27cf16635208a813db4a563e5e58408
Viktor Szakats <commit@vsz.me>
2024-10-21 11:59:53 +0200

    libssh2.h: bump to 1.11.1

--- include/libssh2.h
+++ include/libssh2.h
@@ -48,7 +48,7 @@
    to make the BANNER define (used by src/session.c) be a valid SSH
    banner. Release versions have no appended strings and may of course not
    have dashes either. */
-#define LIBSSH2_VERSION                             "1.11.1_DEV"
+#define LIBSSH2_VERSION                             "1.11.1"
 
 /* The numeric version number is also available "in parts" by using these
    defines: */
