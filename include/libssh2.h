9cba3a5829f8e605f6e175c81f4faa3c75342a60
Viktor Szakats <commit@vsz.me>
2024-10-21 12:00:17 +0200

    libssh2.h: bump to 1.11.2_DEV

--- include/libssh2.h
+++ include/libssh2.h
@@ -54,7 +54,7 @@
    have dashes either. */
 #define LIBSSH2_VERSION                             "1.11.2_DEV"
    have dashes either. */
-#define LIBSSH2_VERSION                             "1.11.1"
+#define LIBSSH2_VERSION                             "1.11.2_DEV"
 
 /* The numeric version number is also available "in parts" by using these
    defines: */
