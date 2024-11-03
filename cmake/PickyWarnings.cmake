dd18ad7c09e1b24323d72823e090e08bb0b364e9
Viktor Szakats <commit@vsz.me>
2024-11-03 02:43:35 +0100

    Revert "test"
    This reverts commit 7811aa0661fab24cc537894a0ee22fc46bf6142b.

--- cmake/PickyWarnings.cmake
+++ cmake/PickyWarnings.cmake
@@ -108,7 +108,6 @@ if(PICKY_COMPILER)
     # -Wswitch-enum                        # clang  2.7  gcc  4.1               # Not used: It basically disallows default case
       -Wtype-limits                        # clang  2.7  gcc  4.3
       -Wunreachable-code                   # clang  2.7  gcc  4.1
-    # -Wunsafe-buffer-usage
       -Wunused-macros                      # clang  2.7  gcc  4.1
       -Wunused-parameter                   # clang  2.7  gcc  4.1
       -Wvla                                # clang  2.8  gcc  4.3
