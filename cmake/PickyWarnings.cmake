a3153a1dbc35afc8b2e548813e5308abbb24ac31
Viktor Szakats <commit@vsz.me>
2024-11-03 02:39:37 +0100

    test 2 fixup

--- cmake/PickyWarnings.cmake
+++ cmake/PickyWarnings.cmake
@@ -44,16 +44,6 @@ if(PICKY_COMPILER)
       -Wall -pedantic
     )
 
-#    if(MSVC)  # clang-cl
-#      list(APPEND _picky_enable
-#        /clang:-Wall
-#      )
-#    else()
-#      list(APPEND _picky_enable
-#        -Wall
-#      )
-#    endif()
-
     if(ENABLE_WERROR)
       list(APPEND _picky_enable
         -pedantic-errors
@@ -254,6 +244,7 @@ if(PICKY_COMPILER)
     if(CMAKE_C_COMPILER_ID STREQUAL "Clang" AND MSVC)
       set(_picky_tmp "")
       foreach(_ccopt IN LISTS _picky)
+        # Prefix -Wall, otherwise clang-cl takes it as an MSVC option and translates it to -Weverything
         if(_ccopt MATCHES "^-W" AND NOT _ccopt STREQUAL "-Wall")
           list(APPEND _picky_tmp ${_ccopt})
         else()
