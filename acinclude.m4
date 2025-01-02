libssh2/libssh2

commit d19b61907039554b8261a90898f2f31e1a706f38

autotools: fix to update `LDFLAGS` for each detected dependency
autotools lib detection routine failed to extend LDFLAGS for each
detection. This could cause successful detection of a dependency, but
later failing to use it. This did not cause an issue as long as all
dependencies lived under the same prefix, but started breaking on macOS
ARM + Homebrew where this was no longer true for mbedTLS and zlib in
particular.

Follow-up to 844115393bffb4e92c6569204cbe4cd8e553480d #1381
Follow-up to ae2770de25949bc7c74e60b4cc6a011bbe1d3d7c #1377
Closes #1384

acinclude.m4
@@ -845,12 +845,11 @@ AC_DEFUN([LIBSSH2_LIB_HAVE_LINKFLAGS], [
 
   AC_LIB_HAVE_LINKFLAGS([$1], [$2], [$3])
 
-  LDFLAGS="$libssh2_save_LDFLAGS"
-
   if test "$ac_cv_lib$1" = "yes"; then :
     $4
   else
     CPPFLAGS="$libssh2_save_CPPFLAGS"
+    LDFLAGS="$libssh2_save_LDFLAGS"
   fi
 ])
 

.github/workflows/ci.yml
@@ -558,8 +558,6 @@ jobs:
       - name: 'autotools configure'
         if: ${{ matrix.build == 'autotools' }}
         run: |
-          # Workaround for GHA ARM runner issue
-          [ '${{ matrix.crypto.name }}' = 'mbedTLS' ] && export LDFLAGS="-L$(brew --prefix)/lib"
           mkdir bld && cd bld && ../configure --enable-werror --enable-debug \
             --with-libz ${{ matrix.crypto.configure }} \
             --disable-docker-tests \

commit 885923038839ee5f7a59d2679d8ded1b90c78beb

configure: fix `--without-lib*-prefix` regression
```
$ ../configure --with-openssl --without-libssl-prefix
[...]
../libtool: line 7756: cd: no/lib: No such file or directory
libtool:   error: cannot determine absolute directory name of 'no/lib'
make[2]: *** [libssh2.la] Error 1
```

Regression from d19b61907039554b8261a90898f2f31e1a706f38 #1384

Reported-by: Christoph Reiter
Fixes #1505

acinclude.m4
@@ -835,21 +835,21 @@ dnl
 dnl For convenience, $4 is expanded if [lib]$1 is found.
 
 AC_DEFUN([LIBSSH2_LIB_HAVE_LINKFLAGS], [
-  libssh2_save_CPPFLAGS="$CPPFLAGS"
-  libssh2_save_LDFLAGS="$LDFLAGS"
 
   if test "${with_lib$1_prefix+set}" = set; then
+    libssh2_save_CPPFLAGS="$CPPFLAGS"
+    libssh2_save_LDFLAGS="$LDFLAGS"
     CPPFLAGS="$CPPFLAGS${CPPFLAGS:+ }-I${with_lib$1_prefix}/include"
     LDFLAGS="$LDFLAGS${LDFLAGS:+ }-L${with_lib$1_prefix}/lib"
+    AC_LIB_HAVE_LINKFLAGS([$1], [$2], [$3])
+    CPPFLAGS="$libssh2_save_CPPFLAGS"
+    LDFLAGS="$libssh2_save_LDFLAGS"
+  else
+    AC_LIB_HAVE_LINKFLAGS([$1], [$2], [$3])
   fi
-
-  AC_LIB_HAVE_LINKFLAGS([$1], [$2], [$3])
 
   if test "$ac_cv_lib$1" = "yes"; then :
     $4
-  else
-    CPPFLAGS="$libssh2_save_CPPFLAGS"
-    LDFLAGS="$libssh2_save_LDFLAGS"
   fi
 ])
 