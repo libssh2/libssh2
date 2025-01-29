1612807b9f2140fce590305fcd92c6f7a28e9c13
Viktor Szakats <commit@vsz.me>
2025-01-29 13:42:48 +0100

    cmake: make integration tests generator-agnostic, use `GIT_SHALLOW`
    Closes #1523

--- tests/cmake/test.sh
+++ tests/cmake/test.sh
@@ -8,17 +8,21 @@ set -u
 
 cd "$(dirname "$0")"
 
-rm -rf bld-fetchcontent; cmake -B bld-fetchcontent -DTEST_INTEGRATION_MODE=FetchContent \
+rm -rf bld-fetchcontent; cmake -B bld-fetchcontent \
+  -DTEST_INTEGRATION_MODE=FetchContent \
   -DFROM_GIT_REPO="${PWD}/../.." \
   -DFROM_GIT_TAG="$(git rev-parse HEAD)"
-make -C bld-fetchcontent
+cmake --build bld-fetchcontent
 
 rm -rf libssh2; ln -s ../.. libssh2
-rm -rf bld-add_subdirectory; cmake -B bld-add_subdirectory -DTEST_INTEGRATION_MODE=add_subdirectory
-make -C bld-add_subdirectory
+rm -rf bld-add_subdirectory; cmake -B bld-add_subdirectory \
+  -DTEST_INTEGRATION_MODE=add_subdirectory
+cmake --build bld-add_subdirectory
 
 rm -rf bld-libssh2; cmake ../.. -B bld-libssh2
-make -C bld-libssh2 DESTDIR=pkg install
-rm -rf bld-find_package; cmake -B bld-find_package -DTEST_INTEGRATION_MODE=find_package \
-  -DCMAKE_PREFIX_PATH="${PWD}/bld-libssh2/pkg/usr/local/lib/cmake/libssh2"
-make -C bld-find_package
+cmake --build bld-libssh2
+cmake --install bld-libssh2 --prefix bld-libssh2/_pkg
+rm -rf bld-find_package; cmake -B bld-find_package \
+  -DTEST_INTEGRATION_MODE=find_package \
+  -DCMAKE_PREFIX_PATH="${PWD}/bld-libssh2/_pkg/lib/cmake/libssh2"
+cmake --build bld-find_package
