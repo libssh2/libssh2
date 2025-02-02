ea6e9dfca1571321635568b62144640c98c4dbb4
Viktor Szakats <commit@vsz.me>
2025-02-02 23:27:11 +0100

    drop local test stuff

--- tests/cmake/test.sh
+++ tests/cmake/test.sh
@@ -30,13 +30,12 @@ if [ "${mode}" = 'all' ] || [ "${mode}" = 'find_package' ]; then
   crypto="${2:-OpenSSL}"
   bld="bld-libssh2-${crypto}"
   rm -rf "${bld}"
-  cmake ../.. -B "${bld}" -DCMAKE_IGNORE_PREFIX_PATH=/usr -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DENABLE_ZLIB_COMPRESSION=ON -DCRYPTO_BACKEND="${crypto}"
+  cmake ../.. -B "${bld}" -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DENABLE_ZLIB_COMPRESSION=ON -DCRYPTO_BACKEND="${crypto}"
   cmake --build "${bld}"
   cmake --install "${bld}" --prefix "${bld}/_pkg"
   rm -rf bld-find_package
   cmake -B bld-find_package \
     -DTEST_INTEGRATION_MODE=find_package \
-    -DCMAKE_PREFIX_PATH="${PWD}/${bld}/_pkg/lib/cmake/libssh2" \
-    -DCMAKE_IGNORE_PREFIX_PATH=/usr
+    -DCMAKE_PREFIX_PATH="${PWD}/${bld}/_pkg/lib/cmake/libssh2"
   cmake --build bld-find_package --verbose
 fi
