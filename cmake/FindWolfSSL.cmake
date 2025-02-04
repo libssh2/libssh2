786aae13c85ee6387b81aec7685cc680ba9e8068
Viktor Szakats <commit@vsz.me>
2025-02-04 14:56:34 +0100

    FindWolfSSL.cmake: drop temporary trace logic

--- cmake/FindWolfSSL.cmake
+++ cmake/FindWolfSSL.cmake
@@ -14,43 +14,6 @@
 # - `WOLFSSL_FOUND`:         System has wolfSSL.
 # - `WOLFSSL_VERSION`:       Version of wolfSSL.
 
-function(libssh2_dumpprops _target)  # Dump all target properties
-  if(TARGET "${_target}")
-    execute_process(COMMAND "${CMAKE_COMMAND}" "--help-property-list" OUTPUT_VARIABLE _cmake_property_list)
-    string(REPLACE "\n" ";" _cmake_property_list "${_cmake_property_list}")
-    list(REMOVE_DUPLICATES _cmake_property_list)
-    list(REMOVE_ITEM _cmake_property_list "")
-    list(APPEND _cmake_property_list "LIBSSH2_PC_MODULES")
-    foreach(_prop IN LISTS _cmake_property_list)
-      if(_prop MATCHES "<CONFIG>")
-        foreach(_config IN ITEMS "DEBUG" "RELEASE" "MINSIZEREL" "RELWITHDEBINFO")
-          string(REPLACE "<CONFIG>" "${_config}" _propconfig "${_prop}")
-          get_property(_is_set TARGET "${_target}" PROPERTY "${_propconfig}" SET)
-          if(_is_set)
-            get_target_property(_val "${_target}" "${_propconfig}")
-            message("${_target}.${_propconfig} = '${_val}'")
-          endif()
-        endforeach()
-      else()
-        get_property(_is_set TARGET "${_target}" PROPERTY "${_prop}" SET)
-        if(_is_set)
-          get_target_property(_val "${_target}" "${_prop}")
-          message("${_target}.${_prop} = '${_val}'")
-        endif()
-      endif()
-    endforeach()
-  endif()
-endfunction()
-
-function(libssh2_dumpvars)  # Dump all defined variables with their values
-  message("::group::CMake Variable Dump")
-  get_cmake_property(_vars VARIABLES)
-  foreach(_var IN ITEMS ${_vars})
-    message("${_var} = '${${_var}}'")
-  endforeach()
-  message("::endgroup::")
-endfunction()
-
 set(_wolfssl_pc_requires "wolfssl")
 
 if(LIBSSH2_USE_PKGCONFIG AND
@@ -108,5 +71,4 @@ if(WOLFSSL_FOUND AND NOT TARGET libssh2::WolfSSL)
     INTERFACE_INCLUDE_DIRECTORIES "${WOLFSSL_INCLUDE_DIRS}"
     INTERFACE_LINK_DIRECTORIES "${WOLFSSL_LIBRARY_DIRS}"
     INTERFACE_LINK_LIBRARIES "${WOLFSSL_LIBRARIES}")
-  libssh2_dumpprops(libssh2::WolfSSL)
 endif()
