934b7f71c2b8013b834ff65e9524ba7a3c7b8720
Viktor Szakats <commit@vsz.me>
2025-04-23 15:55:24 +0200

    test.sh: add cmake bin vars

--- tests/cmake/test.sh
+++ tests/cmake/test.sh
@@ -22,19 +22,19 @@ src='../..'
 
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'FetchContent' ]; then
   rm -rf bld-fetchcontent
-  cmake -B bld-fetchcontent \
+  "${cmake_consumer}" -B bld-fetchcontent \
     -DTEST_INTEGRATION_MODE=FetchContent \
     -DFROM_GIT_REPO="${PWD}/${src}" \
     -DFROM_GIT_TAG="$(git rev-parse HEAD)"
-  cmake --build bld-fetchcontent
+  "${cmake_consumer}" --build bld-fetchcontent
 fi
 
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'add_subdirectory' ]; then
   rm -rf libssh2; ln -s "${src}" libssh2
   rm -rf bld-add_subdirectory
-  cmake -B bld-add_subdirectory \
+  "${cmake_consumer}" -B bld-add_subdirectory \
     -DTEST_INTEGRATION_MODE=add_subdirectory
-  cmake --build bld-add_subdirectory
+  "${cmake_consumer}" --build bld-add_subdirectory
 fi
 
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'find_package' ]; then
@@ -42,15 +42,15 @@ if [ "${mode}" = 'all' ] || [ "${mode}" = 'find_package' ]; then
   bldp="bld-libssh2-${crypto}"
   prefix="${PWD}/${bldp}/_pkg"
   rm -rf "${bldp}"
-  cmake "${src}" -B "${bldp}" -DCMAKE_INSTALL_PREFIX="${prefix}" \
+  "${cmake_provider}" "${src}" -B "${bldp}" -DCMAKE_INSTALL_PREFIX="${prefix}" \
     -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF \
     -DENABLE_ZLIB_COMPRESSION=ON \
     -DCRYPTO_BACKEND="${crypto}"
-  cmake --build "${bldp}"
-  cmake --install "${bldp}"
+  "${cmake_provider}" --build "${bldp}"
+  "${cmake_provider}" --install "${bldp}"
   rm -rf bld-find_package
-  cmake -B bld-find_package \
+  "${cmake_consumer}" -B bld-find_package \
     -DTEST_INTEGRATION_MODE=find_package \
     -DCMAKE_PREFIX_PATH="${prefix}/lib/cmake/libssh2"
-  cmake --build bld-find_package --verbose
+  "${cmake_consumer}" --build bld-find_package --verbose
 fi
