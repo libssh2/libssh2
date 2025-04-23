77df767784f6336dfea166b9202b6be98595b833
Viktor Szakats <commit@vsz.me>
2025-04-23 09:30:08 +0200

    cmake: extend integration tests
    - ci/GHA: add cmake integration tests for Windows.
    - ci/GHA: test `add_subdirectory` with Libgcrypt.
    - make them run faster with prefill, unity, Ninja, omitting curl tool.
    - add support for any build configuration.
    - add old-cmake support with auto-detection.
    - auto-detect Ninja.
    - run consumer test apps to see if they work.
      Also show the cryptography backend.
    - add support for Windows.
    - make it more verbose.
    - re-add `ExternalProject` cmake consumer test. It's broken.
    - tidy up terminology.
    
    Cherry-picked from #1581
    
    Closes #1589

--- tests/cmake/test.c
+++ tests/cmake/test.c
@@ -6,8 +6,34 @@
 #include "libssh2.h"
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
-    printf("libssh2_version(0): |%s|\n", libssh2_version(0));
+    const char *crypto_str;
+
+    (void)argc;
+
+    switch(libssh2_crypto_engine()) {
+    case libssh2_gcrypt:
+        crypto_str = "libgcrypt";
+        break;
+    case libssh2_mbedtls:
+        crypto_str = "mbedTLS";
+        break;
+    case libssh2_openssl:
+        crypto_str = "openssl compatible";
+        break;
+    case libssh2_os400qc3:
+        crypto_str = "OS400QC3";
+        break;
+    case libssh2_wincng:
+        crypto_str = "WinCNG";
+        break;
+    default:
+        crypto_str = "(unrecognized)";
+    }
+
+    printf("libssh2 test: |%s|%s|%s|\n", argv[0],
+                                         libssh2_version(0), crypto_str);
+
     return 0;
 }
--- tests/cmake/test.sh
+++ tests/cmake/test.sh
@@ -1,44 +1,130 @@
-#!/bin/sh
+#!/bin/sh -x
 #
 # Copyright (C) Viktor Szakats
 # SPDX-License-Identifier: BSD-3-Clause
+
+# shellcheck disable=SC2086
 
 set -eu
 
 cd "$(dirname "$0")"
 
-mode="${1:-all}"
+command -v ninja >/dev/null && export CMAKE_GENERATOR=Ninja  # 3.17+
 
-if [ "${mode}" = 'all' ] || [ "${mode}" = 'FetchContent' ]; then
-  rm -rf bld-fetchcontent
-  cmake -B bld-fetchcontent \
+mode="${1:-all}"; shift
+
+cmake_consumer="${CMAKE_CONSUMER:-cmake}"
+cmake_provider="${CMAKE_PROVIDER:-${cmake_consumer}}"
+
+# 'modern': supports -S/-B (3.13+), --install (3.15+)
+"${cmake_consumer}" --help | grep -q -- '--install' && cmake_consumer_modern=1
+"${cmake_provider}" --help | grep -q -- '--install' && cmake_provider_modern=1
+
+cmake_opts='-DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DENABLE_ZLIB_COMPRESSION=ON'
+
+src='../..'
+
+runresults() {
+  set +x
+  for bin in "$1"/test-consumer*; do
+    file "${bin}" || true
+    ${LIBSSH2_TEST_EXE_RUNNER:-} "${bin}" || true
+  done
+  set -x
+}
+
+if [ "${mode}" = 'ExternalProject' ]; then  # Broken
+  (cd "${src}"; git archive --format=tar HEAD) | gzip > source.tar.gz
+  src="${PWD}/source.tar.gz"
+  sha="$(openssl dgst -sha256 "${src}" | grep -a -i -o -E '[0-9a-f]{64}$')"
+  bldc='bld-externalproject'
+  rm -rf "${bldc}"
+  if [ -n "${cmake_consumer_modern:-}" ]; then  # 3.15+
+    "${cmake_consumer}" -B "${bldc}" ${cmake_opts} -DCMAKE_UNITY_BUILD=ON "$@" \
+      -DTEST_INTEGRATION_MODE=ExternalProject \
+      -DFROM_ARCHIVE="${src}" -DFROM_HASH="${sha}"
+    "${cmake_consumer}" --build "${bldc}" --verbose
+  else
+    mkdir "${bldc}"; cd "${bldc}"
+    "${cmake_consumer}" .. ${cmake_opts} "$@" \
+      -DTEST_INTEGRATION_MODE=ExternalProject \
+      -DFROM_ARCHIVE="${src}" -DFROM_HASH="${sha}"
+    VERBOSE=1 "${cmake_consumer}" --build .
+    cd ..
+  fi
+  runresults "${bldc}"
+fi
+
+if [ "${mode}" = 'all' ] || [ "${mode}" = 'FetchContent' ]; then  # 3.14+
+  src="${PWD}/${src}"
+  bldc='bld-fetchcontent'
+  rm -rf "${bldc}"
+  "${cmake_consumer}" -B "${bldc}" ${cmake_opts} -DCMAKE_UNITY_BUILD=ON "$@" \
     -DTEST_INTEGRATION_MODE=FetchContent \
-    -DFROM_GIT_REPO="${PWD}/../.." \
+    -DFROM_GIT_REPO="${src}" \
     -DFROM_GIT_TAG="$(git rev-parse HEAD)"
-  cmake --build bld-fetchcontent
+  "${cmake_consumer}" --build "${bldc}" --verbose
+  PATH="${bldc}/_deps/libssh2-build/lib:${PATH}"
+  runresults "${bldc}"
 fi
 
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'add_subdirectory' ]; then
-  rm -rf libssh2; ln -s ../.. libssh2
-  rm -rf bld-add_subdirectory
-  cmake -B bld-add_subdirectory \
-    -DTEST_INTEGRATION_MODE=add_subdirectory
-  cmake --build bld-add_subdirectory
+  rm -rf libssh2
+  if ! ln -s "${src}" libssh2; then
+    rm -rf libssh2; mkdir libssh2; (cd "${src}"; git archive --format=tar HEAD) | tar -x --directory=libssh2  # for MSYS2/Cygwin
+  fi
+  bldc='bld-add_subdirectory'
+  rm -rf "${bldc}"
+  if [ -n "${cmake_consumer_modern:-}" ]; then  # 3.15+
+    "${cmake_consumer}" -B "${bldc}" ${cmake_opts} -DCMAKE_UNITY_BUILD=ON "$@" \
+      -DTEST_INTEGRATION_MODE=add_subdirectory
+    "${cmake_consumer}" --build "${bldc}" --verbose
+  else
+    mkdir "${bldc}"; cd "${bldc}"
+    # Disable `pkg-config` for CMake <= 3.12. These versions cannot propagate
+    # library directories to the consumer project.
+    "${cmake_consumer}" .. ${cmake_opts} -DCURL_USE_PKGCONFIG=OFF "$@" \
+      -DTEST_INTEGRATION_MODE=add_subdirectory
+    VERBOSE=1 "${cmake_consumer}" --build .
+    cd ..
+  fi
+  PATH="${bldc}/libssh2/src:${PATH}"
+  runresults "${bldc}"
 fi
 
 if [ "${mode}" = 'all' ] || [ "${mode}" = 'find_package' ]; then
-  crypto="${2:-OpenSSL}"
-  bld="bld-libssh2-${crypto}"
-  rm -rf "${bld}"
-  cmake ../.. -B "${bld}" -DCMAKE_INSTALL_PREFIX="${PWD}/${bld}/_pkg" \
-    -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF \
-    -DENABLE_ZLIB_COMPRESSION=ON \
-    -DCRYPTO_BACKEND="${crypto}"
-  cmake --build "${bld}"
-  cmake --install "${bld}"
-  rm -rf bld-find_package
-  cmake -B bld-find_package \
-    -DTEST_INTEGRATION_MODE=find_package \
-    -DCMAKE_PREFIX_PATH="${PWD}/${bld}/_pkg/lib/cmake/libssh2"
-  cmake --build bld-find_package --verbose
+  src="${PWD}/${src}"
+  bldp='bld-libssh2'
+  prefix="${PWD}/${bldp}/_pkg"
+  rm -rf "${bldp}"
+  if [ -n "${cmake_provider_modern:-}" ]; then  # 3.15+
+    "${cmake_provider}" -B "${bldp}" -S "${src}" ${cmake_opts} -DCMAKE_UNITY_BUILD=ON "$@" \
+      -DCMAKE_INSTALL_PREFIX="${prefix}"
+    "${cmake_provider}" --build "${bldp}"
+    "${cmake_provider}" --install "${bldp}"
+  else
+    mkdir "${bldp}"; cd "${bldp}"
+    "${cmake_provider}" "${src}" ${cmake_opts} "$@" \
+      -DCMAKE_INSTALL_PREFIX="${prefix}"
+    "${cmake_provider}" --build .
+    make install
+    cd ..
+  fi
+  bldc='bld-find_package'
+  rm -rf "${bldc}"
+  if [ -n "${cmake_consumer_modern:-}" ]; then  # 3.15+
+    "${cmake_consumer}" -B "${bldc}" \
+      -DTEST_INTEGRATION_MODE=find_package \
+      -DCMAKE_PREFIX_PATH="${prefix}/lib/cmake/libssh2"
+    "${cmake_consumer}" --build "${bldc}" --verbose
+  else
+    mkdir "${bldc}"; cd "${bldc}"
+    "${cmake_consumer}" .. \
+      -DTEST_INTEGRATION_MODE=find_package \
+      -DCMAKE_PREFIX_PATH="${prefix}/lib/cmake/libssh2"
+    VERBOSE=1 "${cmake_consumer}" --build .
+    cd ..
+  fi
+  PATH="${prefix}/bin:${PATH}"
+  runresults "${bldc}"
 fi
