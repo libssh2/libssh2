1800e4a42b0082899eaeb34e06c65e7174a4f523
Viktor Szakats <commit@vsz.me>
2025-09-14 00:38:47 +0200

    fix indentation

--- src/userauth.c
+++ src/userauth.c
@@ -1492,7 +1492,8 @@ _libssh2_key_sign_algorithm(LIBSSH2_SESSION *session,
 
     if(match) {
         if(*key_method_len == 28 &&
-        memcmp(key_method, "ssh-rsa-cert-v01@openssh.com", *key_method_len)) {
+           memcmp(key_method,
+                  "ssh-rsa-cert-v01@openssh.com", *key_method_len)) {
             if(*key_method)
                 LIBSSH2_FREE(session, *key_method);
             *key_method = LIBSSH2_ALLOC(session, match_len + suffix_len);
