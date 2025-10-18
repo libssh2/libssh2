c9dc92194916c56db14b64517089d5229dc19c75
Viktor Szakats <commit@vsz.me>
2025-10-18 22:11:35 +0200

    require 3.1.0, turns out 3.0.0 never compiled
    ```
    /home/runner/work/libssh2/libssh2/src/mbedtls.c: In function ‘_libssh2_mbedtls_cipher_init’:
    /home/runner/work/libssh2/libssh2/src/mbedtls.c:129:24: error: implicit declaration of function ‘mbedtls_cipher_info_get_key_bitlen’; did you mean ‘mbedtls_cipher_get_key_bitlen’? [-Wimplicit-function-declaration]
      129 |                   (int)mbedtls_cipher_info_get_key_bitlen(cipher_info),
          |                        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
          |                        mbedtls_cipher_get_key_bitlen
    /home/runner/work/libssh2/libssh2/src/mbedtls.c:129:24: error: nested extern declaration of ‘mbedtls_cipher_info_get_key_bitlen’ [-Werror=nested-externs]
    /home/runner/work/libssh2/libssh2/src/mbedtls.c:134:19: error: implicit declaration of function ‘mbedtls_cipher_info_get_iv_size’; did you mean ‘mbedtls_cipher_get_iv_size’? [-Wimplicit-function-declaration]
      134 |                   mbedtls_cipher_info_get_iv_size(cipher_info));
          |                   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
          |                   mbedtls_cipher_get_iv_size
    /home/runner/work/libssh2/libssh2/src/mbedtls.c:134:19: error: nested extern declaration of ‘mbedtls_cipher_info_get_iv_size’ [-Werror=nested-externs]
    ```
    https://github.com/libssh2/libssh2/actions/runs/18620223849/job/53090381294?pr=1727#step:22:49

--- src/mbedtls.c
+++ src/mbedtls.c
@@ -43,8 +43,8 @@
 
 #include <stdlib.h>
 
-#if MBEDTLS_VERSION_NUMBER < 0x03000000
-  #error "mbedTLS 3.0.0 or later required"
+#if MBEDTLS_VERSION_NUMBER < 0x03010000
+  #error "mbedTLS 3.1.0 or later required"
 #endif
 
 /*******************************************************************/
