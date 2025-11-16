4b18365282e5709ab93dd7f7e5d8ca0357780361
Joshua Rogers <MegaManSec@users.noreply.github.com>
2025-11-16 23:47:45 +0800

    Update src/agent.c
    Change error message to be more specific
    
    Co-authored-by: Viktor Szakats <vszakats@users.noreply.github.com>

--- src/agent.c
+++ src/agent.c
@@ -721,7 +721,7 @@ agent_transact_pageant(LIBSSH2_AGENT *agent, agent_transaction_ctx_t transctx)
         UnmapViewOfFile(p);
         CloseHandle(filemap);
         return _libssh2_error(agent->session, LIBSSH2_ERROR_AGENT_PROTOCOL,
-                              "no response from pageant");
+                              "pagent did not handle message");
     }
 
     UnmapViewOfFile(p);
