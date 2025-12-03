dd2977f02551bf07fda72ca96ba9174496551b72
Lars Nordin <Lars.Nordin@LNdata.se>
2025-12-03 09:22:24 +0100

    Update userauth.c

--- src/userauth.c
+++ src/userauth.c
@@ -1985,15 +1985,15 @@ retry_auth:
                                 "Invalid signature for supplied public key, "
                                 "or bad username/public key combination");
             }
-
-            LIBSSH2_FREE(session, session->userauth_pblc_data);
-            session->userauth_pblc_data = NULL;
-            session->userauth_pblc_state = libssh2_NB_state_idle;
-
-            _libssh2_error(session, LIBSSH2_ERROR_PUBLICKEY_UNVERIFIED,
-                                "Invalid signature for supplied public key, "
-                                "or bad username/public key combination");
         }
+
+        LIBSSH2_FREE(session, session->userauth_pblc_data);
+        session->userauth_pblc_data = NULL;
+        session->userauth_pblc_state = libssh2_NB_state_idle;
+
+        return _libssh2_error(session, LIBSSH2_ERROR_PUBLICKEY_UNVERIFIED,
+                              "Invalid signature for supplied public key, "
+                              "or bad username/public key combination");
     }
 
     LIBSSH2_FREE(session, session->userauth_pblc_data);
