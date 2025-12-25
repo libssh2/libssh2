4ee369424b671ebd6e7da51a761c153540930981
Viktor Szakats <commit@vsz.me>
2025-12-26 00:06:25 +0100

    checksrc.pl sync from curl, update exceptions

--- ci/checksrc.sh
+++ ci/checksrc.sh
@@ -9,14 +9,22 @@ cd "$(dirname "$0")"/..
 git ls-files '*.[ch]' '*.cc' | xargs -n1 \
 ci/checksrc.pl -i4 -m79 -AFIXME -AERRNOVAR -AFOPENMODE -ATYPEDEFSTRUCT \
   -aaccept \
+  -aatoi \
+  -acalloc \
+  -aCreateFileA \
   -afclose \
   -afopen \
   -afprintf \
+  -afree \
+  -amalloc \
   -aprintf \
+  -arealloc \
   -arecv \
   -asend \
   -asnprintf \
   -asocket \
   -asocketpair \
+  -astrdup \
+  -astrtok \
   -astrtol \
   -avsnprintf
