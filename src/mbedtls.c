1bf0876d8162a0a89f362e02964668ef8f95bab6
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2026-03-18 08:54:41 +0100

    Fix returning a pointer to stack memory

--- src/mbedtls.c
+++ src/mbedtls.c
@@ -831,7 +831,10 @@ _libssh2_mbedtls_pub_priv_keyfile(LIBSSH2_SESSION *session,
     if(ret) {
         mbedtls_strerror(ret, (char *)buf, sizeof(buf));
         mbedtls_pk_free(&pkey);
-        return _libssh2_error(session, LIBSSH2_ERROR_FILE, buf);
+        return _libssh2_error_flags(session,
+                                    LIBSSH2_ERROR_FILE,
+                                    buf,
+                                    LIBSSH2_ERR_FLAG_DUP);
     }
 
     ret = _libssh2_mbedtls_pub_priv_key(session, method, method_len,
@@ -889,7 +892,10 @@ _libssh2_mbedtls_pub_priv_keyfilememory(LIBSSH2_SESSION *session,
     if(ret) {
         mbedtls_strerror(ret, (char *)buf, sizeof(buf));
         mbedtls_pk_free(&pkey);
-        return _libssh2_error(session, LIBSSH2_ERROR_FILE, buf);
+        return _libssh2_error_flags(session,
+                                    LIBSSH2_ERROR_FILE,
+                                    buf,
+                                    LIBSSH2_ERR_FLAG_DUP);
     }
 
     ret = _libssh2_mbedtls_pub_priv_key(session, method, method_len,
