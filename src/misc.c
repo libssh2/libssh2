8ad700b045d6b0615d160601cebcc49371c201d4
Will Cosgrove <will@everydaysoftware.net>
2026-03-31 13:23:31 -0700

    fix type

--- src/misc.c
+++ src/misc.c
@@ -969,8 +969,8 @@ int _libssh2_eob(struct string_buf *buf)
 
 int _libssh2_timingsafe_bcmp(const void *b1, const void *b2, size_t n)
 {
-    const unsigned char *p1 = (unsigned char *)b1;
-    const unsigned char *p2 = (unsigned char *)b2;
+    const unsigned char *p1 = (const unsigned char *)b1;
+    const unsigned char *p2 = (const unsigned char *)b2;
     int ret = 0;
 
     for(; n > 0; n--)
