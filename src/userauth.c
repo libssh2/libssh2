92e1439d27c0c7c44bd1089fcf173aa3f4dfbbf5
Lars Nordin <Lars.Nordin@LNdata.se>
2026-04-07 16:54:48 +0200

    Update userauth.c

--- src/userauth.c
+++ src/userauth.c
@@ -304,8 +304,7 @@ userauth_password(LIBSSH2_SESSION *session,
           SSH_MSG_USERAUTH_FAILURE,
           SSH_MSG_USERAUTH_PASSWD_CHANGEREQ,
           SSH_MSG_USERAUTH_BANNER,
-          0
-        };
+          0 };
     int rc;
     unsigned int banner_len;
 
