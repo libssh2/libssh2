9c2f16982bec3e0340b34c0ee2bb763ebc6db493
Viktor Szakats <commit@vsz.me>
2026-04-08 20:30:09 +0200

    libssh2_poll: add deprecated attribute, drop from no-deprecated builds
    It was deprecated in v1.2.0 (2009-08-10).
    
    Follow-up to 2b8038e175e030113a0a75592a69cb4f98c23fe1

--- include/libssh2.h
+++ include/libssh2.h
@@ -760,8 +760,11 @@ libssh2_userauth_publickey_sk(LIBSSH2_SESSION *session,
                                   ((*sign_callback)),
                               void **abstract);
 
+#ifndef LIBSSH2_NO_DEPRECATED
+LIBSSH2_DEPRECATED(1.2.0, "Use system poll() or select()")
 LIBSSH2_API int libssh2_poll(LIBSSH2_POLLFD *fds, unsigned int nfds,
                              long timeout);
+#endif
 
 /* Channel API */
 #define LIBSSH2_CHANNEL_WINDOW_DEFAULT  (2*1024*1024)
