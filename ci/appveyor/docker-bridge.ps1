5deb8a697581a5e97e6da4ece68422f29f98500a
Viktor Szakats <commit@vsz.me>
2026-04-12 16:40:05 +0200

    try something 3

--- ci/appveyor/docker-bridge.ps1
+++ ci/appveyor/docker-bridge.ps1
@@ -22,7 +22,7 @@ elseif($ip.StartsWith('10.0.')) {
 $extip = (New-Object Net.WebClient).DownloadString('https://www.appveyor.com/tools/my-ip.aspx').Trim()
 
 # allow inbound traffic
-New-NetFirewallRule -DisplayName 'SSH via RDP port' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 22,3389
+New-NetFirewallRule -DisplayName "SSH via RDP port" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 22,3389
 
 # launch remote docker daemon with reverse SSH tunnel
 $scriptPath = (split-path -parent $MyInvocation.MyCommand.Definition) -replace '\\', '/'
