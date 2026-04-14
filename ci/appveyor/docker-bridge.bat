35a977355b9c10ea1bc40a6dd5d26f7e65c4e4a6
Viktor Szakats <commit@vsz.me>
2026-04-15 00:00:38 +0200

    docker-bridge.bat fold long line

--- ci/appveyor/docker-bridge.bat
+++ ci/appveyor/docker-bridge.bat
@@ -9,4 +9,16 @@ C:\msys64\usr\bin\sh -l -c "/usr/bin/ssh-keygen -b 2048 -t rsa -f auth -q -N '' 
 C:\msys64\usr\bin\sh -l -c "/usr/bin/ssh-keygen -A"
 C:\msys64\usr\bin\sh -l -c "/usr/bin/sshd"
 
-C:\msys64\usr\bin\sh -l -c '/usr/bin/curl -F "account=%APPVEYOR_ACCOUNT_NAME%" -F "project=%APPVEYOR_PROJECT_SLUG%" -F "buildid=%APPVEYOR_BUILD_VERSION%" -F "base=%APPVEYOR_REPO_BRANCH%" -F "hash=%APPVEYOR_REPO_COMMIT%" -F "repo=%APPVEYOR_REPO_NAME%" -F "ssh_host=%2" -F "ssh_port=%3" -F "ssh_user=`whoami`" -F "ssh_forward=127.0.0.1:%OPENSSH_SERVER_PORT% 127.0.0.1:%OPENSSH_SERVER_PORT%,127.0.0.1:2375 /var/run/docker.sock" -F "ssh_hostkey=`paste -d , /etc/ssh/ssh_host_*_key.pub`" -F "ssh_privkey=`paste -sd , auth`" -s "https://stuff.marc-hoersken.de/libssh2/dispatch.php"'
+C:\msys64\usr\bin\sh -l -c '/usr/bin/curl ^
+  -F "account=%APPVEYOR_ACCOUNT_NAME%" ^
+  -F "project=%APPVEYOR_PROJECT_SLUG%" ^
+  -F "buildid=%APPVEYOR_BUILD_VERSION%" ^
+  -F "base=%APPVEYOR_REPO_BRANCH%" ^
+  -F "hash=%APPVEYOR_REPO_COMMIT%" ^
+  -F "repo=%APPVEYOR_REPO_NAME%" ^
+  -F "ssh_host=%2" ^
+  -F "ssh_port=%3" ^
+  -F "ssh_user=`whoami`" ^
+  -F "ssh_forward=127.0.0.1:%OPENSSH_SERVER_PORT% 127.0.0.1:%OPENSSH_SERVER_PORT%,127.0.0.1:2375 /var/run/docker.sock" ^
+  -F "ssh_hostkey=`paste -d , /etc/ssh/ssh_host_*_key.pub`" ^
+  -F "ssh_privkey=`paste -sd , auth`" -s "https://stuff.marc-hoersken.de/libssh2/dispatch.php"'
