c55ab958e6c447b1229a260f5a1ff93c1f995103
Viktor Szakats <commit@vsz.me>
2026-04-24 10:17:17 +0200

    os400qc3: open PEM files in binary mode
    Follow-up to 0ca33b61b65e57fc52e580294d036c9575d455ec #1883

--- src/os400qc3.c
+++ src/os400qc3.c
@@ -313,7 +313,6 @@ static const char   beginprivkeyhdr[] = "-----BEGIN PRIVATE KEY-----";
 static const char   endprivkeyhdr[] = "-----END PRIVATE KEY-----";
 static const char   beginrsaprivkeyhdr[] = "-----BEGIN RSA PRIVATE KEY-----";
 static const char   endrsaprivkeyhdr[] = "-----END RSA PRIVATE KEY-----";
-static const char   fopenrmode[] = "r";
 static const char   fopenrbmode[] = "rb";
 
 /* The rest of character literals in this module are in EBCDIC. */
@@ -2111,7 +2110,7 @@ load_rsa_private_file(LIBSSH2_SESSION *session, const char *filename,
                       unsigned const char *passphrase,
                       loadkeyproc proc1, loadkeyproc proc8, void *loadkeydata)
 {
-    FILE *fp = fopen(filename, fopenrmode);
+    FILE *fp = fopen(filename, fopenrbmode);
     unsigned char *data = NULL;
     size_t datalen = 0;
     int ret;
