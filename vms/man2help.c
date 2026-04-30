a1f3f003522dbaf50d95d85bfb912d5b5641f8c8
Viktor Szakats <commit@vsz.me>
2026-04-30 13:53:27 +0200

    simplify fpcopy

--- vms/man2help.c
+++ vms/man2help.c
@@ -29,20 +29,12 @@ struct pf_fabnam {
 
 /*----------------------------------------------------------*/
 
-static void fpcopy(char *output, char *input, int len)
+static void fpcopy(char *output, char *input, int size)
 {
-    char *is, *os;
-    int i;
+    if(size)
+        memcpy(output, input, (size_t)size);
 
-    if(len) {
-        for(is = input, os = output, i = 0; i < len; ++i, ++is, ++os) {
-            *os = *is;
-        }
-        *os = 0;
-    }
-    else {
-        output[0] = 0;
-    }
+    output[size] = 0;
 }
 
 /*----------------------------------------------------------*/
