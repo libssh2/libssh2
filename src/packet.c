09855f0da102dc05a784166e95f2488b037c86c7
d3x0r <d3x0r@no-reply.github.com>
2026-05-29 12:25:53 -0700

    Should be using listen_state->channel instead of channel

--- src/packet.c
+++ src/packet.c
@@ -239,9 +239,9 @@ static inline int packet_queue_listener(
                              * doesn't need to be called, so just do the
                              * work it did.
                              */
-                            _libssh2_list_add(&channel->session->channels,
-                                              &channel->node);
-                            LIBSSH2_LISTENER_CONNECT(session, listn, channel);
+                            _libssh2_list_add(&listen_state->channel->session->channels,
+                                              &listen_state->channel->node);
+                            LIBSSH2_LISTENER_CONNECT(session, listn, listen_state->channel);
                         }
                         else {
                             _libssh2_list_add(&listn->queue,
