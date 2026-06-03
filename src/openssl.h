a2e8d36c0a8fc34388bb38f7216ac43a45606f0a
Viktor Szakats <commit@vsz.me>
2026-06-03 13:04:55 +0200

    openssl.h fmt sp

--- src/openssl.h
+++ src/openssl.h
@@ -411,10 +411,10 @@ typedef enum {
 #define ssh2_bn_init_from_bin()   ssh2_bn_init()
 #define ssh2_bn_set_word(bn, val) !BN_set_word(bn, val)
 int ssh2_bn_from_bin(libssh2_bn *bn, size_t len, const unsigned char *val);
-#define ssh2_bn_to_bin(bn, val) (BN_bn2bin(bn, val) <= 0)
-#define ssh2_bn_bytes(bn)       BN_num_bytes(bn)
-#define ssh2_bn_bits(bn)        BN_num_bits(bn)
-#define ssh2_bn_free(bn)        BN_clear_free(bn)
+#define ssh2_bn_to_bin(bn, val)   (BN_bn2bin(bn, val) <= 0)
+#define ssh2_bn_bytes(bn)         BN_num_bytes(bn)
+#define ssh2_bn_bits(bn)          BN_num_bits(bn)
+#define ssh2_bn_free(bn)          BN_clear_free(bn)
 
 /* Default generate and safe prime sizes for
    diffie-hellman-group-exchange-sha1 */
