5cea2f1a6150697af8f9f8d9244c7b13ba502f94
Viktor Szakats <commit@vsz.me>
2026-06-04 17:07:36 +0200

    os400qc3.h move dh protos next to mappings

--- src/os400qc3.h
+++ src/os400qc3.h
@@ -322,6 +322,11 @@ struct os400qc3_dh_ctx {  /* Diffie-Hellman context. */
 #define ssh2_dh_secret(dhctx, secret, f, p, bnctx) \
     ssh2_os400qc3_dh_secret(dhctx, secret, f, p)
 
+int ssh2_os400qc3_dh_key_pair(ssh2_dh_ctx *dhctx, ssh2_bn *public,
+                              ssh2_bn *g, ssh2_bn *p, int group_order);
+int ssh2_os400qc3_dh_secret(ssh2_dh_ctx *dhctx, ssh2_bn *secret,
+                            ssh2_bn *f, ssh2_bn *p);
+
 /*******************************************************************
  *
  * OS/400 QC3 crypto-library backend: Support procedure prototypes.
@@ -348,10 +353,6 @@ int ssh2_os400qc3_rsa_signv(LIBSSH2_SESSION *session, int algo,
                             int veccount,
                             const struct iovec vector[],
                             ssh2_rsa_ctx *ctx);
-int ssh2_os400qc3_dh_key_pair(ssh2_dh_ctx *dhctx, ssh2_bn *public,
-                              ssh2_bn *g, ssh2_bn *p, int group_order);
-int ssh2_os400qc3_dh_secret(ssh2_dh_ctx *dhctx, ssh2_bn *secret,
-                            ssh2_bn *f, ssh2_bn *p);
 
 #endif /* LIBSSH2_OS400QC3_H */
 
