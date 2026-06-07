63a2adaff098dd3f27e3295aabbc468d3f7ba696
Viktor Szakats <vszakats@users.noreply.github.com>
2026-06-08 00:19:34 +0200

    Update tests/ossfuzz/ssh2_sftp_packet_fuzzer.cc
    Delete redundant includes.

--- tests/ossfuzz/ssh2_sftp_packet_fuzzer.cc
+++ tests/ossfuzz/ssh2_sftp_packet_fuzzer.cc
@@ -33,12 +33,8 @@
  * The session handshake will fail (the fuzz data is not a valid KEXINIT)
  * but the parsing paths are exercised before the error is returned.
  */
-
-#include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
