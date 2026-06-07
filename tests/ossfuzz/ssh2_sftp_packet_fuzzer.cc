9b3df4d3aae1bea5601ccc0e525b0a0c66fcac66
Viktor Szakats <vszakats@users.noreply.github.com>
2026-06-08 00:12:09 +0200

    Update tests/ossfuzz/ssh2_sftp_packet_fuzzer.cc

--- tests/ossfuzz/ssh2_sftp_packet_fuzzer.cc
+++ tests/ossfuzz/ssh2_sftp_packet_fuzzer.cc
@@ -119,7 +119,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     }
 
     /* ------------------------------------------------------------------ */
-    /* Run the client handshake against our synthetic server data.         */
+    /* Run the client handshake against our synthetic server data.        */
     /* ------------------------------------------------------------------ */
     session = libssh2_session_init();
     if(!session)
