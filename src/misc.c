0bae1437de203fa3c13a3c2b6e4922f3a2fe24dd
Viktor Szakats <commit@vsz.me>
2026-06-08 15:00:17 +0200

    checksrc: disallow fprintf, except for select subdirectories
    checksrc-all.pl apply fix upstream

--- src/misc.c
+++ src/misc.c
@@ -607,6 +607,7 @@ void ssh2_deb_low(LIBSSH2_SESSION *session, int context,
         (session->tracehandler)(session, session->tracehandler_context, buffer,
                                 msglen);
     else
+        /* !checksrc! disable BANNEDFUNC 1 */
         fprintf(stderr, "%s\n", buffer);
 }
 
