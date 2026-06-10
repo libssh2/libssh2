4fef6e50d594ad807316cede0a1d0d273965c698
Viktor Szakats <commit@vsz.me>
2026-06-10 23:10:10 +0200

    fixup

--- src/openssl.c
+++ src/openssl.c
@@ -1926,9 +1926,9 @@ static int ossl_ed25519_openssh_priv_to_pubkey(LIBSSH2_SESSION *session,
         goto clean_exit;
     }
 
-    if(tmp_len > 0 && comment) {
+    if(tmp_len > 0 && buf) {
         ssh2_deb((session, LIBSSH2_TRACE_AUTH, "Key comment: %.*s",
-                  (int)tmp_len, comment));
+                  (int)tmp_len, buf));
     }
 
     /* Padding */
