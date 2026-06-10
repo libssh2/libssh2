5eaf13611ec2b708fb6f5106884e3130f340bb42
Viktor Szakats <commit@vsz.me>
2026-06-10 22:07:17 +0200

    pem.c err msg improvements

--- src/pem.c
+++ src/pem.c
@@ -561,7 +561,7 @@ static int openssh_pem_parse_data(LIBSSH2_SESSION *session,
             if(ssh2_get_string(&kdf_buf, &salt, &salt_len) ||
                ssh2_get_u32(&kdf_buf, &rounds) != 0) {
                 ret = ssh2_err(session, LIBSSH2_ERROR_PROTO,
-                               "kdf contains unexpected values");
+                               "KDF contains unexpected values");
                 goto out;
             }
 
@@ -576,7 +576,7 @@ static int openssh_pem_parse_data(LIBSSH2_SESSION *session,
         }
         else {
             ret = ssh2_err(session, LIBSSH2_ERROR_KEYFILE_AUTH_FAILED,
-                           "bcrypted without passphrase");
+                           "bcrypt-encrypted without passphrase");
             goto out;
         }
 
