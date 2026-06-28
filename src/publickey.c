9c764e82a2a662f18d023fa506b082a1aef4a794
Viktor Szakats <commit@vsz.me>
2026-06-28 02:35:56 +0200

    tweak error code
    https://github.com/libssh2/libssh2/pull/2128#discussion_r3487064797

--- src/publickey.c
+++ src/publickey.c
@@ -1050,7 +1050,7 @@ int libssh2_publickey_list_fetch(LIBSSH2_PUBLICKEY *pkey,
                 if(list[keys].num_attrs) {
                     if(list[keys].num_attrs >
                            SIZE_MAX / sizeof(libssh2_publickey_attribute)) {
-                        ssh2_err(session, LIBSSH2_ERROR_BUFFER_TOO_SMALL,
+                        ssh2_err(session, LIBSSH2_ERROR_OUT_OF_BOUNDARY,
                                  "Too many publickey attributes");
                         goto err_exit;
                     }
