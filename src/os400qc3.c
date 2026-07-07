07b9fe92f9db385959b3f102fbbbb5c561a9e8af
Viktor Szakats <commit@vsz.me>
2026-07-07 23:47:13 +0200

    Revert "maybe"
    This reverts commit dff9f704d4b80b9b70cc9f5a65e1ac51d81dce78.

--- src/os400qc3.c
+++ src/os400qc3.c
@@ -966,22 +966,10 @@ int ssh2_hmac_ctx_init(ssh2_hmac_ctx *ctx)
     return 1;
 }
 
-int ssh2_hmac_init(ssh2_hmac_ctx *ctx, ssh2_hmac_alg alg,
-                   void *key, size_t keylen)
+static int os400qc3_hmac_init(ssh2_hmac_ctx *ctx, ssh2_hmac_alg alg,
+                              size_t minkeylen, void *key, int keylen)
 {
-    size_t minkeylen;
     Qus_EC_t errcode;
-
-    if(alg == SSH2_MD5_HMAC)
-        minkeylen = SSH2_MD5_DIG_LEN;
-    else if(alg == SSH2_SHA1_HMAC)
-        minkeylen = SSH2_SHA1_DIG_LEN;
-    else if(alg == SSH2_SHA256_HMAC)
-        minkeylen = SSH2_SHA256_DIG_LEN;
-    else if(alg == SSH2_SHA512_HMAC)
-        minkeylen = SSH2_SHA512_DIG_LEN;
-    else
-        return 0;
 
     if(keylen < minkeylen) {
         char *lkey = alloca(minkeylen);
@@ -1001,6 +989,23 @@ int ssh2_hmac_init(ssh2_hmac_ctx *ctx, ssh2_hmac_alg alg,
                         NULL, NULL, ctx->key.Key_Context_Token,
                         (char *)&errcode);
     return errcode.Bytes_Available ? 0 : 1;
+}
+
+int ssh2_hmac_init(ssh2_hmac_ctx *ctx, ssh2_hmac_alg alg,
+                   void *key, size_t keylen)
+{
+    size_t minkeylen;
+    if(alg == SSH2_MD5_HMAC)
+        minkeylen = SSH2_MD5_DIG_LEN;
+    else if(alg == SSH2_SHA1_HMAC)
+        minkeylen = SSH2_SHA1_DIG_LEN;
+    else if(alg == SSH2_SHA256_HMAC)
+        minkeylen = SSH2_SHA256_DIG_LEN;
+    else if(alg == SSH2_SHA512_HMAC)
+        minkeylen = SSH2_SHA512_DIG_LEN;
+    else
+        return 0;
+    return os400qc3_hmac_init(ctx, alg, minkeylen, key, keylen);
 }
 
 int ssh2_hmac_update(ssh2_hmac_ctx *ctx, const void *data, size_t datalen)
@@ -1414,8 +1419,8 @@ static int pbkdf2(LIBSSH2_SESSION *session, char **dk,
         return -1;
 
     /* Create an HMAC context for our computations. */
-    if(!ssh2_hmac_init(&hctx, pkcs5->hash, pkcs5->hashlen,
-                       (void *)passphrase, strlen(passphrase)))
+    if(!os400qc3_hmac_init(&hctx, pkcs5->hash, pkcs5->hashlen,
+                           (void *)passphrase, strlen(passphrase)))
         return -1;
 
     /* Allocate the derived key buffer. */
