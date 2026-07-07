c44e081d3b579de8e460ac2436dd1804e38ee0ba
Viktor Szakats <commit@vsz.me>
2026-07-08 00:26:51 +0200

    knownhost: fix hmac context memleaks on error paths
    Bug: https://github.com/libssh2/libssh2/pull/2244#discussion_r3539968455
    
    Closes #2245

--- src/knownhost.c
+++ src/knownhost.c
@@ -424,16 +424,16 @@ static int knownhost_check(LIBSSH2_KNOWNHOSTS *hosts,
                        stored hash. */
                     unsigned char hash[SSH2_SHA1_DIG_LEN];
                     ssh2_hmac_ctx ctx;
-                    if(!ssh2_hmac_ctx_init(&ctx))
-                        break;
 
                     if(SSH2_SHA1_DIG_LEN != node->name_len)
                         /* the name hash length must be the sha1 size or
                            we cannot match it */
                         break;
-                    if(!ssh2_hmac_sha1_init(&ctx, node->salt, node->salt_len))
+                    if(!ssh2_hmac_ctx_init(&ctx))
                         break;
-                    if(!ssh2_hmac_update(&ctx, host, strlen(host)) ||
+                    if(!ssh2_hmac_sha1_init(&ctx,
+                                            node->salt, node->salt_len) ||
+                       !ssh2_hmac_update(&ctx, host, strlen(host)) ||
                        !ssh2_hmac_final(&ctx, hash, SSH2_SHA1_DIG_LEN)) {
                         ssh2_hmac_cleanup(&ctx);
                         break;
