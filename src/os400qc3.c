4584f165049947d5ec96029a4a260df2e0bbfc06
Viktor Szakats <commit@vsz.me>
2026-07-07 23:53:41 +0200

    os400qc3.c fixup

--- src/os400qc3.c
+++ src/os400qc3.c
@@ -995,14 +995,16 @@ int ssh2_hmac_init(ssh2_hmac_ctx *ctx, ssh2_hmac_alg alg,
                    void *key, size_t keylen)
 {
     size_t minkeylen;
-    if(alg == SSH2_MD5_HMAC)
-        minkeylen = SSH2_MD5_DIG_LEN;
-    else if(alg == SSH2_SHA1_HMAC)
+    if(alg == SSH2_SHA1_HMAC)
         minkeylen = SSH2_SHA1_DIG_LEN;
     else if(alg == SSH2_SHA256_HMAC)
         minkeylen = SSH2_SHA256_DIG_LEN;
     else if(alg == SSH2_SHA512_HMAC)
         minkeylen = SSH2_SHA512_DIG_LEN;
+#if LIBSSH2_MD5
+    else if(alg == SSH2_MD5_HMAC)
+        minkeylen = SSH2_MD5_DIG_LEN;
+#endif
     else
         return 0;
     return os400qc3_hmac_init(ctx, alg, minkeylen, key, keylen);
