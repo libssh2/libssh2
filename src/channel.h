9265d8f914ba0c7472a65f42dc6af1b2a2de29ac
Viktor Szakats <commit@vsz.me>
2026-07-08 03:31:16 +0200

    fixup

--- src/channel.h
+++ src/channel.h
@@ -1,6 +1,8 @@
 #ifndef LIBSSH2_CHANNEL_H
 #define LIBSSH2_CHANNEL_H
 /* Copyright (C) Daniel Stenberg
+ * All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
