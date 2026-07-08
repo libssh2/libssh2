c2e8a33f863fcc96fa7ff337177f15595f0db901
Viktor Szakats <commit@vsz.me>
2026-07-09 00:22:28 +0200

    fixup arg order

--- src/openssl.c
+++ src/openssl.c
@@ -3730,7 +3730,7 @@ clean_exit:
     return rc == 1 ? 0 : -1;
 }
 
-int ssh2_ed25519_verify(LIBSSH2_SESSION *session, ssh2_ed25519_ctx *ed_ctx,
+int ssh2_ed25519_verify(ssh2_ed25519_ctx *ed_ctx, LIBSSH2_SESSION *session,
                         const uint8_t *s, size_t s_len,
                         const uint8_t *m, size_t m_len)
 {
