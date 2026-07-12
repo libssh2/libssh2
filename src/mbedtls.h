f3d0a7902aa34fe8080752ba22e061c1f659ee12
Viktor Szakats <commit@vsz.me>
2026-07-12 17:13:41 +0200

    drop algos missing from 3+

--- src/mbedtls.h
+++ src/mbedtls.h
@@ -80,16 +80,8 @@
 #define LIBSSH2_AES_CBC 1
 #define LIBSSH2_AES_CTR 1
 #define LIBSSH2_AES_GCM 0
-#ifdef MBEDTLS_CIPHER_BLOWFISH_CBC
-# define LIBSSH2_BLOWFISH 1
-#else
-# define LIBSSH2_BLOWFISH 0
-#endif
-#ifdef MBEDTLS_CIPHER_ARC4_128
-# define LIBSSH2_RC4 1
-#else
-# define LIBSSH2_RC4 0
-#endif
+#define LIBSSH2_BLOWFISH 0
+#define LIBSSH2_RC4 0
 #define LIBSSH2_CAST 0
 #ifdef MBEDTLS_CIPHER_DES_EDE3_CBC
 # define LIBSSH2_3DES 1
@@ -198,12 +190,6 @@ typedef enum {
 #define ssh2_cipher_aes256     MBEDTLS_CIPHER_AES_256_CBC
 #define ssh2_cipher_aes192     MBEDTLS_CIPHER_AES_192_CBC
 #define ssh2_cipher_aes128     MBEDTLS_CIPHER_AES_128_CBC
-#if LIBSSH2_BLOWFISH
-#define ssh2_cipher_blowfish   MBEDTLS_CIPHER_BLOWFISH_CBC
-#endif
-#if LIBSSH2_RC4
-#define ssh2_cipher_arcfour    MBEDTLS_CIPHER_ARC4_128
-#endif
 #if LIBSSH2_3DES
 #define ssh2_cipher_3des       MBEDTLS_CIPHER_DES_EDE3_CBC
 #endif
