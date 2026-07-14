f05da595054032664a590bba7b4301df2bf481eb
Viktor Szakats <commit@vsz.me>
2026-07-14 17:33:36 +0200

    move agent backend macros to header, for reuse

---
+++ src/agent.h
@@ -0,0 +1,52 @@
+#ifndef LIBSSH2_AGENT_H
+#define LIBSSH2_AGENT_H
+/*
+ * Copyright (C) Daiki Ueno
+ * Copyright (C) Daniel Stenberg
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "libssh2_priv.h"
+
+#ifdef HAVE_SYS_UN_H
+/* Use the existence of sys/un.h as a test if Unix domain socket (AF_UNIX)
+   is supported. Windows also supports it via winsock*.h, but not used here
+   at this time. */
+#include <sys/un.h>
+#define SSH2_AGENT_BACKEND_UNIX "Unix"
+#endif
+
+#if defined(_WIN32) && !defined(LIBSSH2_WINDOWS_UWP)
+#define SSH2_AGENT_BACKEND_WIN32_PAGEANT "Pageant"
+#define SSH2_AGENT_BACKEND_WIN32_OPENSSH "OpenSSH"
+#endif
+
+#endif /* LIBSSH2_AGENT_H */
