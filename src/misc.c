645ad4f87010e4098c149ff253070ab8ef91b722
Viktor Szakats <commit@vsz.me>
2026-07-21 17:40:28 +0200

    fixup

--- src/misc.c
+++ src/misc.c
@@ -999,15 +999,15 @@ int ssh2_timingsafe_bcmp(const void *b1, const void *b2, size_t n)
 }
 
 #ifndef LIBSSH2_KEY_SK
-int ssh2_sk_pubkey_blob(LIBSSH2_SESSION *session,
-                        char **method, size_t *method_len,
-                        unsigned char **pubkeydata, size_t *pubkeydata_len,
-                        int *algorithm, unsigned char *flags,
-                        const char **application,
-                        const unsigned char **key_handle, size_t *handle_len,
-                        const char *privatekey,
-                        const char *privkeyblob, size_t privkeyblob_len,
-                        const char *passphrase)
+int ssh2_sk_pubkey(LIBSSH2_SESSION *session,
+                   char **method, size_t *method_len,
+                   unsigned char **pubkeydata, size_t *pubkeydata_len,
+                   int *algorithm, unsigned char *flags,
+                   const char **application,
+                   const unsigned char **key_handle, size_t *handle_len,
+                   const char *privatekey,
+                   const char *privkeyblob, size_t privkeyblob_len,
+                   const char *passphrase)
 {
     (void)method;
     (void)method_len;
