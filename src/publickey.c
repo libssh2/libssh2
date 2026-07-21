c21756c76fb3ca92d6f5095414f3cf9a614c6d4e
Viktor Szakats <commit@vsz.me>
2026-07-21 18:04:12 +0200

    init the whole extended list with zero

--- src/publickey.c
+++ src/publickey.c
@@ -1116,6 +1116,9 @@ int libssh2_publickey_list_fetch(LIBSSH2_PUBLICKEY *pkey,
                                  "publickey attributes");
                         goto err_exit;
                     }
+                    memset(list[keys].attrs, 0,
+                           list[keys].num_attrs *
+                               sizeof(libssh2_publickey_attribute));
                     for(i = 0; i < list[keys].num_attrs; i++) {
                         if(pkey->listFetch_data_len <
                            (size_t)(pkey->listFetch_s - pkey->listFetch_data) +
