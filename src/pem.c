0d03d17e037eced0f09284b190c07bc7e112bc9e
Viktor Szakats <commit@vsz.me>
2026-07-22 04:09:10 +0200

    u2

--- src/pem.c
+++ src/pem.c
@@ -33,30 +33,6 @@
 
 #include "libssh2_priv.h"
 
-static int pem_readline_file(char *line, int line_size, FILE *fp)
-{
-    size_t len;
-
-    if(!line)
-        return -1;
-    if(!fgets(line, line_size, fp))
-        return -1;
-
-    if(*line) {
-        len = strlen(line);
-        if(len > 0 && line[len - 1] == '\n')
-            line[len - 1] = '\0';
-    }
-
-    if(*line) {
-        len = strlen(line);
-        if(len > 0 && line[len - 1] == '\r')
-            line[len - 1] = '\0';
-    }
-
-    return 0;
-}
-
 static int pem_readline_blob(char *line, size_t line_size,
                              const char *blob, size_t blob_len,
                              size_t *blob_offset)
