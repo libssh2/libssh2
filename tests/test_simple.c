552f9611e0e2b5d14960dcb46b5c324186ebf3c3
Viktor Szakats <commit@vsz.me>
2026-07-23 23:24:58 +0200

    long line

--- tests/test_simple.c
+++ tests/test_simple.c
@@ -80,8 +80,8 @@ static int test_ssh2_dh_validate(void)
     for(i = 0; i < SSH2_ARRAYSIZE(tests); i++) {
         int got;
 #ifdef LIBSSH2_LIBGCRYPT
-        int fi = atoi(tests[i].f);
-        gcry_mpi_t f = gcry_mpi_set_ui(NULL, (unsigned long)(fi >= 0 ? fi : -fi));
+        int j = atoi(tests[i].f);
+        gcry_mpi_t f = gcry_mpi_set_ui(NULL, (unsigned long)(j < 0 ? -j : j));
         gcry_mpi_t p = gcry_mpi_set_ui(NULL, (unsigned long)atoi(tests[i].p));
         if(tests[i].f[0] == '-')
             gcry_mpi_neg(f, f);
