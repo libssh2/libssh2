cf71ff2e1dbafc39240affc0d4439b9f6265b468
Viktor Szakats <commit@vsz.me>
2026-07-28 12:49:32 +0200

    simplify with strncmp

--- src/pem.c
+++ src/pem.c
@@ -191,9 +191,7 @@ int ssh2_pem_parse(LIBSSH2_SESSION *session,
     if(pem_readline(line, LINE_SIZE, blob, blob_len, &off))
         goto out;
 
-    if(passphrase &&
-       strlen(line) == sizeof(crypt_annotation) - 1 &&
-       !memcmp(line, crypt_annotation, sizeof(crypt_annotation) - 1)) {
+    if(passphrase && !strncmp(line, crypt_annotation, strlen(line))) {
         const struct crypt_method **all_methods, *cur_method;
         int i;
 
