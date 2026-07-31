6d6c00a5fb437cc80e80a8433cc74f767f975ca4
Viktor Szakats <commit@vsz.me>
2026-08-01 00:13:48 +0200

    fixup

--- src/session.c
+++ src/session.c
@@ -1760,8 +1760,7 @@ int libssh2_poll(LIBSSH2_POLLFD *fds, unsigned int nfds, long timeout_ms)
                 }
             }
         }
-#endif /* HAVE_POLL */
-          timeout_remaining) is equal to 0 */
+#endif /* HAVE_POLL, timeout_ms (and by extension timeout_remaining) is 0 */
     } while(timeout_remaining > 0 && !active_fds);
 
 #ifdef HAVE_POLL
