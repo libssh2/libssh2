7e6570e36039f152f2e2bed4a76510738c247c58
Viktor Szakats <commit@vsz.me>
2026-08-01 16:17:30 +0200

    set file formats explicitly

--- tests/keys-generate.sh
+++ tests/keys-generate.sh
@@ -13,12 +13,12 @@ rm ./openssh_server/*_key || true
 
 ssh-keygen -t rsa     -b 2048 -N ''          -m PEM     -C ''                        -f 'openssh_server/ssh_host_rsa_key'
 ssh-keygen -t ecdsa   -b  256 -N ''          -m PEM     -C ''                        -f 'openssh_server/ssh_host_ecdsa_key'
-ssh-keygen -t ed25519         -N ''                     -C ''                        -f 'openssh_server/ssh_host_ed25519_key'
+ssh-keygen -t ed25519         -N ''          -m RFC4716 -C ''                        -f 'openssh_server/ssh_host_ed25519_key'
 
 rm ./openssh_server/ca_* || true
 
-ssh-keygen -t ecdsa   -b  521 -N ''                     -C 'ca_ecdsa'                -f 'openssh_server/ca_ecdsa'
-ssh-keygen -t rsa     -b 3072 -N ''                     -C 'ca_rsa'                  -f 'openssh_server/ca_rsa'
+ssh-keygen -t ecdsa   -b  521 -N ''          -m RFC4716 -C 'ca_ecdsa'                -f 'openssh_server/ca_ecdsa'
+ssh-keygen -t rsa     -b 3072 -N ''          -m RFC4716 -C 'ca_rsa'                  -f 'openssh_server/ca_rsa'
 
 # tests
 
@@ -33,19 +33,19 @@ ssh-keygen -t dsa             -N ''          -m PEM     -C 'id_dsa_wrong'       
 
 ssh-keygen -t rsa     -b 2048 -N ''          -m PEM     -C 'id_rsa'                  -f 'keys/id_rsa'
 ssh-keygen -t rsa     -b 2048 -N "${pw}"     -m PEM     -C 'id_rsa_encrypted'        -f 'keys/id_rsa_encrypted'
-ssh-keygen -t rsa     -b 2048 -N ''                     -C ''                        -f 'keys/id_rsa_openssh'       # empty comment
-ssh-keygen -t rsa     -b 2048 -N "${pw}"                -C 'id_rsa_aes256gcm'        -f 'keys/id_rsa_aes256gcm'     -Z aes256-gcm@openssh.com
-ssh-keygen -t rsa     -b 4096 -N ''                     -C 'id_rsa_signed'           -f 'keys/id_rsa_signed'
+ssh-keygen -t rsa     -b 2048 -N ''          -m RFC4716 -C ''                        -f 'keys/id_rsa_openssh'       # empty comment
+ssh-keygen -t rsa     -b 2048 -N "${pw}"     -m RFC4716 -C 'id_rsa_aes256gcm'        -f 'keys/id_rsa_aes256gcm'     -Z aes256-gcm@openssh.com
+ssh-keygen -t rsa     -b 4096 -N ''          -m RFC4716 -C 'id_rsa_signed'           -f 'keys/id_rsa_signed'
 ssh-keygen                    -I "${id}" -n "${pr}"     -s 'openssh_server/ca_rsa'      'keys/id_rsa_signed.pub'
-ssh-keygen -t rsa     -b 4096 -N ''                     -C 'id_rsa_sha2_256_signed'  -f 'keys/id_rsa_sha2_256_signed'
+ssh-keygen -t rsa     -b 4096 -N ''          -m RFC4716 -C 'id_rsa_sha2_256_signed'  -f 'keys/id_rsa_sha2_256_signed'
 ssh-keygen                    -I "${id}" -n "${pr}"     -s 'openssh_server/ca_rsa'      'keys/id_rsa_sha2_256_signed.pub'
 
-ssh-keygen -t ecdsa   -b  384 -N ''                     -C ''                        -f 'keys/id_ecdsa'             # empty comment
-ssh-keygen -t ecdsa   -b  384 -N ''                     -C 'id_ecdsa_signed'         -f 'keys/id_ecdsa_signed'
+ssh-keygen -t ecdsa   -b  384 -N ''          -m RFC4716 -C ''                        -f 'keys/id_ecdsa'             # empty comment
+ssh-keygen -t ecdsa   -b  384 -N ''          -m RFC4716 -C 'id_ecdsa_signed'         -f 'keys/id_ecdsa_signed'
 ssh-keygen                    -I "${id}" -n "${pr}"     -s 'openssh_server/ca_ecdsa'    'keys/id_ecdsa_signed.pub'
 
-ssh-keygen -t ed25519         -N ''                     -C 'id_ed25519'              -f 'keys/id_ed25519'
-ssh-keygen -t ed25519         -N "${pw}"                -C 'id_ed25519_encrypted'    -f 'keys/id_ed25519_encrypted' -Z aes256-ctr
+ssh-keygen -t ed25519         -N ''          -m RFC4716 -C 'id_ed25519'              -f 'keys/id_ed25519'
+ssh-keygen -t ed25519         -N "${pw}"     -m RFC4716 -C 'id_ed25519_encrypted'    -f 'keys/id_ed25519_encrypted' -Z aes256-ctr
 
 cat \
   'keys/id_dsa.pub' \
