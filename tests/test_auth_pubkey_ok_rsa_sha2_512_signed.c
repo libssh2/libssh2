bf34563107d803d3203812373c6f70c5fda1072d
Viktor Szakats <commit@vsz.me>
2026-08-02 16:34:41 +0200

    test_*signed: use PEM format for backends not supporting OpenSSH format for RSA, allow overriding username

--- tests/test_auth_pubkey_ok_rsa_sha2_512_signed.c
+++ tests/test_auth_pubkey_ok_rsa_sha2_512_signed.c
@@ -9,15 +9,19 @@
 
 int test(LIBSSH2_SESSION *session)
 {
-#if LIBSSH2_RSA_SHA2 && \
-    (defined(LIBSSH2_OPENSSL) || defined(LIBSSH2_WOLFSSL))
+#if LIBSSH2_RSA_SHA2
     const char *user = getenv("FIXTURE_USER");
     /* set in Dockerfile */
     return test_auth_pubkey(session, 0,
                             user ? user : "libssh2",
                             NULL,
                             "keys/id_rsa_sha2_512_signed-cert.pub",
-                            "keys/id_rsa_sha2_512_signed");
+#if defined(LIBSSH2_OPENSSL) || defined(LIBSSH2_WOLFSSL)
+                            "keys/id_rsa_sha2_512_signed"
+#else
+                            "keys/id_rsa_sha2_512_signed_pem"
+#endif
+                            );
 #else
     (void)session;
     return 0;
--- tests/test_auth_pubkey_ok_rsa_signed.c
+++ tests/test_auth_pubkey_ok_rsa_signed.c
@@ -9,15 +9,19 @@
 
 int test(LIBSSH2_SESSION *session)
 {
-#if LIBSSH2_RSA_SHA1 && \
-    (defined(LIBSSH2_OPENSSL) || defined(LIBSSH2_WOLFSSL))
+#if LIBSSH2_RSA_SHA1
     const char *user = getenv("FIXTURE_USER");
     /* set in Dockerfile */
     return test_auth_pubkey(session, 0,
                             user ? user : "libssh2",
                             NULL,
                             "keys/id_rsa_signed-cert.pub",
-                            "keys/id_rsa_signed");
+#if defined(LIBSSH2_OPENSSL) || defined(LIBSSH2_WOLFSSL)
+                            "keys/id_rsa_signed"
+#else
+                            "keys/id_rsa_signed_pem"
+#endif
+                            );
 #else
     (void)session;
     return 0;
