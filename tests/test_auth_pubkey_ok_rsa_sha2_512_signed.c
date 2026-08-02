9a959ada91ce7b900db358e0d58063272dfd0ad9
Viktor Szakats <commit@vsz.me>
2026-08-02 17:57:38 +0200

    tests: allow overriding the username in `test_*_signed` tests
    Via env `FIXTURE_USER`.
    
    To make it easier to run tests locally, in particular those using signed
    keys, with the username embedded in them.
    
    Cherry-picked from #2483

--- tests/test_auth_pubkey_ok_rsa_sha2_512_signed.c
+++ tests/test_auth_pubkey_ok_rsa_sha2_512_signed.c
@@ -5,13 +5,16 @@
 
 #include "runner.h"
 
+#include <stdlib.h>  /* for getenv() */
+
 int test(LIBSSH2_SESSION *session)
 {
 #if LIBSSH2_RSA_SHA2 && \
     (defined(LIBSSH2_OPENSSL) || defined(LIBSSH2_WOLFSSL))
+    const char *user = getenv("FIXTURE_USER");
     /* set in Dockerfile */
     return test_auth_pubkey(session, 0,
-                            "libssh2",
+                            user ? user : "libssh2",
                             NULL,
                             "keys/id_rsa_sha2_512_signed-cert.pub",
                             "keys/id_rsa_sha2_512_signed");
