4b5303ab6dbd3e7c8df03a0ffa13aea866770caf
Viktor Szakats <commit@vsz.me>
2026-08-03 13:21:44 +0200

    tests: add PKCS8-formatted plain and encrypted keys and tests
    They are supported via the backends, except OS/400 which has its own
    parser, leaving libgcrypt and WinCNG (untested) without support. With
    mbedTLS PKCS8 encryption (aes-128-cbc) requires 3.6.0+.
    
    Add RSA, ECDSA and ED25519 keys.
    
    What I find interesting in PKCS8 private keys, is the easy way to
    inspect the ASN.1 content (`openssl asn1parse -dump -in <fn>`). I could
    not find similar tooling for PEM and OpenSSH files.
    
    Also:
    - tests/openssh_server/authorized_keys: sync up comments with private
      keys and the generator script.
    
    Ref: https://github.com/Mbed-TLS/mbedtls/releases/tag/v3.6.0
    
    Closes #2500

---
+++ tests/test_auth_pubkey_ok_ecdsa_pkcs8_encrypted.c
@@ -0,0 +1,23 @@
+/* Copyright (C) The libssh2 project and its contributors.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "runner.h"
+
+int test(LIBSSH2_SESSION *session)
+{
+#if LIBSSH2_ECDSA && \
+    !defined(LIBSSH2_LIBGCRYPT) && !defined(LIBSSH2_WINCNG) && \
+    (!defined(LIBSSH2_MBEDTLS) || MBEDTLS_VERSION_NUMBER >= 0x03060000)
+    /* set in Dockerfile */
+    return test_auth_pubkey(session, 0,
+                            "libssh2",
+                            "libssh2",
+                            "keys/id_ecdsa_pkcs8_encrypted.pub",
+                            "keys/id_ecdsa_pkcs8_encrypted");
+#else
+    (void)session;
+    return 0;
+#endif
+}
---
+++ tests/test_auth_pubkey_ok_ed25519_pkcs8.c
@@ -0,0 +1,22 @@
+/* Copyright (C) The libssh2 project and its contributors.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "runner.h"
+
+int test(LIBSSH2_SESSION *session)
+{
+#if LIBSSH2_ED25519 && \
+    !defined(LIBSSH2_LIBGCRYPT) && !defined(LIBSSH2_WINCNG)
+    /* set in Dockerfile */
+    return test_auth_pubkey(session, 0,
+                            "libssh2",
+                            NULL,
+                            "keys/id_ed25519_pkcs8.pub",
+                            "keys/id_ed25519_pkcs8");
+#else
+    (void)session;
+    return 0;
+#endif
+}
