42c13a107c10a51668b3aad3ec11978251fa9d1b
Viktor Szakats <commit@vsz.me>
2026-08-03 13:49:52 +0200

    also disable for WinCNG, not tested, but since using local key parsers, not supported

--- tests/test_auth_pubkey_ok_rsa_pkcs8.c
+++ tests/test_auth_pubkey_ok_rsa_pkcs8.c
@@ -7,7 +7,7 @@
 
 int test(LIBSSH2_SESSION *session)
 {
-#if LIBSSH2_RSA_SHA1 && !defined(LIBSSH2_LIBGCRYPT)
+#if LIBSSH2_RSA_SHA1 && !defined(LIBSSH2_LIBGCRYPT) && !defined(LIBSSH2_WINCNG)
     /* set in Dockerfile */
     return test_auth_pubkey(session, 0,
                             "libssh2",
