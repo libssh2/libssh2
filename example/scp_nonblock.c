13988d302e38c776baf7da433ee639d07bf0aff4
Viktor Szakats <commit@vsz.me>
2026-08-03 11:19:46 +0200

    examples: drop local macro in MSVC functions

--- example/scp_nonblock.c
+++ example/scp_nonblock.c
@@ -47,15 +47,13 @@ static int gettimeofday(struct timeval *tp, void *tzp)
 {
     (void)tzp;
     if(tp) {
-/* Offset between 1601-01-01 and 1970-01-01 in 100 nanosec units */
-#define WIN32_FT_OFFSET 116444736000000000
         union {
             libssh2_uint64_t ns100; /* time since 1 Jan 1601 in 100ns units */
             FILETIME ft;
         } now;
         GetSystemTimeAsFileTime(&now.ft);
         tp->tv_usec = (long)((now.ns100 / 10) % 1000000);
-        tp->tv_sec = (long)((now.ns100 - WIN32_FT_OFFSET) / 10000000);
+        tp->tv_sec = (long)((now.ns100 - 116444736000000000) / 10000000);
     }
     return 0;
 }
