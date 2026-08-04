935c012c99c1bed620885cf30ecfe3c80ac7309e
Viktor Szakats <commit@vsz.me>
2026-08-04 09:25:12 +0200

    mbedtls.c fix errcode confusion

--- src/mbedtls.c
+++ src/mbedtls.c
@@ -263,7 +263,7 @@ int ssh2_cipher_crypt(ssh2_cipher_ctx *ctx, SSH2_CIPHER_T(algo),
                 memcpy(block + blocksize - authlen, tag, authlen);
             else if(ssh2_timingsafe_bcmp(tag, block + blocksize - authlen,
                                          authlen))
-                return MBEDTLS_ERR_GCM_AUTH_FAILED;
+                return -1;  /* GCM auth failed */
 
             /* Increment IV for next packet */
             for(i = 11; i >= 4; i--)
