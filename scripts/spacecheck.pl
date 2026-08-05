ced76044fb9afa389163d9019ca2b57fcab7166c
Viktor Szakats <commit@vsz.me>
2026-08-06 01:55:52 +0200

    spch

--- scripts/spacecheck.pl
+++ scripts/spacecheck.pl
@@ -51,6 +51,7 @@ my @double_empty_lines = (
 my @longline = (
     '^libssh2-style\.el$',
     'tests/keys/.+\.pub$',
+    'tests/openssh_server/.+\.pub$',
     'tests/openssh_server/authorized_keys$',
     'tests/openssh_server/ca_user_keys.pub$',
     'tests/openssh_server/sshd_config$',
