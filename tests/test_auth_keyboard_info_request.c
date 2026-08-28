567709be32b71b6c9022272530d185ff5aef4eef
Viktor Szakats <commit@vsz.me>
2026-08-28 10:21:43 +0200

    Revert "test_auth_keyboard_info_request: drop unused `free_count` global variable"
    This reverts commit 1d7e0ba25f87fc4b92f1a1302301d441b9d8e3ab.

--- tests/test_auth_keyboard_info_request.c
+++ tests/test_auth_keyboard_info_request.c
@@ -215,6 +215,7 @@ failed_malloc_test_cases[FAILED_MALLOC_TEST_CASES_LEN] = {
 };
 
 static int alloc_count = 0;
+static int free_count = 0;
 
 static LIBSSH2_ALLOC_FUNC(test_alloc)
 {
@@ -230,6 +231,7 @@ static LIBSSH2_ALLOC_FUNC(test_alloc)
 static LIBSSH2_FREE_FUNC(test_free)
 {
     (void)abstract;
+    free_count++;
     free(ptr);
 }
 
@@ -243,6 +245,7 @@ static int test_case(int num,
     LIBSSH2_SESSION *session;
 
     alloc_count = 0;
+    free_count = 0;
 
     session = libssh2_session_init_ex(test_alloc, test_free, NULL, abstract);
     if(!session) {
