d25d8b090f729f38976024b85e617125ca2f7d77
Viktor Szakats <commit@vsz.me>
2026-09-03 09:46:33 +0200

    am

---
+++ tests/keys-dump.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# Copyright (C) Viktor Szakats
+# SPDX-License-Identifier: BSD-3-Clause
+
+# Dump keys in more human-parsable formats
+
+for dir in keys openssh_server; do
+  [ -d "${dir}-dump" ] || mkdir "${dir}-dump"
+  rm -f "${dir}-dump"/*
+
+  find "${dir}" -maxdepth 1 \( -name 'ca_host_*' -o -name 'ca_user_*' -o -name 'id_*' \) | while read -r f; do
+    o="${dir}-dump/$(basename $f)"
+    echo $f
+    if [[ "$f" = *'.pub' ]]; then
+      b64="$(grep -o -E ' .+ ' "$f")"
+      [ -z "$b64" ] && b64="$(grep -o -E ' .+$' "$f")"
+      echo "$b64" | tr -d ' ' | base64 -d > "$o.bin"
+      if [[ "$f" = *'cert'* ]]; then
+        ssh-keygen -L -f "$f" > "$o.cert.txt"
+      fi
+    else
+      if [[ "$f" = *'pkcs8'* ]]; then
+        openssl asn1parse -dump -in "$f" > "$o.pkey.asn1.txt"
+      else
+        grep -v -E '^(DEK-Info|Proc-Type)' "$f" | base64 -d > "$o.pkey.bin"
+      fi
+      openssl pkey -text -noout -passin pass:libssh2 -in "$f" >> "$o.pkey.txt" 2>/dev/null
+      [ -s "$o.pkey.txt" ] || rm -f "$o.pkey.txt"
+    fi
+    cp -p "$f" "$o"
+  done
+done
+
+openssl -version
