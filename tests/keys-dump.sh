27bf5689a17f717f25d480b2823789d0614340fb
Viktor Szakats <commit@vsz.me>
2026-09-03 10:27:07 +0200

    fixups, simplify

--- tests/keys-dump.sh
+++ tests/keys-dump.sh
@@ -9,13 +9,13 @@ for dir in keys openssh_server; do
   [ -d "${dir}-dump" ] || mkdir "${dir}-dump"
   rm -f "${dir}-dump"/*
 
-  find "${dir}" -maxdepth 1 \( -name 'ca_host_*' -o -name 'ca_user_*' -o -name 'id_*' \) | while read -r f; do
+  find "${dir}" -maxdepth 1 \( -name 'ca_*' -o -name 'id_*' -o -name 'ssh_*' \) | while read -r f; do
     o="${dir}-dump/$(basename "$f")"
     echo "$f"
     if [[ "$f" = *'.pub' ]]; then
       b64="$(grep -o -E ' .+ ' "$f")"
       [ -z "$b64" ] && b64="$(grep -o -E ' .+$' "$f")"
-      echo "$b64" | tr -d ' ' | base64 -d > "$o.bin"
+      echo "$b64" | tr -d ' ' | gbase64 -d > "$o.bin"
       if [[ "$f" = *'cert'* ]]; then
         ssh-keygen -L -f "$f" > "$o.cert.txt"
       fi
@@ -23,7 +23,7 @@ for dir in keys openssh_server; do
       if [[ "$f" = *'pkcs8'* ]]; then
         openssl asn1parse -dump -in "$f" > "$o.pkey.asn1.txt"
       else
-        grep -v -E '^(DEK-Info|Proc-Type)' "$f" | base64 -d > "$o.pkey.bin"
+        grep -v -E '^(-----|DEK-Info|Proc-Type)' "$f" | gbase64 -d > "$o.pkey.bin"
       fi
       openssl pkey -text -noout -passin pass:libssh2 -in "$f" >> "$o.pkey.txt" 2>/dev/null
       [ -s "$o.pkey.txt" ] || rm -f "$o.pkey.txt"
