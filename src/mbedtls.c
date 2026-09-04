f2f18b85334c00aced78f4aca966ff3160fd0b73
Viktor Szakats <commit@vsz.me>
2026-09-04 15:40:12 +0200

    mbedtls.c

--- src/mbedtls.c
+++ src/mbedtls.c
@@ -700,7 +700,7 @@ static unsigned char *mbed_gen_publickey_from_rsa(LIBSSH2_SESSION *session,
 static int mbed_pub_priv_key(LIBSSH2_SESSION *session, char **method,
                              unsigned char **pubkeydata,
                              size_t *pubkeydata_len,
-                             mbedtls_pk_context *pkey)
+                             const mbedtls_pk_context *pkey)
 {
     char *method_buf = NULL;
     unsigned char *key = NULL;
