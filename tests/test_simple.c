94b15f731c3c2b040a875c0a0dcde42a94386d44
Viktor Szakats <commit@vsz.me>
2026-09-07 15:22:06 +0200

    s2

--- tests/test_simple.c
+++ tests/test_simple.c
@@ -234,7 +234,6 @@ static int test_ssh2_dh_validate(void)
         const char *f; const char *p; int expected;
     };
     static const struct tbn tests[] = {
-        {  "-1",  "10", -1 },
         {   "2",  "10", -3 },
         {   "1",  "10", -1 },
         {   "0",  "10", -1 },
@@ -249,32 +248,12 @@ static int test_ssh2_dh_validate(void)
 
     for(i = 0; i < SSH2_ARRAYSIZE(tests); i++) {
         struct tbn t = tests[i];
-        int got;
+        int got = 0;
         ssh2_bn *f = ssh2_bn_init();
         ssh2_bn *p = ssh2_bn_init();
-#ifdef LIBSSH2_LIBGCRYPT
-        ssh2_bn_set_word(f, (unsigned long)abs(atoi(t.f)));
-        ssh2_bn_set_word(p, (unsigned long)atoi(t.p));
-        if(t.f[0] == '-')
-            gcry_mpi_neg(f, f);
+        ssh2_bn_set_word(f, (uint32_t)atoi(t.f));
+        ssh2_bn_set_word(p, (uint32_t)atoi(t.p));
         got = ssh2_dh_validate(f, p);
-#elif defined(LIBSSH2_MBEDTLS)
-        if(mbedtls_mpi_read_string(f, 10, t.f) ||
-           mbedtls_mpi_read_string(p, 10, t.p))
-            got = -9;
-        else
-            got = ssh2_dh_validate(f, p);
-#elif defined(LIBSSH2_OPENSSL) || \
-    (defined(LIBSSH2_WOLFSSL) && LIBWOLFSSL_VERSION_HEX >= 0x05006000)
-        BIGNUM *f = BN_new(), *p = BN_new();
-        if(!BN_dec2bn(&f, t.f) ||
-           !BN_dec2bn(&p, t.p))
-            got = -9;
-        else
-            got = ssh2_dh_validate(f, p);
-#else
-        got = t.expected;
-#endif
         ssh2_bn_free(f);
         ssh2_bn_free(p);
         if(got != t.expected) {
