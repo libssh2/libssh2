3f63884125486fd7d348206337e6ebe878ef8e04
Viktor Szakats <commit@vsz.me>
2026-09-12 23:38:25 +0200

    readd OPENSSH_NO_DOCKER

--- tests/openssh_fixture.c
+++ tests/openssh_fixture.c
@@ -475,10 +475,12 @@ int start_openssh_fixture(void)
     }
 #endif
 
-    docker_cmd = getenv("FIXTURE_CONTAINER_CMD");
-    if(!docker_cmd)
-        docker_cmd = "docker";
-    have_docker = !!*docker_cmd;
+    if(!getenv("OPENSSH_NO_DOCKER")) /* for compatibility */
+        docker_cmd = getenv("FIXTURE_CONTAINER_CMD");
+        if(!docker_cmd)
+            docker_cmd = "docker";
+        have_docker = !!*docker_cmd;
+    }
 
     ret = build_openssh_server_docker_image();
     if(!ret)
