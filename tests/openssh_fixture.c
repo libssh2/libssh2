27df98c50b8d170fc378714b80a955a862a8f93b
Viktor Szakats <commit@vsz.me>
2026-09-12 23:14:45 +0200

    comment

--- tests/openssh_fixture.c
+++ tests/openssh_fixture.c
@@ -213,6 +213,7 @@ static int start_openssh_server(char **container_id_out)
         const char *container_host_port = openssh_server_port();
         if(container_host_port) {
             if(strstr(docker_cmd, "container"))
+                /* Requires Apple container 0.7.0+ for '--progress none' */
                 return run_command(container_id_out, "%s run --progress none "
                                    "--rm -d -p %s:22 libssh2/openssh_server",
                                    docker_cmd, container_host_port);
