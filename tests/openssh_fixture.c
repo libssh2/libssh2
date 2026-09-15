43bd36b9d698c8ee940d23291f27710479d52648
Viktor Szakats <commit@vsz.me>
2026-09-15 19:55:50 +0200

    Revert "openssh_fixture.c list /etc/ssh after creation"
    This reverts commit bbdb9d5f74775bc4432a7eb539d34af39ac17db3.

--- tests/openssh_fixture.c
+++ tests/openssh_fixture.c
@@ -179,26 +179,11 @@ static int build_openssh_server_container_image(void)
                     return ret;
             }
         }
-        run_command(NULL,
-                    "%s build --quiet --tag libssh2/openssh_server "
-                    "--file %s %s", container_cmd,
-                    srcdir_path("openssh_server/Containerfile"),
-                    srcdir_path("openssh_server"));
-
-        {
-            char *out = NULL;
-            int ret = run_command(&out, "%s run --tty "
-                                  "libssh2/openssh_server "
-                                  "ls -lA /etc/ssh", container_cmd);
-            if(ret)
-                fprintf(stderr, "Failed to ls: %d\n", ret);
-            else
-                fprintf(stderr,
-                        "----- debug ------\n%s\n"
-                        "------------------\n", out);
-            free(out);
-            return ret;
-        }
+        return run_command(NULL,
+                           "%s build --quiet --tag libssh2/openssh_server "
+                           "--file %s %s", container_cmd,
+                           srcdir_path("openssh_server/Containerfile"),
+                           srcdir_path("openssh_server"));
     }
     else
         return 0;
